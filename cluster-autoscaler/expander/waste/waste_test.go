be796d8218aedcb42b6795cd6102c931eb418fd8
Marcin Wielgus <mwielgus@google.com>
2017-01-03 17:04:42 +0100

    Cluster-autoscaler: add get nodes function to cloud provider interface

--- cluster-autoscaler/expander/waste/waste_test.go
+++ cluster-autoscaler/expander/waste/waste_test.go
@@ -37,6 +37,7 @@ func (f *FakeNodeGroup) IncreaseSize(delta int) error    { return nil }
 func (f *FakeNodeGroup) DeleteNodes([]*apiv1.Node) error { return nil }
 func (f *FakeNodeGroup) Id() string                      { return f.id }
 func (f *FakeNodeGroup) Debug() string                   { return f.id }
+func (f *FakeNodeGroup) Nodes() ([]string, error)        { return []string{}, nil }
 
 func makeNodeInfo(cpu int64, memory int64, pods int64) *schedulercache.NodeInfo {
 	node := &apiv1.Node{
