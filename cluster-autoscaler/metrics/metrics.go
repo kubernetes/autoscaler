83ef3d2be3547699dab8292af74e20a119389c5f
Maciej Pytel <maciekpytel@google.com>
2017-04-13 13:11:37 +0200

    Added CA metrics related to cluster state

--- cluster-autoscaler/metrics/metrics.go
+++ cluster-autoscaler/metrics/metrics.go
@@ -17,15 +17,50 @@ limitations under the License.
 package metrics
 
 import (
+	"reflect"
 	"time"
+
+	"k8s.io/autoscaler/cluster-autoscaler/clusterstate"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+const (
+	caNamespace   = "cluster_autoscaler"
+	readyLabel    = "ready"
+	unreadyLabel  = "unready"
+	startingLabel = "notStarted"
+)
+
 var (
+	/**** Metrics related to cluster state ****/
+	clusterSafeToAutoscale = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: caNamespace,
+			Name:      "cluster_safe_to_autoscale",
+			Help:      "Whether or not cluster is healthy enough for autoscaling. 1 if it is, 0 otherwise.",
+		},
+	)
+
+	nodesCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: caNamespace,
+			Name:      "nodes_count",
+			Help:      "Number of nodes in cluster.",
+		}, []string{"state"},
+	)
+
+	unschedulablePodsCount = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: caNamespace,
+			Name:      "unschedulable_pods_count",
+			Help:      "Number of unschedulable pods in the cluster.",
+		},
+	)
+
 	lastTimestamp = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "cluster_autoscaler",
+			Namespace: caNamespace,
 			Name:      "last_time_seconds",
 			Help:      "Last time CA run some main loop fragment.",
 		}, []string{"main"},
@@ -33,7 +68,7 @@ var (
 
 	lastDuration = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "cluster_autoscaler",
+			Namespace: caNamespace,
 			Name:      "last_duration_microseconds",
 			Help:      "Time spent in last main loop fragments in microseconds.",
 		}, []string{"main"},
@@ -41,7 +76,7 @@ var (
 
 	duration = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
-			Namespace: "cluster_autoscaler",
+			Namespace: caNamespace,
 			Name:      "duration_microseconds",
 			Help:      "Time spent in main loop fragments in microseconds.",
 		}, []string{"main"},
@@ -52,6 +87,9 @@ func init() {
 	prometheus.MustRegister(duration)
 	prometheus.MustRegister(lastDuration)
 	prometheus.MustRegister(lastTimestamp)
+	prometheus.MustRegister(clusterSafeToAutoscale)
+	prometheus.MustRegister(nodesCount)
+	prometheus.MustRegister(unschedulablePodsCount)
 }
 
 func durationToMicro(start time.Time) float64 {
@@ -67,4 +105,25 @@ func UpdateDuration(label string, start time.Time) {
 // UpdateLastTime records the time the step identified by the label was started
 func UpdateLastTime(label string) {
 	lastTimestamp.WithLabelValues(label).Set(float64(time.Now().Unix()))
+}
+
+// UpdateClusterState updates metrics related to cluster state
+func UpdateClusterState(csr *clusterstate.ClusterStateRegistry) {
+	if csr == nil || reflect.ValueOf(csr).IsNil() {
+		return
+	}
+	if csr.IsClusterHealthy() {
+		clusterSafeToAutoscale.Set(1)
+	} else {
+		clusterSafeToAutoscale.Set(0)
+	}
+	readiness := csr.GetClusterReadiness()
+	nodesCount.WithLabelValues(readyLabel).Set(float64(readiness.Ready))
+	nodesCount.WithLabelValues(unreadyLabel).Set(float64(readiness.Unready + readiness.LongNotStarted))
+	nodesCount.WithLabelValues(startingLabel).Set(float64(readiness.NotStarted))
+}
+
+// UpdateUnschedulablePodsCount records number of currently unschedulable pods
+func UpdateUnschedulablePodsCount(podsCount int) {
+	unschedulablePodsCount.Set(float64(podsCount))
 }
