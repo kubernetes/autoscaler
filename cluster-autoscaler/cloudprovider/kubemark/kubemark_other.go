6e841840560e4bc7a25b7d4d4e5dcf5855e1a35e
Beata Skiba <bskiba@google.com>
2017-06-29 16:29:26 +0200

    Cloud Provider Interface for Kubemark
    This allows to run Custer Autoscaler on Kubemark.
    See autoscaler/cluster-autoscaler/proposals/kubemark_integration.md
    for more details.

--- cluster-autoscaler/cloudprovider/kubemark/kubemark_other.go
+++ cluster-autoscaler/cloudprovider/kubemark/kubemark_other.go
@@ -1,3 +1,5 @@
+// +build !linux
+
 /*
 Copyright 2017 The Kubernetes Authors.
 
@@ -14,8 +16,60 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +build !linux
-
-// Dummy implementation. Real one should be build on linux.
+// Dummy implementation. Real one should be built on linux.
 
 package kubemark
+
+import (
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
+	"k8s.io/autoscaler/cluster-autoscaler/utils/errors"
+	"k8s.io/kubernetes/plugin/pkg/scheduler/schedulercache"
+)
+
+type KubemarkCloudProvider struct{}
+
+func BuildKubemarkCloudProvider(kubemarkController *kubemark.KubemarkController, specs []string) (*KubemarkCloudProvider, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (kubemark *KubemarkCloudProvider) Name() string { return "" }
+
+func (kubemark *KubemarkCloudProvider) NodeGroups() []cloudprovider.NodeGroup {
+	return []cloudProvider.NodeGroup{}
+}
+
+func (kubemark *KubemarkCloudProvider) Pricing() (cloudprovider.PricingModel, errors.AutoscalerError) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (kubemark *KubemarkCloudProvider) NodeGroupForNode(node *apiv1.Node) (cloudprovider.NodeGroup, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+func (kubemark *KubemarkCloudProvider) GetAvilableMachineTypes() ([]string, error) {
+	return []string{}, cloudprovider.ErrNotImplemented
+}
+
+func (kubemark *KubemarkCloudProvider) NewNodeGroup(name string, machineType string, labels map[string]string, extraResources map[string]resource.Quantity) (cloudprovider.NodeGroup, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+
+type FakeNodeGroup struct{}
+
+func (f *FakeNodeGroup) Id() string                            { return "" }
+func (f *FakeNodeGroup) MinSize() int                          { return 0 }
+func (f *FakeNodeGroup) MaxSize() int                          { return 0 }
+func (f *FakeNodeGroup) Debug() string                         { return "" }
+func (f *FakeNodeGroup) Nodes() ([]string, error)              { return []string{}, cloudprovider.ErrNotImplemented }
+func (f *FakeNodeGroup) DeleteNodes(nodes []*apiv1.Node) error { return cloudprovider.ErrNotImplemented }
+func (f *FakeNodeGroup) IncreaseSize(delta int) error          { return cloudprovider.ErrNotImplemented }
+func (f *FakeNodeGroup) TargetSize() (int, error)              { return 0, cloudprovider.ErrNotImplemented }
+func (f *FakeNodeGroup) DecreaseTargetSize(delta int) error    { return cloudprovider.ErrNotImplemented }
+func (f *FakeNodeGroup) TemplateNodeInfo() (*schedulercache.NodeInfo, error) {
+	return nil, cloudprovider.ErrNotImplemented
+}
+func (f *FakeNodeGroup) Exist() (bool, error) { return true, nil }
+func (f *FakeNodeGroup) Create() error        { return cloudprovider.ErrNotImplemented }
+func (f *FakeNodeGroup) Delete() error        { return cloudprovider.ErrNotImplemented }
