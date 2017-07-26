d856967921632199a01bee5d94f8a5cb9d6d8df5
Maciej Pytel <maciekpytel@google.com>
2017-07-26 15:37:40 +0200

    Remove checking and resetting PodSchedulable condition
    The performance cost was too high and the pods should
    be filtered out by follow up checks anyway.
    Check out https://github.com/kubernetes/autoscaler/issues/187
    for details.

--- cluster-autoscaler/core/utils.go
+++ cluster-autoscaler/core/utils.go
@@ -31,10 +31,8 @@ import (
 
 	apiv1 "k8s.io/api/core/v1"
 	extensionsv1 "k8s.io/api/extensions/v1beta1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kube_client "k8s.io/client-go/kubernetes"
 	api "k8s.io/kubernetes/pkg/api"
-	podv1 "k8s.io/kubernetes/pkg/api/v1/pod"
 	kubeletapis "k8s.io/kubernetes/pkg/kubelet/apis"
 	"k8s.io/kubernetes/plugin/pkg/scheduler/schedulercache"
 
@@ -45,60 +43,6 @@ const (
 	// ReschedulerTaintKey is the name of the taint created by rescheduler.
 	ReschedulerTaintKey = "CriticalAddonsOnly"
 )
-
-// GetAllNodesAvailableTime returns time when the newest node became available for scheduler.
-// TODO: This function should use LastTransitionTime from NodeReady condition.
-func GetAllNodesAvailableTime(nodes []*apiv1.Node) time.Time {
-	var result time.Time
-	for _, node := range nodes {
-		if node.CreationTimestamp.After(result) {
-			result = node.CreationTimestamp.Time
-		}
-	}
-	return result.Add(1 * time.Minute)
-}
-
-// SlicePodsByPodScheduledTime slices given pod array into those where PodScheduled condition
-// have been updated after the thresold and others.
-// Each pod must be in condition "Scheduled: False; Reason: Unschedulable"
-func SlicePodsByPodScheduledTime(pods []*apiv1.Pod, threshold time.Time) (oldPods []*apiv1.Pod, newPods []*apiv1.Pod) {
-	for _, pod := range pods {
-		_, condition := podv1.GetPodCondition(&pod.Status, apiv1.PodScheduled)
-		if condition != nil {
-			if condition.LastTransitionTime.After(threshold) {
-				newPods = append(newPods, pod)
-			} else {
-				oldPods = append(oldPods, pod)
-			}
-		}
-	}
-	return
-}
-
-// ResetPodScheduledCondition resets pod condition PodScheduled to "unknown" for all the pods with LastTransitionTime
-// not after the threshold time.
-func ResetPodScheduledCondition(kubeClient kube_client.Interface, pods []*apiv1.Pod) {
-	for _, pod := range pods {
-		if err := resetPodScheduledConditionForPod(kubeClient, pod); err != nil {
-			glog.Errorf("Error during reseting pod condition for %s/%s: %v", pod.Namespace, pod.Name, err)
-		}
-	}
-}
-
-func resetPodScheduledConditionForPod(kubeClient kube_client.Interface, pod *apiv1.Pod) error {
-	_, condition := podv1.GetPodCondition(&pod.Status, apiv1.PodScheduled)
-	if condition != nil {
-		glog.V(4).Infof("Reseting pod condition for %s/%s, last transition: %s",
-			pod.Namespace, pod.Name, condition.LastTransitionTime.Time.String())
-		condition.Status = apiv1.ConditionUnknown
-		condition.LastTransitionTime = metav1.Time{
-			Time: time.Now(),
-		}
-		_, err := kubeClient.Core().Pods(pod.Namespace).UpdateStatus(pod)
-		return err
-	}
-	return fmt.Errorf("Expected condition PodScheduled")
-}
 
 // FilterOutSchedulable checks whether pods from <unschedulableCandidates> marked as unschedulable
 // by Scheduler actually can't be scheduled on any node and filter out the ones that can.
