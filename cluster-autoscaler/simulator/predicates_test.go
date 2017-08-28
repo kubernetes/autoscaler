281afa71470bb77462e8fa8d495ddb35ec432649
Maciej Pytel <maciekpytel@google.com>
2017-08-28 19:37:28 +0200

    precompute predicateMetadata in scale-down

--- cluster-autoscaler/simulator/predicates_test.go
+++ cluster-autoscaler/simulator/predicates_test.go
@@ -56,11 +56,11 @@ func TestPredicates(t *testing.T) {
 	_, err = predicateChecker.FitsAny(p3, nodeInfos)
 	assert.Error(t, err)
 
-	err = predicateChecker.CheckPredicates(p2, ni1, ReturnVerboseError)
+	err = predicateChecker.CheckPredicates(p2, nil, ni1, ReturnVerboseError)
 	assert.True(t, strings.Contains(err.Error(), "Insufficient cpu"))
-	assert.Error(t, predicateChecker.CheckPredicates(p2, ni1, ReturnVerboseError))
-	assert.NoError(t, predicateChecker.CheckPredicates(p4, ni1, ReturnVerboseError))
-	assert.NoError(t, predicateChecker.CheckPredicates(p2, ni2, ReturnVerboseError))
-	assert.NoError(t, predicateChecker.CheckPredicates(p4, ni2, ReturnVerboseError))
-	assert.Error(t, predicateChecker.CheckPredicates(p3, ni2, ReturnVerboseError))
+	assert.Error(t, predicateChecker.CheckPredicates(p2, nil, ni1, ReturnVerboseError))
+	assert.NoError(t, predicateChecker.CheckPredicates(p4, nil, ni1, ReturnVerboseError))
+	assert.NoError(t, predicateChecker.CheckPredicates(p2, nil, ni2, ReturnVerboseError))
+	assert.NoError(t, predicateChecker.CheckPredicates(p4, nil, ni2, ReturnVerboseError))
+	assert.Error(t, predicateChecker.CheckPredicates(p3, nil, ni2, ReturnVerboseError))
 }
