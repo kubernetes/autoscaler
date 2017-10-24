26ee7c0f2022ed667e52280fedfa4d0835fb301e
Maciej Pytel <maciekpytel@google.com>
2017-10-24 14:54:29 +0200

    Add Refresh method to cloud provider
    This can be used to dynamically update cloud provider
    config (in particular list of managed NodeGroups and their
    min/max constraints).
    Add GKE implementation.

--- cluster-autoscaler/cloudprovider/gce/gce_cloud_provider.go
+++ cluster-autoscaler/cloudprovider/gce/gce_cloud_provider.go
@@ -162,6 +162,12 @@ func (gce *GceCloudProvider) GetResourceLimiter() (*cloudprovider.ResourceLimite
 	return gce.resourceLimiter, nil
 }
 
+// Refresh is called before every main loop and can be used to dynamically update cloud provider state.
+// In particular the list of node groups returned by NodeGroups can change as a result of CloudProvider.Refresh().
+func (gce *GceCloudProvider) Refresh() error {
+	return gce.gceManager.Refresh()
+}
+
 // GceRef contains s reference to some entity in GCE/GKE world.
 type GceRef struct {
 	Project string
