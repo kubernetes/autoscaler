d81dca5991a4978088fe2e2ebfe4d991b4b4eb8a
Maciej Pytel <maciekpytel@google.com>
2017-11-10 17:41:19 +0100

    Mark nodes with uninitialized GPUs as unready

--- cluster-autoscaler/utils/gpu/gpu.go
+++ cluster-autoscaler/utils/gpu/gpu.go
@@ -26,29 +26,69 @@ import (
 const (
 	// ResourceNvidiaGPU is the name of the Nvidia GPU resource.
 	ResourceNvidiaGPU = "nvidia.com/gpu"
+	// GPULabel is the label added to nodes with GPU resource on GKE.
+	GPULabel = "cloud.google.com/gke-accelerator"
 )
 
-// SetGPUAllocatableToCapacity allows us to tolerate the fact that nodes with
-// GPUs can have allocatable set to 0 for multiple minutes after becoming ready
-// Without this workaround, Cluster Autoscaler will trigger an unnecessary
-// additional scale up before the node is fully operational.
-// TODO: Remove this once we handle dynamically privisioned resources well.
-func SetGPUAllocatableToCapacity(nodes []*apiv1.Node) []*apiv1.Node {
-	result := []*apiv1.Node{}
-	for _, node := range nodes {
-		newNode := node
-		if gpuCapacity, ok := node.Status.Capacity[ResourceNvidiaGPU]; ok {
-			if gpuAllocatable, ok := node.Status.Allocatable[ResourceNvidiaGPU]; !ok || gpuAllocatable.IsZero() {
-				nodeCopy, err := api.Scheme.DeepCopy(node)
-				if err != nil {
-					glog.Errorf("Failed to make a copy of node %v", node.ObjectMeta.Name)
-				} else {
-					newNode = nodeCopy.(*apiv1.Node)
-					newNode.Status.Allocatable[ResourceNvidiaGPU] = gpuCapacity.DeepCopy()
-				}
+// FilterOutNodesWithUnreadyGpus removes nodes that should have GPU, but don't have it in allocatable
+// from ready nodes list and updates their status to unready on all nodes list.
+// This is a hack/workaround for nodes with GPU coming up without installed drivers, resulting
+// in GPU missing from their allocatable and capacity.
+func FilterOutNodesWithUnreadyGpus(allNodes, readyNodes []*apiv1.Node) ([]*apiv1.Node, []*apiv1.Node) {
+	newAllNodes := make([]*apiv1.Node, 0)
+	newReadyNodes := make([]*apiv1.Node, 0)
+	nodesWithUnreadyGpu := make(map[string]*apiv1.Node)
+	for _, node := range readyNodes {
+		isUnready := false
+		_, hasGpuLabel := node.Labels[GPULabel]
+		gpuAllocatable, hasGpuAllocatable := node.Status.Allocatable[ResourceNvidiaGPU]
+		// We expect node to have GPU based on label, but it doesn't show up
+		// on node object. Assume the node is still not fully started (installing
+		// GPU drivers).
+		if hasGpuLabel && (!hasGpuAllocatable || gpuAllocatable.IsZero()) {
+			newNode, err := getUnreadyNodeCopy(node)
+			if err != nil {
+				glog.Errorf("Failed to override status of node %v with unready GPU: %v",
+					node.Name, err)
+			} else {
+				glog.V(3).Infof("Overriding status of node %v, which seems to have unready GPU",
+					node.Name)
+				nodesWithUnreadyGpu[newNode.Name] = newNode
+				isUnready = true
 			}
 		}
-		result = append(result, newNode)
+		if !isUnready {
+			newReadyNodes = append(newReadyNodes, node)
+		}
 	}
-	return result
+	// Override any node with unready GPU with its "unready" copy
+	for _, node := range allNodes {
+		if newNode, found := nodesWithUnreadyGpu[node.Name]; found {
+			newAllNodes = append(newAllNodes, newNode)
+		} else {
+			newAllNodes = append(newAllNodes, node)
+		}
+	}
+	return newAllNodes, newReadyNodes
+}
+
+func getUnreadyNodeCopy(node *apiv1.Node) (*apiv1.Node, error) {
+	nodeCopy, err := api.Scheme.DeepCopy(node)
+	if err != nil {
+		return nil, err
+	}
+	newNode := nodeCopy.(*apiv1.Node)
+	newReadyCondition := apiv1.NodeCondition{
+		Type:               apiv1.NodeReady,
+		Status:             apiv1.ConditionFalse,
+		LastTransitionTime: node.CreationTimestamp,
+	}
+	newNodeConditions := []apiv1.NodeCondition{newReadyCondition}
+	for _, condition := range newNode.Status.Conditions {
+		if condition.Type != apiv1.NodeReady {
+			newNodeConditions = append(newNodeConditions, condition)
+		}
+	}
+	newNode.Status.Conditions = newNodeConditions
+	return newNode, nil
 }
