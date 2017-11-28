65aca02fdc69896318edce039fd3bbf1e97465d6
Marcin Wielgus <mwielgus@google.com>
2017-11-28 14:20:55 +0100

    Rename override to _override to allow ./... patterns in go command

--- cluster-autoscaler/fix_gopath.sh
+++ cluster-autoscaler/fix_gopath.sh
@@ -24,7 +24,7 @@ for item in $with_vendor; do
 done
 
 echo Overriding GKE API
-cp $GOPATH/src/k8s.io/autoscaler/cluster-autoscaler/override/google.golang.org/api/container/v1alpha1/*  $GOPATH/src/google.golang.org/api/container/v1alpha1
-cp $GOPATH/src/k8s.io/autoscaler/cluster-autoscaler/override/google.golang.org/api/container/v1beta1/*  $GOPATH/src/google.golang.org/api/container/v1beta1
+cp $GOPATH/src/k8s.io/autoscaler/cluster-autoscaler/_override/google.golang.org/api/container/v1alpha1/*  $GOPATH/src/google.golang.org/api/container/v1alpha1
+cp $GOPATH/src/k8s.io/autoscaler/cluster-autoscaler/_override/google.golang.org/api/container/v1beta1/*  $GOPATH/src/google.golang.org/api/container/v1beta1
 cd $GOPATH/src/google.golang.org/api/
 git commit -a -m "Api override for NAP"
