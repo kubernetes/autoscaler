d498b91c2e70cbde25894f00f4bf0b96d1f62dd6
tkulczynski <tkulczynski@users.noreply.github.com>
2018-01-23 22:05:09 +0100

    Implement admission controller (#563)
    * Implement admission controller
    
    * Fix typo: change hpa to vpa
    
    * Store certs in a secret instead of hardcoding them in go source
    
    * Put certs into a struct instead of global vars

---
+++ vertical-pod-autoscaler/admission-controller/logic/recommendation_provider_test.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package logic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	apiv1 "k8s.io/api/core/v1"
+	vpa_types "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/poc.autoscaling.k8s.io/v1alpha1"
+	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/utils/test"
+)
+
+func TestUpdateResourceRequests(t *testing.T) {
+	type testCase struct {
+		pod            *apiv1.Pod
+		vpa            *vpa_types.VerticalPodAutoscaler
+		expectedAction bool
+		expectedMem    string
+		expectedCPU    string
+	}
+	containerName := "container1"
+	labels := map[string]string{"app": "testingApp"}
+	vpa := test.BuildTestVerticalPodAutoscaler(containerName, "2", "1", "3", "200M", "100M", "1G", "app = testingApp")
+
+	uninitialized := test.BuildTestPod("test_uninitialized", containerName, "", "", nil, nil)
+	uninitialized.ObjectMeta.Labels = labels
+
+	initialized := test.BuildTestPod("test_initialized", containerName, "1", "100M", nil, nil)
+	initialized.ObjectMeta.Labels = labels
+
+	mismatchedVPA := test.BuildTestVerticalPodAutoscaler(containerName, "2", "1", "3", "200M", "100M", "1G", "app = differentApp")
+
+	testCases := []testCase{{
+		pod:            uninitialized,
+		vpa:            vpa,
+		expectedAction: true,
+		expectedMem:    "200M",
+		expectedCPU:    "2",
+	}, {
+		pod:            initialized,
+		vpa:            vpa,
+		expectedAction: true,
+		expectedMem:    "200M",
+		expectedCPU:    "2",
+	}, {
+		pod:            uninitialized,
+		vpa:            mismatchedVPA,
+		expectedAction: false,
+	}}
+	for _, tc := range testCases {
+		vpaLister := &test.VerticalPodAutoscalerListerMock{}
+		vpaLister.On("List").Return([]*vpa_types.VerticalPodAutoscaler{tc.vpa}, nil)
+
+		recommendationProvider := &recommendationProvider{
+			vpaLister: vpaLister,
+		}
+
+		requests, err := recommendationProvider.GetRequestForPod(tc.pod)
+
+		if tc.expectedAction {
+			assert.Nil(t, err)
+			assert.Equal(t, len(requests), 1)
+		} else {
+			assert.Equal(t, len(requests), 0)
+		}
+	}
+}
---
+++ vertical-pod-autoscaler/admission-controller/main.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"net/http"
+	"time"
+
+	"github.com/golang/glog"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/autoscaler/vertical-pod-autoscaler/admission-controller/logic"
+	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/poc.autoscaling.k8s.io/v1alpha1"
+	vpa_clientset "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/client/clientset/versioned"
+	vpa_lister "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/client/listers/poc.autoscaling.k8s.io/v1alpha1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	certsDir = *flag.String("certs-dir", "/etc/tls-certs", `Where the TLS cert files are stored.`)
+)
+
+func newReadyVPALister(stopChannel <-chan struct{}) vpa_lister.VerticalPodAutoscalerLister {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		glog.Fatal(err)
+	}
+	listWatcher := cache.NewListWatchFromClient(
+		vpa_clientset.NewForConfigOrDie(config).PocV1alpha1().RESTClient(),
+		"verticalpodautoscalers", v1.NamespaceAll, fields.Everything())
+	store := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+	lister := vpa_lister.NewVerticalPodAutoscalerLister(store)
+	reflector := cache.NewReflector(listWatcher, &v1alpha1.VerticalPodAutoscaler{}, store, time.Hour)
+	go reflector.Run(stopChannel)
+	return lister
+}
+
+func main() {
+	flag.Parse()
+	certs := initCerts(certsDir)
+	stopChannel := make(chan struct{})
+	vpaLister := newReadyVPALister(stopChannel)
+	as := &admissionServer{logic.NewRecommendationProvider(vpaLister)}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		as.serve(w, r)
+	})
+	clientset := getClient()
+	server := &http.Server{
+		Addr:      ":8000",
+		TLSConfig: configTLS(clientset, certs.serverCert, certs.serverKey),
+	}
+	go selfRegistration(clientset, certs.caCert)
+	server.ListenAndServeTLS("", "")
+}
