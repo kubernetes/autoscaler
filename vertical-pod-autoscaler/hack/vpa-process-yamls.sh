9d69058aca4112bb69930000c2ae552d9086f4e8
Tomek Kulczyński <tkulczynski@google.com>
2018-01-30 13:56:35 +0100

    Use TAG environment variable as well as REGISTRY in vpa scripts. Unify default registries and tags in all vpa components.

--- vertical-pod-autoscaler/hack/vpa-process-yamls.sh
+++ vertical-pod-autoscaler/hack/vpa-process-yamls.sh
@@ -23,11 +23,17 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 YAMLS="api/vpa-crd.yaml deploy/vpa-rbac.yaml deploy/admission-controller-rbac.yaml deploy/updater-deployment.yaml deploy/recommender-deployment.yaml deploy/admission-controller-deployment.yaml"
 
 REGISTRY_TO_APPLY=${REGISTRY-gcr.io/kubernetes-develop}
+TAG_TO_APPLY=${TAG-0.0.1}
 
 if [ "x$REGISTRY" != "xgcr.io/kubernetes-develop" ]; then
-  echo "WARNING! Using image repository from REGISTRY env variable (${REGISTRY_TO_APPLY}) instead of gcr.io/kubernetes-develop"
+  echo "WARNING! Using image repository from REGISTRY env variable (${REGISTRY_TO_APPLY}) instead of gcr.io/kubernetes-develop."
+fi
+
+
+if [ "x$TAG" != "x0.0.1" ]; then
+  echo "WARNING! Using tag from TAG env variable (${TAG_TO_APPLY}) instead of the default (0.0.1)."
 fi
 
 for i in $YAMLS; do
-  sed -e "s,gcr.io/kubernetes-develop/,${REGISTRY_TO_APPLY}/," ${SCRIPT_ROOT}/$i | kubectl $1 -f -
+  sed -e "s,gcr.io/kubernetes-develop/\([a-z-]*\):.*,${REGISTRY_TO_APPLY}/\1:${TAG_TO_APPLY}," ${SCRIPT_ROOT}/$i | kubectl $1 -f -
 done
