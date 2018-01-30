83521b6d3912e78e05a70c804c5be5af39ee2799
tkulczynski <tkulczynski@users.noreply.github.com>
2018-01-30 15:35:06 +0100

    Improvements to deploy scripts (#591)
    * Use TAG environment variable as well as REGISTRY in vpa scripts. Unify default registries and tags in all vpa components.
    
    * Add a possibility to only create/delete one of the components

--- vertical-pod-autoscaler/hack/vpa-process-yamls.sh
+++ vertical-pod-autoscaler/hack/vpa-process-yamls.sh
@@ -20,14 +20,42 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
+function print_help {
+  echo "ERROR! Usage: vpa-process-yamls.sh <action> [<component>]"
+  echo "<action> should be either 'create' or 'delete'."
+  echo "<component> might be on of 'admission-controller', 'updater', 'recommender'."
+  echo "If <component> is set, only the deployment of that component will be processed,"
+  echo "otherwise all components and configs will be processed."
+}
+
+if [ $# -eq 0 ]; then
+  print_help
+  exit 1
+fi
+
+if [ $# -gt 2 ]; then
+  print_help
+  exit 1
+fi
+
 YAMLS="api/vpa-crd.yaml deploy/vpa-rbac.yaml deploy/admission-controller-rbac.yaml deploy/updater-deployment.yaml deploy/recommender-deployment.yaml deploy/admission-controller-deployment.yaml"
 
+if [ $# -gt 1 ]; then
+  YAMLS="deploy/$2-deployment.yaml"
+fi
+
 REGISTRY_TO_APPLY=${REGISTRY-gcr.io/kubernetes-develop}
+TAG_TO_APPLY=${TAG-0.0.1}
 
 if [ "x$REGISTRY" != "xgcr.io/kubernetes-develop" ]; then
-  echo "WARNING! Using image repository from REGISTRY env variable (${REGISTRY_TO_APPLY}) instead of gcr.io/kubernetes-develop"
+  echo "WARNING! Using image repository from REGISTRY env variable (${REGISTRY_TO_APPLY}) instead of gcr.io/kubernetes-develop."
+fi
+
+
+if [ "x$TAG" != "x0.0.1" ]; then
+  echo "WARNING! Using tag from TAG env variable (${TAG_TO_APPLY}) instead of the default (0.0.1)."
 fi
 
 for i in $YAMLS; do
-  sed -e "s,gcr.io/kubernetes-develop/,${REGISTRY_TO_APPLY}/," ${SCRIPT_ROOT}/$i | kubectl $1 -f -
+  sed -e "s,gcr.io/kubernetes-develop/\([a-z-]*\):.*,${REGISTRY_TO_APPLY}/\1:${TAG_TO_APPLY}," ${SCRIPT_ROOT}/$i | kubectl $1 -f -
 done
