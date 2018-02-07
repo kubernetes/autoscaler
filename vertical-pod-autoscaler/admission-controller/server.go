e93c43bd84bbef38017ccbf0e5f10db55ad6afa6
Tomek Kulczyński <tkulczynski@google.com>
2018-02-07 17:44:34 +0100

    Make admission controller set default mode for VPA objects

--- vertical-pod-autoscaler/admission-controller/server.go
+++ vertical-pod-autoscaler/admission-controller/server.go
@@ -27,10 +27,60 @@ import (
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/autoscaler/vertical-pod-autoscaler/admission-controller/logic"
+	vpa_types "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/poc.autoscaling.k8s.io/v1alpha1"
 )
 
 type admissionServer struct {
 	recommendationProvider logic.RecommendationProvider
+}
+
+type patchRecord struct {
+	Op    string      `json:"op,inline"`
+	Path  string      `json:"path,inline"`
+	Value interface{} `json:"value"`
+}
+
+func (s *admissionServer) getPatchesForPodResourceRequest(raw []byte) ([]patchRecord, error) {
+	pod := v1.Pod{}
+	if err := json.Unmarshal(raw, &pod); err != nil {
+		return nil, err
+	}
+	if len(pod.Name) == 0 {
+		pod.Name = pod.GenerateName + "%"
+	}
+	glog.V(4).Infof("Admitting pod %v", pod.ObjectMeta)
+	requests, err := s.recommendationProvider.GetRequestForPod(&pod)
+	if err != nil {
+		return nil, err
+	}
+	patches := []patchRecord{}
+	for i, resources := range requests {
+		for resource, request := range resources {
+			patches = append(patches, patchRecord{
+				Op:    "add",
+				Path:  fmt.Sprintf("/spec/containers/%d/resources/requests/%s", i, resource),
+				Value: request.String()})
+		}
+	}
+	return patches, nil
+}
+
+func getPatchesForVPADefaults(raw []byte) ([]patchRecord, error) {
+	vpa := vpa_types.VerticalPodAutoscaler{}
+	if err := json.Unmarshal(raw, &vpa); err != nil {
+		return nil, err
+	}
+	glog.V(4).Infof("Processing vpa: %v", vpa)
+	patches := []patchRecord{}
+	if vpa.Spec.UpdatePolicy.UpdateMode == "" {
+		// Sets the whole updatePolicy. Note that if it contained mutiple fields
+		// this needed to be changed to respect their original values.
+		patches = append(patches, patchRecord{
+			Op:    "add",
+			Path:  "/spec/updatePolicy",
+			Value: vpa_types.PodUpdatePolicy{UpdateMode: vpa_types.UpdateModeAuto}})
+	}
+	return patches, nil
 }
 
 // only allow pods to pull images from specific registry.
@@ -43,36 +93,25 @@ func (s *admissionServer) admit(data []byte) *v1beta1.AdmissionResponse {
 	// The externalAdmissionHookConfiguration registered via selfRegistration
 	// asks the kube-apiserver only sends admission request regarding pods.
 	podResource := metav1.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
-	if ar.Request.Resource != podResource {
-		glog.Errorf("expect resource to be %s", podResource)
-		return nil
+	vpaResource := metav1.GroupVersionResource{Group: "poc.autoscaling.k8s.io", Version: "v1alpha1", Resource: "verticalpodautoscalers"}
+	var patches []patchRecord
+	var err error
+
+	switch ar.Request.Resource {
+	case podResource:
+		patches, err = s.getPatchesForPodResourceRequest(ar.Request.Object.Raw)
+	case vpaResource:
+		patches, err = getPatchesForVPADefaults(ar.Request.Object.Raw)
+	default:
+		patches, err = nil, fmt.Errorf("expected the resource to be %v or %v", podResource, vpaResource)
 	}
 
-	raw := ar.Request.Object.Raw
-	pod := v1.Pod{}
-	if err := json.Unmarshal(raw, &pod); err != nil {
-		glog.Error(err)
-		return nil
-	}
-	if len(pod.Name) == 0 {
-		pod.Name = pod.GenerateName + "%"
-	}
-	requests, err := s.recommendationProvider.GetRequestForPod(&pod)
 	if err != nil {
 		glog.Error(err)
 		return nil
 	}
 	response := v1beta1.AdmissionResponse{}
 	response.Allowed = true
-	patches := []map[string]string{}
-	for i, resources := range requests {
-		for resource, request := range resources {
-			patches = append(patches, map[string]string{
-				"op":    "add",
-				"path":  fmt.Sprintf("/spec/containers/%d/resources/requests/%s", i, resource),
-				"value": request.String()})
-		}
-	}
 	if len(patches) > 0 {
 		patch, err := json.Marshal(patches)
 		if err != nil {
@@ -82,9 +121,8 @@ func (s *admissionServer) admit(data []byte) *v1beta1.AdmissionResponse {
 		patchType := v1beta1.PatchTypeJSONPatch
 		response.PatchType = &patchType
 		response.Patch = patch
-		glog.V(4).Infof("Setting resource for pod %s: %v", pod.ObjectMeta.Name, patches)
+		glog.V(4).Infof("Sending patches: %v", patches)
 	}
-	glog.V(4).Infof("Replying with %v", response)
 	return &response
 }
 
