d8c0595290c4096e2a6601a7e8798e661d91a891
Slawomir Chylek <schylek@google.com>
2018-02-15 16:48:41 +0100

    VPA updater e2e test introduced.

---
+++ vertical-pod-autoscaler/e2e/common.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package autoscaling
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	"k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	vpa_types "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/poc.autoscaling.k8s.io/v1alpha1"
+	"k8s.io/kubernetes/test/e2e/framework"
+)
+
+const (
+	recommenderComponent = "recommender"
+	updateComponent      = "updater"
+	pollInterval         = framework.Poll
+	pollTimeout          = 5 * time.Minute
+)
+
+func e2eDescribe(scenario, name string, body func()) bool {
+	return ginkgo.Describe(fmt.Sprintf("[VPA] [%s] %s", scenario, name), body)
+}
+
+func recommenderE2eDescribe(name string, body func()) bool {
+	return e2eDescribe(recommenderComponent, name, body)
+}
+
+func updaterE2eDescribe(name string, body func()) bool {
+	return e2eDescribe(updateComponent, name, body)
+}
+
+func hamsterDeployment(f *framework.Framework, cpuQuantity, memoryQuantity resource.Quantity) *extensions.Deployment {
+	d := framework.NewDeployment("hamster-deployment", 3, map[string]string{"app": "hamster"}, "hamster", "gcr.io/google_containers/ubuntu-slim:0.1", extensions.RollingUpdateDeploymentStrategyType)
+	d.ObjectMeta.Namespace = f.Namespace.Name
+	d.Spec.Template.Spec.Containers[0].Resources.Requests = v1.ResourceList{
+		v1.ResourceCPU:    cpuQuantity,
+		v1.ResourceMemory: memoryQuantity,
+	}
+	d.Spec.Template.Spec.Containers[0].Command = []string{"/bin/sh"}
+	d.Spec.Template.Spec.Containers[0].Args = []string{"-c", "/usr/bin/yes >/dev/null"}
+	return d
+}
+
+func newVPA(f *framework.Framework, name string, selector *metav1.LabelSelector) *vpa_types.VerticalPodAutoscaler {
+	vpa := vpa_types.VerticalPodAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: f.Namespace.Name,
+		},
+		Spec: vpa_types.VerticalPodAutoscalerSpec{
+			Selector: selector,
+			UpdatePolicy: vpa_types.PodUpdatePolicy{
+				UpdateMode: vpa_types.UpdateModeAuto,
+			},
+			ResourcePolicy: vpa_types.PodResourcePolicy{
+				ContainerPolicies: []vpa_types.ContainerResourcePolicy{},
+			},
+		},
+	}
+	return &vpa
+}
+
+func parseQuantityOrDie(text string) resource.Quantity {
+	quantity, err := resource.ParseQuantity(text)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	return quantity
+}
---
+++ vertical-pod-autoscaler/e2e/updater.go
@@ -0,0 +1,119 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package autoscaling
+
+import (
+	"fmt"
+	"reflect"
+
+	apiv1 "k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	vpa_types "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/poc.autoscaling.k8s.io/v1alpha1"
+	vpa_clientset "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/client/clientset/versioned"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/test/e2e/framework"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+var _ = updaterE2eDescribe("Updater", func() {
+	f := framework.NewDefaultFramework("vertical-pod-autoscaling")
+
+	ginkgo.It("restarts pods", func() {
+
+		ginkgo.By("Setting up a hamster deployment")
+		c := f.ClientSet
+		ns := f.Namespace.Name
+
+		cpuQuantity := parseQuantityOrDie("100m")
+		memoryQuantity := parseQuantityOrDie("100Mi")
+
+		d := hamsterDeployment(f, cpuQuantity, memoryQuantity)
+		d, err := c.ExtensionsV1beta1().Deployments(ns).Create(d)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		err = framework.WaitForDeploymentComplete(c, d)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		podList, err := framework.GetPodsForDeployment(c, d)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		ginkgo.By("Setting up a VPA CRD")
+		config, err := framework.LoadConfig()
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		vpaCRD := newVPA(f, "hamster-vpa", &metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"app": "hamster",
+			},
+		})
+
+		newCPUQuantity, err := resource.ParseQuantity("200m")
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		vpaCRD.Status.Recommendation.ContainerRecommendations = []vpa_types.RecommendedContainerResources{
+			{
+				Name:   "hamster",
+				Target: apiv1.ResourceList{apiv1.ResourceCPU: newCPUQuantity},
+			},
+		}
+
+		vpaClientSet := vpa_clientset.NewForConfigOrDie(config)
+		vpaClient := vpaClientSet.PocV1alpha1()
+		_, err = vpaClient.VerticalPodAutoscalers(ns).Create(vpaCRD)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		ginkgo.By("Waiting for pods to be restarted")
+
+		err = waitForPodSetChangedInDeployment(c, d, podList)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	})
+})
+
+func makePodSet(pods *apiv1.PodList) map[string]bool {
+	result := make(map[string]bool)
+	for _, p := range pods.Items {
+		result[p.Name] = true
+	}
+	return result
+}
+
+func waitForPodSetChangedInDeployment(c clientset.Interface, deployment *extensions.Deployment, podList *apiv1.PodList) error {
+	initialPodSet := makePodSet(podList)
+
+	err := wait.PollImmediate(pollInterval, pollTimeout, func() (bool, error) {
+		var err error
+		currentPodList, err := framework.GetPodsForDeployment(c, deployment)
+		if err != nil {
+			return false, err
+		}
+
+		currentPodSet := makePodSet(currentPodList)
+
+		return !reflect.DeepEqual(initialPodSet, currentPodSet), nil
+
+	})
+
+	if err != nil {
+		return fmt.Errorf("Waiting for set of pods changed in %v: %v", deployment.Name, err)
+	}
+	return nil
+}
