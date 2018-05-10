2c048a7207f77af442a89776cb0ce6399544fcd2
Łukasz Osipiuk <lukaszos@google.com>
2018-05-10 16:45:09 +0200

    Move commons structs from scale_up_test.go to scale_test_common.go

---
+++ cluster-autoscaler/core/scale_test_common.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package core
+
+import (
+	"k8s.io/autoscaler/cluster-autoscaler/context"
+)
+
+type nodeConfig struct {
+	name   string
+	cpu    int64
+	memory int64
+	ready  bool
+	group  string
+}
+
+type podConfig struct {
+	name   string
+	cpu    int64
+	memory int64
+	node   string
+}
+
+type scaleTestConfig struct {
+	nodes                []nodeConfig
+	pods                 []podConfig
+	extraPods            []podConfig
+	expectedScaleUp      string
+	expectedScaleUpGroup string
+	expectedScaleDowns   []string
+	options              context.AutoscalingOptions
+}
