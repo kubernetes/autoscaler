715dcb0d657cce2d086b13553c5d4191d0867bf2
Joachim Bartosik <jbartosik@google.com>
2018-06-25 18:45:43 +0200

    Support labels in TestCloudProvider.

--- cluster-autoscaler/cloudprovider/test/test_cloud_provider.go
+++ cluster-autoscaler/cloudprovider/test/test_cloud_provider.go
@@ -147,6 +147,8 @@ func (tcp *TestCloudProvider) NewNodeGroup(machineType string, labels map[string
 		exist:           false,
 		autoprovisioned: true,
 		machineType:     machineType,
+		labels:          labels,
+		taints:          taints,
 	}, nil
 }
 
@@ -222,6 +224,8 @@ type TestNodeGroup struct {
 	exist           bool
 	autoprovisioned bool
 	machineType     string
+	labels          map[string]string
+	taints          []apiv1.Taint
 }
 
 // MaxSize returns maximum size of the node group.
@@ -372,4 +376,14 @@ func (tng *TestNodeGroup) TemplateNodeInfo() (*schedulercache.NodeInfo, error) {
 		return nil, fmt.Errorf("No template declared for %s", tng.id)
 	}
 	return template, nil
+}
+
+// Labels returns labels passed to the test node group when it was created.
+func (tng *TestNodeGroup) Labels() map[string]string {
+	return tng.labels
+}
+
+// Taints returns taintspassed to the test node group when it was created.
+func (tng *TestNodeGroup) Taints() []apiv1.Taint {
+	return tng.taints
 }
