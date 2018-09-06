e33cb20fb55805b970538c559304e7191af66ead
Karol Gołąb <kgolab@google.com>
2018-09-06 11:35:02 +0200

    Update godeps to include k/k/pkg/client/metrics/prometheus import.

--- cluster-autoscaler/vendor/k8s.io/kubernetes/pkg/kubelet/kubelet_network_others.go
+++ cluster-autoscaler/vendor/k8s.io/kubernetes/pkg/kubelet/kubelet_network_others.go
@@ -1,5 +1,7 @@
+// +build !linux
+
 /*
-Copyright 2016 The Kubernetes Authors.
+Copyright 2018 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,6 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package
+package kubelet
 
-package componentconfig
+// Do nothing.
+func (kl *Kubelet) syncNetworkUtil() {}
