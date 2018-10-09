33960659fc30b6f0cec6d0cbd39169de9bd23581
Karol Gołąb <kgolab@google.com>
2018-10-09 10:51:48 +0200

    Keep small-weight samples for at least MemoryAggregationWindowLength

--- vertical-pod-autoscaler/pkg/recommender/model/aggregations_config.go
+++ vertical-pod-autoscaler/pkg/recommender/model/aggregations_config.go
@@ -52,12 +52,20 @@ var (
 	CPUHistogramDecayHalfLife = time.Hour * 24
 )
 
+const (
+	// minSampleWeight is the minimal weight of any sample (prior to including decaying factor)
+	minSampleWeight = 0.1
+	// epsilon is the minimal weight kept in histograms, it should be small enough that old samples
+	// (just inside MemoryAggregationWindowLength) added with minSampleWeight are still kept
+	epsilon = 0.001 * minSampleWeight
+)
+
 func cpuHistogramOptions() util.HistogramOptions {
 	// CPU histograms use exponential bucketing scheme with the smallest bucket
 	// size of 0.01 core, max of 1000.0 cores and the relative error of HistogramRelativeError.
 	//
-	// When parametes below are changed SupportedCheckpointVersion has to be bumped.
-	options, err := util.NewExponentialHistogramOptions(1000.0, 0.01, 1.+HistogramBucketSizeGrowth, 0.1)
+	// When parameters below are changed SupportedCheckpointVersion has to be bumped.
+	options, err := util.NewExponentialHistogramOptions(1000.0, 0.01, 1.+HistogramBucketSizeGrowth, epsilon)
 	if err != nil {
 		panic("Invalid CPU histogram options") // Should not happen.
 	}
@@ -68,8 +76,8 @@ func memoryHistogramOptions() util.HistogramOptions {
 	// Memory histograms use exponential bucketing scheme with the smallest
 	// bucket size of 10MB, max of 1TB and the relative error of HistogramRelativeError.
 	//
-	// When parametes below are changed SupportedCheckpointVersion has to be bumped.
-	options, err := util.NewExponentialHistogramOptions(1e12, 1e7, 1.+HistogramBucketSizeGrowth, 0.1)
+	// When parameters below are changed SupportedCheckpointVersion has to be bumped.
+	options, err := util.NewExponentialHistogramOptions(1e12, 1e7, 1.+HistogramBucketSizeGrowth, epsilon)
 	if err != nil {
 		panic("Invalid memory histogram options") // Should not happen.
 	}
