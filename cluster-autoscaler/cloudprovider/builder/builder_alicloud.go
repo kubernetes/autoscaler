d1c1f2be67cabb47269d2dc5d99ba24601442059
ringtail <zhongwei.lzw@alibaba-inc.com>
2018-10-23 20:23:34 +0800

    Add Alibaba Cloud Provider support with no vendor (#1309)
    * Add Alibaba Cloud Provider
    
    * Add Alibaba Cloud Provider Documents
    
    * Add Alibaba Cloud Provider Examples
    
    * Remove sdk test cases
    
    * fix conflicts in cloud-provider builder
    
    * remove LICENSE in cloudprovider

--- cluster-autoscaler/cloudprovider/builder/builder_alicloud.go
+++ cluster-autoscaler/cloudprovider/builder/builder_alicloud.go
@@ -1,4 +1,4 @@
-// +build !gce,!aws,!azure,!kubemark,!alicloud
+// +build alicloud
 
 /*
 Copyright 2018 The Kubernetes Authors.
@@ -21,37 +21,22 @@ package builder
 import (
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/alicloud"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/aws"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/azure"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gke"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 )
 
 // AvailableCloudProviders supported by the cloud provider builder.
 var AvailableCloudProviders = []string{
-	aws.ProviderName,
-	azure.ProviderName,
-	gce.ProviderNameGCE,
-	gke.ProviderNameGKE,
 	alicloud.ProviderName,
 }
 
-// DefaultCloudProvider is GCE.
-const DefaultCloudProvider = gce.ProviderNameGCE
+// DefaultCloudProvider for alicloud-only build is alicloud.
+const DefaultCloudProvider = alicloud.ProviderName
 
 func buildCloudProvider(opts config.AutoscalingOptions, do cloudprovider.NodeGroupDiscoveryOptions, rl *cloudprovider.ResourceLimiter) cloudprovider.CloudProvider {
 	switch opts.CloudProviderName {
-	case gce.ProviderNameGCE:
-		return gce.BuildGCE(opts, do, rl)
-	case gke.ProviderNameGKE:
-		return gke.BuildGKE(opts, do, rl)
-	case aws.ProviderName:
-		return aws.BuildAWS(opts, do, rl)
-	case azure.ProviderName:
-		return azure.BuildAzure(opts, do, rl)
 	case alicloud.ProviderName:
 		return alicloud.BuildAlicloud(opts, do, rl)
 	}
+
 	return nil
 }
