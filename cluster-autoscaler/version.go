d1d348913905ab6dd98261a80072388cb1e6cf46
Łukasz Osipiuk <lukaszos@google.com>
2018-11-28 16:51:59 +0100

    Cluster Autoscaler release 1.13.1

--- cluster-autoscaler/version.go
+++ cluster-autoscaler/version.go
@@ -17,4 +17,4 @@ limitations under the License.
 package main
 
 // ClusterAutoscalerVersion contains version of CA.
-const ClusterAutoscalerVersion = "1.13.0"
+const ClusterAutoscalerVersion = "1.13.1"
