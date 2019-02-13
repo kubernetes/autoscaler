ee64e3e491afd147606d298928b7056aacc1f56f
lsytj0413 <511121939@qq.com>
2019-02-13 11:44:54 +0800

    fix(expander): avoid panic when random expander

--- cluster-autoscaler/expander/mostpods/mostpods_test.go
+++ cluster-autoscaler/expander/mostpods/mostpods_test.go
@@ -21,25 +21,23 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	apiv1 "k8s.io/api/core/v1"
+
 	"k8s.io/autoscaler/cluster-autoscaler/expander"
 )
 
 func TestMostPods(t *testing.T) {
-	eo0 := expander.Option{Debug: "EO0"}
 	e := NewStrategy()
 
+	eo0 := expander.Option{Debug: "EO0"}
 	ret := e.BestOption([]expander.Option{eo0}, nil)
 	assert.Equal(t, *ret, eo0)
 
 	eo1 := expander.Option{Debug: "EO1", Pods: []*apiv1.Pod{nil}}
-
 	ret = e.BestOption([]expander.Option{eo0, eo1}, nil)
 	assert.Equal(t, *ret, eo1)
 
 	eo1b := expander.Option{Debug: "EO1b", Pods: []*apiv1.Pod{nil}}
-
 	ret = e.BestOption([]expander.Option{eo0, eo1, eo1b}, nil)
 	assert.NotEqual(t, *ret, eo0)
-
 	assert.True(t, assert.ObjectsAreEqual(*ret, eo1) || assert.ObjectsAreEqual(*ret, eo1b))
 }
