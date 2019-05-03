431c0dfe5d4a68174f7ca002195f75c084f59452
Marc Sensenich <sensenichm91@gmail.com>
2019-05-02 20:49:13 -0400

    Allow for node template labels to come from the VMSS's tags

--- cluster-autoscaler/cloudprovider/azure/azure_util.go
+++ cluster-autoscaler/cloudprovider/azure/azure_util.go
@@ -76,6 +76,9 @@ const (
 	k8sWindowsVMAgentPoolPrefixIndex       = 1
 	k8sWindowsVMAgentOrchestratorNameIndex = 2
 	k8sWindowsVMAgentPoolInfoIndex         = 3
+
+	nodeLabelTagName = "k8s.io|cluster-autoscaler|node-template|label|"
+	nodeTaintTagName = "k8s.io|cluster-autoscaler|node-template|taint|"
 )
 
 var (
