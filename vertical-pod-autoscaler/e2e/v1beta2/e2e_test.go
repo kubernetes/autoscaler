77fca3a3cdbb9251d9b1681ab8e674eec7a923e9
Beata Skiba <bskiba@google.com>
2019-05-09 09:51:24 +0200

    Initialize flags for e2e

--- vertical-pod-autoscaler/e2e/v1beta2/e2e_test.go
+++ vertical-pod-autoscaler/e2e/v1beta2/e2e_test.go
@@ -21,14 +21,17 @@ import (
 	"os"
 	"testing"
 
+	"k8s.io/kubernetes/test/e2e/framework"
 	"k8s.io/kubernetes/test/e2e/framework/viperconfig"
 )
 
 func init() {
+	framework.HandleFlags()
 	if err := viperconfig.ViperizeFlags("", "e2e"); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	framework.AfterReadingAllFlags(&framework.TestContext)
 }
 
 func TestVpaE2E(t *testing.T) {
