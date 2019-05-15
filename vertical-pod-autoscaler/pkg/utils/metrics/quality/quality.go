f32a1a63e328f3b4de98a969e3c7517c63627d0e
Beata Skiba <bskiba@google.com>
2019-05-15 14:47:32 +0200

    Record more quality metrics
    - Absolute diff between usage and recommendation
    - Recommendation as observed on receiving a usage sample

--- vertical-pod-autoscaler/pkg/utils/metrics/quality/quality.go
+++ vertical-pod-autoscaler/pkg/utils/metrics/quality/quality.go
@@ -18,16 +18,26 @@ limitations under the License.
 package quality
 
 import (
+	"fmt"
 	"strconv"
 
 	corev1 "k8s.io/api/core/v1"
+	vpa_types "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1beta2"
 	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/utils/metrics"
+	"k8s.io/klog"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 const (
 	metricsNamespace = metrics.TopMetricsNamespace + "quality"
+)
+
+var (
+	// Buckets between 0.01 and 655.36
+	cpuBuckets = prometheus.ExponentialBuckets(0.01, 2., 17)
+	// Buckets between 1MB and
+	memoryBuckets = prometheus.ExponentialBuckets(1e6, 2., 17)
 )
 
 var (
@@ -38,14 +48,46 @@ var (
 			Help:      "Diffs between usage and recommendation, normalized by recommendation value",
 			Buckets: []float64{-1., -.75, -.5, -.25, -.1, -.05, -0.025, -.01, -.005, -0.0025, -.001, 0.,
 				.001, .0025, .005, .01, .025, .05, .1, .25, .5, .75, 1., 2.5, 5., 10., 25., 50., 100.},
-		}, []string{"resource", "is_oom"},
+		}, []string{"update_mode", "resource", "is_oom"},
 	)
 	usageMissingRecommendationCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Namespace: metricsNamespace,
 			Name:      "usage_sample_missing_recommendation_count",
-			Help:      "Count of usage samples when a recommendation should be present but is missing.",
-		}, []string{"resource", "is_oom"},
+			Help:      "Count of usage samples when a recommendation should be present but is missing",
+		}, []string{"update_mode", "resource", "is_oom"},
+	)
+	cpuUsageRecommendationDiff = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: metricsNamespace,
+			Name:      "cpu_usage_recommendation_diffs_cores",
+			Help:      "Absolute diffs between usage and recommendation for CPU",
+			Buckets:   prependNegativeBuckets(cpuBuckets),
+		}, []string{"update_mode", "recommendation_missing"},
+	)
+	memoryUsageRecommendationDiff = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: metricsNamespace,
+			Name:      "mem_usage_recommendation_diffs_bytes",
+			Help:      "Absolute diffs between usage and recommendation for memory",
+			Buckets:   prependNegativeBuckets(memoryBuckets),
+		}, []string{"update_mode", "recommendation_missing", "is_oom"},
+	)
+	cpuRecommendations = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: metricsNamespace,
+			Name:      "cpu_recommendations_cores",
+			Help:      "CPU recommendation values as observed on recorded usage sample",
+			Buckets:   cpuBuckets,
+		}, []string{"update_mode"},
+	)
+	memoryRecommendations = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: metricsNamespace,
+			Name:      "mem_recommendations_bytes",
+			Help:      "Memory recommendation values as observed on recorded usage sample",
+			Buckets:   memoryBuckets,
+		}, []string{"update_mode", "is_oom"},
 	)
 )
 
@@ -53,17 +95,81 @@ var (
 func Register() {
 	prometheus.MustRegister(usageRecommendationRelativeDiff)
 	prometheus.MustRegister(usageMissingRecommendationCounter)
+	prometheus.MustRegister(cpuUsageRecommendationDiff)
+	prometheus.MustRegister(memoryUsageRecommendationDiff)
+	prometheus.MustRegister(cpuRecommendations)
+	prometheus.MustRegister(memoryRecommendations)
 }
 
-// ObserveUsageRecommendationRelativeDiff records relative diff between usage and
+// observeUsageRecommendationRelativeDiff records relative diff between usage and
 // recommendation if recommendation has a positive value.
-func ObserveUsageRecommendationRelativeDiff(usage, recommendation float64, isOOM bool, resource corev1.ResourceName) {
+func observeUsageRecommendationRelativeDiff(usage, recommendation float64, isOOM bool, resource corev1.ResourceName, updateMode *vpa_types.UpdateMode) {
 	if recommendation > 0 {
-		usageRecommendationRelativeDiff.WithLabelValues(string(resource), strconv.FormatBool(isOOM)).Observe((usage - recommendation) / recommendation)
+		usageRecommendationRelativeDiff.WithLabelValues(updateModeToString(updateMode), string(resource), strconv.FormatBool(isOOM)).Observe((usage - recommendation) / recommendation)
 	}
 }
 
-// ObserveMissingRecommendation counts usage samples with missing recommendations.
-func ObserveMissingRecommendation(isOOM bool, resource corev1.ResourceName) {
-	usageMissingRecommendationCounter.WithLabelValues(string(resource), strconv.FormatBool(isOOM)).Inc()
+// observeMissingRecommendation counts usage samples with missing recommendations.
+func observeMissingRecommendation(isOOM bool, resource corev1.ResourceName, updateMode *vpa_types.UpdateMode) {
+	usageMissingRecommendationCounter.WithLabelValues(updateModeToString(updateMode), string(resource), strconv.FormatBool(isOOM)).Inc()
+}
+
+// observeUsageRecommendationDiff records absolute diff between usage and
+// recommendation.
+func observeUsageRecommendationDiff(usage, recommendation float64, isRecommendationMissing, isOOM bool, resource corev1.ResourceName, updateMode *vpa_types.UpdateMode) {
+	switch resource {
+	case corev1.ResourceCPU:
+		cpuUsageRecommendationDiff.WithLabelValues(updateModeToString(updateMode), strconv.FormatBool(isRecommendationMissing)).Observe(usage - recommendation)
+	case corev1.ResourceMemory:
+		memoryUsageRecommendationDiff.WithLabelValues(updateModeToString(updateMode),
+			strconv.FormatBool(isRecommendationMissing), strconv.FormatBool(isOOM)).Observe(usage - recommendation)
+	default:
+		klog.Warningf("Unknown resource: %v", resource)
+	}
+}
+
+// observeRecommendation records the value of recommendation.
+func observeRecommendation(recommendation float64, isOOM bool, resource corev1.ResourceName, updateMode *vpa_types.UpdateMode) {
+	switch resource {
+	case corev1.ResourceCPU:
+		cpuRecommendations.WithLabelValues(updateModeToString(updateMode)).Observe(recommendation)
+	case corev1.ResourceMemory:
+		memoryRecommendations.WithLabelValues(updateModeToString(updateMode), strconv.FormatBool(isOOM)).Observe(recommendation)
+	default:
+		klog.Warningf("Unknown resource: %v", resource)
+	}
+}
+
+// ObserveQualityMetrics records all quality metrics that we can derive from usage and recommendation.
+func ObserveQualityMetrics(usage, recommendation float64, isOOM bool, resource corev1.ResourceName, updateMode *vpa_types.UpdateMode) {
+	observeRecommendation(recommendation, isOOM, resource, updateMode)
+	observeUsageRecommendationDiff(usage, recommendation, false, isOOM, resource, updateMode)
+	observeUsageRecommendationRelativeDiff(usage, recommendation, isOOM, resource, updateMode)
+}
+
+// ObserveQualityMetricsRecommendationMissing records all quality metrics that we can derive from usage when recommendation is missing.
+func ObserveQualityMetricsRecommendationMissing(usage float64, isOOM bool, resource corev1.ResourceName, updateMode *vpa_types.UpdateMode) {
+	observeMissingRecommendation(isOOM, resource, updateMode)
+	observeUsageRecommendationDiff(usage, 0, true, isOOM, resource, updateMode)
+}
+
+func prependNegativeBuckets(buckets []float64) []float64 {
+	bucketCount := len(buckets)
+	result := make([]float64, bucketCount*2+1)
+	result[bucketCount] = 0
+	for i, bucket := range buckets {
+		if bucket < 0 {
+			panic(fmt.Sprintf("Positive buckets expected. Got: %v", bucket))
+		}
+		result[bucketCount-i-1] = -bucket
+		result[bucketCount+i+1] = bucket
+	}
+	return result
+}
+
+func updateModeToString(updateMode *vpa_types.UpdateMode) string {
+	if updateMode == nil {
+		return ""
+	}
+	return string(*updateMode)
 }
