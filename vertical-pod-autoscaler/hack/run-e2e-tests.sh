2d5c9e34e5b5c0ff7bb8d42d5090b3554cb58296
Beata Skiba <bskiba@google.com>
2019-11-22 17:23:46 +0100

    Fix running e2e tests.
    Use vendor directory to run tests, since we rely on generated data.

--- vertical-pod-autoscaler/hack/run-e2e-tests.sh
+++ vertical-pod-autoscaler/hack/run-e2e-tests.sh
@@ -42,12 +42,14 @@ fi
 
 SUITE=$1
 
+export GO111MODULE=on
+
 case ${SUITE} in
   recommender|updater|admission-controller|actuation|full-vpa)
     export KUBECONFIG=$HOME/.kube/config
-    go test ${SCRIPT_ROOT}/e2e/v1beta2/*go -v -test.timeout=60m  --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
+    go test -mod vendor ${SCRIPT_ROOT}/e2e/v1beta2/*go -v -test.timeout=60m  --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
     FIRST_RESULT=$?
-    go test ${SCRIPT_ROOT}/e2e/v1/*go -v -test.timeout=60m  --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
+    go test -mod vendor ${SCRIPT_ROOT}/e2e/v1/*go -v -test.timeout=60m  --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
     echo First test result: ${FIRST_RESULT}
     if [ $FIRST_RESULT -gt 0 ]; then
       echo "Please check first \"go test\" logs!"
