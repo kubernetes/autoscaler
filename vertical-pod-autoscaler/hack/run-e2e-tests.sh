40aa850b126d49f8ce0e865be3c9d09c6fc105c8
Beata Skiba <bskiba@google.com>
2019-11-27 22:27:36 +0100

    Sync e2e_test file with k/k master
    Without this change test flags are not registered and this fails with go
    1.13

--- vertical-pod-autoscaler/hack/run-e2e-tests.sh
+++ vertical-pod-autoscaler/hack/run-e2e-tests.sh
@@ -48,9 +48,9 @@ case ${SUITE} in
   recommender|updater|admission-controller|actuation|full-vpa)
     export KUBECONFIG=$HOME/.kube/config
     pushd ${SCRIPT_ROOT}/e2e
-    go test -mod vendor ./v1beta2/*go -v --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
+    go test -mod vendor ./v1beta2/*go -v --test.timeout=60m --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
     FIRST_RESULT=$?
-    go test -mod vendor ./v1/*go -v --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
+    go test -mod vendor ./v1/*go -v --test.timeout=60m --args --ginkgo.v=true --ginkgo.focus="\[VPA\] \[${SUITE}\]" --report-dir=/workspace/_artifacts --disable-log-dump
     popd
     echo First test result: ${FIRST_RESULT}
     if [ $FIRST_RESULT -gt 0 ]; then
