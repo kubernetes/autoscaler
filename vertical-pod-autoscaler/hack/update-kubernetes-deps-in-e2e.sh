6e00c7bc587c87219a307e35d5d01a9bed102f33
Beata Skiba <bskiba@google.com>
2019-11-27 23:25:08 +0100

    Add generating bindata to update deps script for e2e

--- vertical-pod-autoscaler/hack/update-kubernetes-deps-in-e2e.sh
+++ vertical-pod-autoscaler/hack/update-kubernetes-deps-in-e2e.sh
@@ -23,16 +23,17 @@ set -eou errexit pipefail nounset
 
 K8S_TAG=${K8S_TAG:-v1.16.2}
 K8S_TAG=${K8S_TAG#v}
+K8S_FORK="git@github.com:kubernetes/kubernetes.git"
 
 export GO111MODULE=on
 
 function update_deps() {
-    # list staged k8s.io repos   
+    # list staged k8s.io repos
     MODS=($(
         curl -sS https://raw.githubusercontent.com/kubernetes/kubernetes/v${K8S_TAG}/go.mod |
         sed -n 's|.*k8s.io/\(.*\) => ./staging/src/k8s.io/.*|k8s.io/\1|p'
     ))
-    
+
     # get matching tag for each staged k8s.io repo
     for MOD in "${MODS[@]}"; do
         V=$(
@@ -41,9 +42,25 @@ function update_deps() {
         )
         go mod edit "-replace=${MOD}=${MOD}@${V}"
     done
-    
-    # update k8s.io/kubernetes to desired tag
-    go get "k8s.io/kubernetes@v${K8S_TAG}"
+}
+
+function generate_bindata() {
+    WORK_DIR="$(mktemp -d /tmp/vpa-bindata.XXXX)"
+    echo "operating in ${WORK_DIR}"
+    echo Getting k8s
+    K8S_REPO=${WORK_DIR}/kubernetes
+    pushd ${WORK_DIR}
+    git clone ${K8S_FORK} ${K8S_REPO}
+
+    pushd ${K8S_REPO}
+    echo "Syncing k8s to ${K8S_TAG}"
+    git checkout "v${K8S_TAG}"
+    echo "Generating k8s bindata"
+    hack/generate-bindata.sh
+    popd
+    popd
+
+    cp ${WORK_DIR}/kubernetes/test/e2e/generated/* ./vendor/k8s.io/kubernetes/test/e2e/generated
 }
 
 
@@ -53,12 +70,19 @@ function update_deps() {
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
     # cd to e2e tests
-    cd $DIR/../e2e
+    cd ${DIR}/../e2e
 
-    # update k8s deps in e2e
+    echo "Updating VPA e2e dependencies to k8s ${K8S_TAG}"
     update_deps
 
+    echo "Updating k8s to ${K8S_TAG}"
+    go get "k8s.io/kubernetes@v${K8S_TAG}"
+
+    echo "Running go mod tidy and vendoring deps"
     # tidy and vendor modules
     go mod tidy
     go mod vendor
+
+    echo "Generating k8s bindata"
+    generate_bindata
 )
