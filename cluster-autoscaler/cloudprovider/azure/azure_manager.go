00b29417154b35df586a20ace9420e68428537ed
t-qini <t-qini@microsoft.com>
2019-12-27 12:13:16 +0800

    Generate azure instance types.

--- cluster-autoscaler/cloudprovider/azure/azure_manager.go
+++ cluster-autoscaler/cloudprovider/azure/azure_manager.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//go:generate go run azure_instance_types/gen.go
+
 package azure
 
 import (
