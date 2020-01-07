72f896b347e1aca291afc20c1421aa1235c5a865
Marwan Ahmed <marwanad@microsoft.com>
2020-01-06 17:59:59 -0800

    add test cases for fetching explicit asgs and auto asgs

--- cluster-autoscaler/cloudprovider/azure/azure_manager_test.go
+++ cluster-autoscaler/cloudprovider/azure/azure_manager_test.go
@@ -17,9 +17,11 @@ limitations under the License.
 package azure
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-10-01/compute"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
 )
@@ -64,4 +66,252 @@ func TestCreateAzureManagerValidConfig(t *testing.T) {
 func TestCreateAzureManagerInvalidConfig(t *testing.T) {
 	_, err := CreateAzureManager(strings.NewReader(invalidAzureCfg), cloudprovider.NodeGroupDiscoveryOptions{})
 	assert.Error(t, err, "failed to unmarshal config body")
+}
+
+func TestFetchExplicitAsgs(t *testing.T) {
+	min, max, name := 1, 15, "test-asg"
+	ngdo := cloudprovider.NodeGroupDiscoveryOptions{
+		NodeGroupSpecs: []string{
+			fmt.Sprintf("%d:%d:%s", min, max, name),
+		},
+	}
+
+	manager := newTestAzureManager(t)
+	manager.fetchExplicitAsgs(ngdo.NodeGroupSpecs)
+
+	asgs := manager.asgCache.get()
+	assert.Equal(t, 1, len(asgs))
+	assert.Equal(t, name, asgs[0].Id())
+	assert.Equal(t, min, asgs[0].MinSize())
+	assert.Equal(t, max, asgs[0].MaxSize())
+}
+
+func TestParseLabelAutoDiscoverySpecs(t *testing.T) {
+	testCases := []struct {
+		name        string
+		specs       []string
+		expected    []labelAutoDiscoveryConfig
+		expectedErr bool
+	}{
+		{
+			name: "ValidSpec",
+			specs: []string{
+				"label:cluster-autoscaler-enabled=true,cluster-autoscaler-name=fake-cluster",
+				"label:test-tag=test-value,another-test-tag=another-test-value",
+			},
+			expected: []labelAutoDiscoveryConfig{
+				{Selector: map[string]string{"cluster-autoscaler-enabled": "true", "cluster-autoscaler-name": "fake-cluster"}},
+				{Selector: map[string]string{"test-tag": "test-value", "another-test-tag": "another-test-value"}},
+			},
+		},
+		{
+			name:        "MissingAutoDiscoverLabel",
+			specs:       []string{"test-tag=test-value,another-test-tag"},
+			expectedErr: true,
+		},
+		{
+			name:        "InvalidAutoDiscoerLabel",
+			specs:       []string{"invalid:test-tag=test-value,another-test-tag"},
+			expectedErr: true,
+		},
+		{
+			name:        "MissingValue",
+			specs:       []string{"label:test-tag="},
+			expectedErr: true,
+		},
+		{
+			name:        "MissingKey",
+			specs:       []string{"label:=test-val"},
+			expectedErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ngdo := cloudprovider.NodeGroupDiscoveryOptions{NodeGroupAutoDiscoverySpecs: tc.specs}
+			actual, err := parseLabelAutoDiscoverySpecs(ngdo)
+			if tc.expectedErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.True(t, assert.ObjectsAreEqualValues(tc.expected, actual), "expected %#v, but found: %#v", tc.expected, actual)
+		})
+	}
+}
+
+func TestListScalesets(t *testing.T) {
+	manager := newTestAzureManager(t)
+	vmssTag := "fake-tag"
+	vmssTagValue := "fake-value"
+	vmssName := "test-vmss"
+
+	ngdo := cloudprovider.NodeGroupDiscoveryOptions{
+		NodeGroupAutoDiscoverySpecs: []string{fmt.Sprintf("label:%s=%s", vmssTag, vmssTagValue)},
+	}
+	specs, err := parseLabelAutoDiscoverySpecs(ngdo)
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name              string
+		specs             map[string]string
+		expected          []cloudprovider.NodeGroup
+		expectedErrString string
+	}{
+		{
+			name:  "ValidMinMax",
+			specs: map[string]string{"min": "5", "max": "50"},
+			expected: []cloudprovider.NodeGroup{&ScaleSet{
+				azureRef: azureRef{
+					Name: vmssName,
+				},
+				minSize: 5,
+				maxSize: 50,
+				manager: manager,
+				curSize: -1,
+			}},
+		},
+		{
+			name:              "InvalidMin",
+			specs:             map[string]string{"min": "some-invalid-string"},
+			expectedErrString: "invalid minimum size specified for vmss:",
+		},
+		{
+			name:              "NoMin",
+			specs:             map[string]string{"max": "50"},
+			expectedErrString: fmt.Sprintf("no minimum size specified for vmss: %s", vmssName),
+		},
+		{
+			name:              "InvalidMax",
+			specs:             map[string]string{"min": "5", "max": "some-invalid-string"},
+			expectedErrString: "invalid maximum size specified for vmss:",
+		},
+		{
+			name:              "NoMax",
+			specs:             map[string]string{"min": "5"},
+			expectedErrString: fmt.Sprintf("no maximum size specified for vmss: %s", vmssName),
+		},
+		{
+			name:              "MaxLessThanOne",
+			specs:             map[string]string{"min": "5", "max": "-5"},
+			expectedErrString: "maximum size must be greater than 1 node",
+		},
+		{
+			name:              "MinLessThanZero",
+			specs:             map[string]string{"min": "-4", "max": "20"},
+			expectedErrString: fmt.Sprintf("minimum size must be a non-negative number of nodes"),
+		},
+		{
+			name:              "MinGreaterThanMax",
+			specs:             map[string]string{"min": "50", "max": "5"},
+			expectedErrString: "maximum size must be greater than minimum size",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tags := make(map[string]*string)
+			tags[vmssTag] = &vmssTagValue
+			if val, ok := tc.specs["min"]; ok {
+				tags["min"] = &val
+			}
+			if val, ok := tc.specs["max"]; ok {
+				tags["max"] = &val
+			}
+
+			scaleSetClient := &VirtualMachineScaleSetsClientMock{
+				FakeStore: map[string]map[string]compute.VirtualMachineScaleSet{
+					"test": {
+						vmssName: fakeVMSSWithTags(vmssName, tags),
+					},
+				},
+			}
+
+			manager.azClient.virtualMachineScaleSetsClient = scaleSetClient
+			asgs, err := manager.listScaleSets(specs)
+			if tc.expectedErrString != "" {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tc.expectedErrString)
+				return
+			}
+			assert.NoError(t, err)
+			assert.True(t, assert.ObjectsAreEqualValues(tc.expected, asgs), "expected %#v, but found: %#v", tc.expected, asgs)
+		})
+	}
+}
+
+func TestGetFilteredAutoscalingGroupsVmss(t *testing.T) {
+	vmssName := "test-vmss"
+	vmssTag := "fake-tag"
+	vmssTagValue := "fake-value"
+	min := "1"
+	minVal := 1
+	max := "5"
+	maxVal := 5
+
+	ngdo := cloudprovider.NodeGroupDiscoveryOptions{
+		NodeGroupAutoDiscoverySpecs: []string{fmt.Sprintf("label:%s=%s", vmssTag, vmssTagValue)},
+	}
+	scaleSetClient := &VirtualMachineScaleSetsClientMock{
+		FakeStore: map[string]map[string]compute.VirtualMachineScaleSet{
+			"test": {
+				vmssName: fakeVMSSWithTags(vmssName, map[string]*string{vmssTag: &vmssTagValue, "min": &min, "max": &max}),
+			},
+		},
+	}
+
+	manager := newTestAzureManager(t)
+	manager.azClient.virtualMachineScaleSetsClient = scaleSetClient
+	specs, err := parseLabelAutoDiscoverySpecs(ngdo)
+	assert.NoError(t, err)
+
+	asgs, err := manager.getFilteredAutoscalingGroups(specs)
+	assert.NoError(t, err)
+	expectedAsgs := []cloudprovider.NodeGroup{&ScaleSet{
+		azureRef: azureRef{
+			Name: vmssName,
+		},
+		minSize: minVal,
+		maxSize: maxVal,
+		manager: manager,
+		curSize: -1,
+	}}
+	assert.True(t, assert.ObjectsAreEqualValues(expectedAsgs, asgs), "expected %#v, but found: %#v", expectedAsgs, asgs)
+}
+
+func TestFetchAutoAsgsVmss(t *testing.T) {
+	vmssName := "test-vmss"
+	vmssTag := "fake-tag"
+	vmssTagValue := "fake-value"
+	minString := "1"
+	minVal := 1
+	maxString := "5"
+	maxVal := 5
+
+	ngdo := cloudprovider.NodeGroupDiscoveryOptions{
+		NodeGroupAutoDiscoverySpecs: []string{fmt.Sprintf("label:%s=%s", vmssTag, vmssTagValue)},
+	}
+	scaleSetClient := &VirtualMachineScaleSetsClientMock{
+		FakeStore: map[string]map[string]compute.VirtualMachineScaleSet{
+			"test": {
+				vmssName: fakeVMSSWithTags(vmssName, map[string]*string{vmssTag: &vmssTagValue, "min": &minString, "max": &maxString}),
+			},
+		},
+	}
+
+	manager := newTestAzureManager(t)
+	manager.azClient.virtualMachineScaleSetsClient = scaleSetClient
+	specs, err := parseLabelAutoDiscoverySpecs(ngdo)
+	assert.NoError(t, err)
+	manager.asgAutoDiscoverySpecs = specs
+
+	// assert cache is empty before fetching auto asgs
+	asgs := manager.asgCache.get()
+	assert.Equal(t, 0, len(asgs))
+
+	manager.fetchAutoAsgs()
+	asgs = manager.asgCache.get()
+	assert.Equal(t, 1, len(asgs))
+	assert.Equal(t, vmssName, asgs[0].Id())
+	assert.Equal(t, minVal, asgs[0].MinSize())
+	assert.Equal(t, maxVal, asgs[0].MaxSize())
 }
