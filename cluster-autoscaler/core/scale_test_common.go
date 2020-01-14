f68a3a0b66b0bcd4b2a377666a0a7bfb3b7803a1
Łukasz Osipiuk <lukaszos@google.com>
2020-01-14 14:06:38 +0100

    Add ClusterSnapshot to AutoscalingContext

--- cluster-autoscaler/core/scale_test_common.go
+++ cluster-autoscaler/core/scale_test_common.go
@@ -154,6 +154,7 @@ func NewScaleTestAutoscalingContext(
 	if err != nil {
 		return context.AutoscalingContext{}, err
 	}
+	clusterSnapshot := simulator.NewBasicClusterSnapshot()
 	return context.AutoscalingContext{
 		AutoscalingOptions: options,
 		AutoscalingKubeClients: context.AutoscalingKubeClients{
@@ -164,6 +165,7 @@ func NewScaleTestAutoscalingContext(
 		},
 		CloudProvider:      provider,
 		PredicateChecker:   predicateChecker,
+		ClusterSnapshot:    clusterSnapshot,
 		ExpanderStrategy:   random.NewStrategy(),
 		EstimatorBuilder:   estimatorBuilder,
 		ProcessorCallbacks: processorCallbacks,
