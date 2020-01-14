4221524fd91c1487fd2700e156fdaa38b7512885
Łukasz Osipiuk <lukaszos@google.com>
2020-01-14 15:13:32 +0100

    Remove OldBinpackingEstimator

--- cluster-autoscaler/estimator/estimator.go
+++ cluster-autoscaler/estimator/estimator.go
@@ -21,19 +21,16 @@ import (
 
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/autoscaler/cluster-autoscaler/simulator"
-	"k8s.io/klog"
 	schedulernodeinfo "k8s.io/kubernetes/pkg/scheduler/nodeinfo"
 )
 
 const (
 	// BinpackingEstimatorName is the name of binpacking estimator.
 	BinpackingEstimatorName = "binpacking"
-	// OldBinpackingEstimatorName is the name of the older binpacking estimator.
-	OldBinpackingEstimatorName = "oldbinpacking"
 )
 
 // AvailableEstimators is a list of available estimators.
-var AvailableEstimators = []string{BinpackingEstimatorName, OldBinpackingEstimatorName}
+var AvailableEstimators = []string{BinpackingEstimatorName}
 
 // Estimator calculates the number of nodes of given type needed to schedule pods.
 type Estimator interface {
@@ -49,12 +46,6 @@ func NewEstimatorBuilder(name string) (EstimatorBuilder, error) {
 	case BinpackingEstimatorName:
 		return func(predicateChecker *simulator.PredicateChecker) Estimator {
 			return NewBinpackingNodeEstimator(predicateChecker)
-		}, nil
-	// Deprecated.
-	case OldBinpackingEstimatorName:
-		klog.Warning(oldBinPackingEstimatorDeprecationMessage)
-		return func(predicateChecker *simulator.PredicateChecker) Estimator {
-			return NewOldBinpackingNodeEstimator(predicateChecker)
 		}, nil
 	}
 	return nil, fmt.Errorf("unknown estimator: %s", name)
