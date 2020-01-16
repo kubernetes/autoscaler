9d62bc94485ed0a8257b807ffe81af7418922926
Łukasz Osipiuk <lukaszos@google.com>
2020-01-16 17:43:54 +0100

    Simplify PodListProcessor interface

--- cluster-autoscaler/processors/pods/pod_list_processor_test.go
+++ cluster-autoscaler/processors/pods/pod_list_processor_test.go
@@ -25,21 +25,14 @@ import (
 	. "k8s.io/autoscaler/cluster-autoscaler/utils/test"
 )
 
-func TestPodListProcessor(t *testing.T) {
+func TestDefaultUnschedulablePodListProcessor(t *testing.T) {
 	context := &context.AutoscalingContext{}
 	p1 := BuildTestPod("p1", 40, 0)
-	p2 := BuildTestPod("p2", 400, 0)
-	n1 := BuildTestNode("n1", 100, 1000)
-	n2 := BuildTestNode("n1", 100, 1000)
 	unschedulablePods := []*apiv1.Pod{p1}
-	allScheduledPods := []*apiv1.Pod{p2}
-	allNodes := []*apiv1.Node{n1, n2}
-	readyNodes := []*apiv1.Node{n1, n2}
 	podListProcessor := NewDefaultPodListProcessor()
-	gotUnschedulablePods, gotAllScheduled, err := podListProcessor.Process(context, unschedulablePods, allScheduledPods, allNodes, readyNodes, []*apiv1.Node{})
-	if len(gotUnschedulablePods) != 1 || len(gotAllScheduled) != 1 || err != nil {
-		t.Errorf("Error podListProcessor.Process() = %v, %v, %v want %v, %v, nil ",
-			gotUnschedulablePods, gotAllScheduled, err, unschedulablePods, allScheduledPods)
+	gotUnschedulablePods, err := podListProcessor.Process(context, unschedulablePods)
+	if len(gotUnschedulablePods) != 1 || err != nil {
+		t.Errorf("Error podListProcessor.Process() = %v,%v want %v, nil ",
+			gotUnschedulablePods, err, unschedulablePods)
 	}
-
 }
