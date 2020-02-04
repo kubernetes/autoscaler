b00fca82c99ada7542105dd257a573ef442cda11
Łukasz Osipiuk <lukaszos@google.com>
2020-02-04 14:35:08 +0100

    Remove InitializeClusterSnapshotNoError function

--- cluster-autoscaler/simulator/test_utils.go
+++ cluster-autoscaler/simulator/test_utils.go
@@ -24,32 +24,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// InitializeClusterSnapshot clears cluster snapshot and then initializes it with given set of nodes and pods.
+// InitializeClusterSnapshotOrDie clears cluster snapshot and then initializes it with given set of nodes and pods.
 // Both Spec.NodeName and Status.NominatedNodeName are used when simulating scheduling pods.
-func InitializeClusterSnapshot(
+func InitializeClusterSnapshotOrDie(
 	t *testing.T,
 	snapshot ClusterSnapshot,
 	nodes []*apiv1.Node,
 	pods []*apiv1.Pod) {
-	initializeClusterSnapshot(t, snapshot, nodes, pods, true)
-}
-
-// InitializeClusterSnapshotNoError behaves like InitializeClusterSnapshot but does not report error from AddPod
-func InitializeClusterSnapshotNoError(
-	t *testing.T,
-	snapshot ClusterSnapshot,
-	nodes []*apiv1.Node,
-	pods []*apiv1.Pod) {
-	initializeClusterSnapshot(t, snapshot, nodes, pods, false)
-}
-
-func initializeClusterSnapshot(
-	t *testing.T,
-	snapshot ClusterSnapshot,
-	nodes []*apiv1.Node,
-	pods []*apiv1.Pod,
-	reportErrors bool) {
-
 	var err error
 
 	err = snapshot.Clear()
@@ -63,14 +44,10 @@ func initializeClusterSnapshot(
 	for _, pod := range pods {
 		if pod.Spec.NodeName != "" {
 			err = snapshot.AddPod(pod, pod.Spec.NodeName)
-			if reportErrors {
-				assert.NoError(t, err, "error while adding pod %s/%s to node %s", pod.Namespace, pod.Name, pod.Spec.NodeName)
-			}
+			assert.NoError(t, err, "error while adding pod %s/%s to node %s", pod.Namespace, pod.Name, pod.Spec.NodeName)
 		} else if pod.Status.NominatedNodeName != "" {
 			err = snapshot.AddPod(pod, pod.Status.NominatedNodeName)
-			if reportErrors {
-				assert.NoError(t, err, "error while adding pod %s/%s to nominated node %s", pod.Namespace, pod.Name, pod.Status.NominatedNodeName)
-			}
+			assert.NoError(t, err, "error while adding pod %s/%s to nominated node %s", pod.Namespace, pod.Name, pod.Status.NominatedNodeName)
 		} else {
 			assert.Fail(t, "pod %s/%s does not have Spec.NodeName nor Status.NominatedNodeName set", pod.Namespace, pod.Name)
 		}
