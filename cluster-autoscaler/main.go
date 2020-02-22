333fd91698cbef3764c8bc6c58edea8460c08c1a
Marc Sensenich <sensenichm91@gmail.com>
2020-02-22 12:07:07 -0500

    Allow for the leader election resourcelock to have a configurable name

--- cluster-autoscaler/main.go
+++ cluster-autoscaler/main.go
@@ -411,7 +411,7 @@ func main() {
 		lock, err := resourcelock.New(
 			leaderElection.ResourceLock,
 			*namespace,
-			"cluster-autoscaler",
+			leaderElection.ResourceName,
 			kubeClient.CoreV1(),
 			kubeClient.CoordinationV1(),
 			resourcelock.ResourceLockConfig{
@@ -449,6 +449,7 @@ func defaultLeaderElectionConfiguration() componentbaseconfig.LeaderElectionConf
 		RenewDeadline: metav1.Duration{Duration: defaultRenewDeadline},
 		RetryPeriod:   metav1.Duration{Duration: defaultRetryPeriod},
 		ResourceLock:  resourcelock.LeasesResourceLock,
+		ResourceName:  "cluster-autoscaler",
 	}
 }
 
