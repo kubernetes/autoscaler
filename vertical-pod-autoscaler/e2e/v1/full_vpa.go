83509cdd15c5cd1969ee02bb9dbe9fc6517e2c31
Joachim Bartosik <jbartosik@google.com>
2020-04-02 12:01:15 +0200

    Revert "Tweak OOM test"
    This reverts commit 8e47ce497937df164b13777b1e7e1094301f4057.

--- vertical-pod-autoscaler/e2e/v1/full_vpa.go
+++ vertical-pod-autoscaler/e2e/v1/full_vpa.go
@@ -18,7 +18,6 @@ package autoscaling
 
 import (
 	"fmt"
-	"time"
 
 	autoscaling "k8s.io/api/autoscaling/v1"
 	apiv1 "k8s.io/api/core/v1"
@@ -44,7 +43,6 @@ const (
 )
 
 var _ = FullVpaE2eDescribe("Pods under VPA", func() {
-	return
 	var (
 		rc           *ResourceConsumer
 		vpaClientSet *vpa_clientset.Clientset
@@ -93,14 +91,14 @@ var _ = FullVpaE2eDescribe("Pods under VPA", func() {
 	ginkgo.It("have cpu requests growing with usage", func() {
 		// initial CPU usage is low so a minimal recommendation is expected
 		err := waitForResourceRequestInRangeInPods(
-			f, pollTimeout, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceCPU,
+			f, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceCPU,
 			ParseQuantityOrDie(minimalCPULowerBound), ParseQuantityOrDie(minimalCPUUpperBound))
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 		// consume more CPU to get a higher recommendation
 		rc.ConsumeCPU(600 * replicas)
 		err = waitForResourceRequestInRangeInPods(
-			f, pollTimeout, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceCPU,
+			f, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceCPU,
 			ParseQuantityOrDie("500m"), ParseQuantityOrDie("900m"))
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	})
@@ -108,7 +106,7 @@ var _ = FullVpaE2eDescribe("Pods under VPA", func() {
 	ginkgo.It("have memory requests growing with usage", func() {
 		// initial memory usage is low so a minimal recommendation is expected
 		err := waitForResourceRequestInRangeInPods(
-			f, pollTimeout, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceMemory,
+			f, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceMemory,
 			ParseQuantityOrDie(minimalMemoryLowerBound), ParseQuantityOrDie(minimalMemoryUpperBound))
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
@@ -116,7 +114,7 @@ var _ = FullVpaE2eDescribe("Pods under VPA", func() {
 		// NOTE: large range given due to unpredictability of actual memory usage
 		rc.ConsumeMem(1024 * replicas)
 		err = waitForResourceRequestInRangeInPods(
-			f, pollTimeout, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceMemory,
+			f, metav1.ListOptions{LabelSelector: "name=hamster"}, apiv1.ResourceMemory,
 			ParseQuantityOrDie("900Mi"), ParseQuantityOrDie("4000Mi"))
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	})
@@ -159,14 +157,14 @@ var _ = FullVpaE2eDescribe("OOMing pods under VPA", func() {
 	ginkgo.It("have memory requests growing with OOMs", func() {
 		listOptions := metav1.ListOptions{LabelSelector: "name=hamster", FieldSelector: getPodSelectorExcludingDonePodsOrDie()}
 		err := waitForResourceRequestInRangeInPods(
-			f, 7*time.Minute, listOptions, apiv1.ResourceMemory,
+			f, listOptions, apiv1.ResourceMemory,
 			ParseQuantityOrDie("1400Mi"), ParseQuantityOrDie("10000Mi"))
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	})
 })
 
-func waitForPodsMatch(f *framework.Framework, timeout time.Duration, listOptions metav1.ListOptions, matcher func(pod apiv1.Pod) bool) error {
-	return wait.PollImmediate(pollInterval, timeout, func() (bool, error) {
+func waitForPodsMatch(f *framework.Framework, listOptions metav1.ListOptions, matcher func(pod apiv1.Pod) bool) error {
+	return wait.PollImmediate(pollInterval, pollTimeout, func() (bool, error) {
 
 		ns := f.Namespace.Name
 		c := f.ClientSet
@@ -195,8 +193,8 @@ func waitForPodsMatch(f *framework.Framework, timeout time.Duration, listOptions
 	})
 }
 
-func waitForResourceRequestInRangeInPods(f *framework.Framework, timeout time.Duration, listOptions metav1.ListOptions, resourceName apiv1.ResourceName, lowerBound, upperBound resource.Quantity) error {
-	err := waitForPodsMatch(f, timeout, listOptions,
+func waitForResourceRequestInRangeInPods(f *framework.Framework, listOptions metav1.ListOptions, resourceName apiv1.ResourceName, lowerBound, upperBound resource.Quantity) error {
+	err := waitForPodsMatch(f, listOptions,
 		func(pod apiv1.Pod) bool {
 			resourceRequest, found := pod.Spec.Containers[0].Resources.Requests[resourceName]
 			framework.Logf("Comparing %v request %v against range of (%v, %v)", resourceName, resourceRequest, lowerBound, upperBound)
