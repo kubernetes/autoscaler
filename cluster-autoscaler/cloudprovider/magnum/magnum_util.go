c6f5c4cc1b9a81887dc919fbd33e9e7468ce6b13
Thomas Hartland <thomas.george.hartland@cern.ch>
2020-05-22 16:35:21 +0200

    Update magnum utils

--- cluster-autoscaler/cloudprovider/magnum/magnum_util.go
+++ cluster-autoscaler/cloudprovider/magnum/magnum_util.go
@@ -16,14 +16,42 @@ limitations under the License.
 
 package magnum
 
+import (
+	"fmt"
+	"strings"
+
+	"k8s.io/api/core/v1"
+)
+
 const (
 	scaleToZeroSupported = false
 )
 
-// NodeRef stores the name, machineID and providerID of a node.
+// NodeRef stores the name, systemUUID and providerID of a node.
+// For refs which are created from fake nodes, IsFake should be true.
 type NodeRef struct {
 	Name       string
-	MachineID  string
+	SystemUUID string
 	ProviderID string
-	IPs        []string
+	IsFake     bool
+}
+
+// isFakeNode returns true if a node object was created from a CA cloudprovider.Instance,
+// or false if it is from an actual node in the cluster.
+func isFakeNode(node *v1.Node) bool {
+	// An actual node will have an object UID.
+	// If there is no UID then the node is fake.
+	return len(node.ObjectMeta.UID) == 0
+}
+
+// parseFakeProviderID takes a fake provider ID in the format
+// fake:///<nodeGroupID>/<minionIndex> and returns the
+// node group ID  and minion index.
+func parseFakeProviderID(id string) (string, string, error) {
+	id2 := strings.TrimPrefix(id, "fake:///")
+	parts := strings.Split(id2, "/")
+	if len(parts) < 2 {
+		return "", "", fmt.Errorf("could not parse fake node provider ID %q", id)
+	}
+	return parts[0], parts[1], nil
 }
