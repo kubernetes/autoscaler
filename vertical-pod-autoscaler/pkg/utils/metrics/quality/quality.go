04ed727a0a8fda4149ec5b0905035a2331d3680a
Karol Gołąb <kgolab@google.com>
2020-05-29 10:44:24 +0200

    Add a metric for observing recommendation stability & changes.

--- vertical-pod-autoscaler/pkg/utils/metrics/quality/quality.go
+++ vertical-pod-autoscaler/pkg/utils/metrics/quality/quality.go
@@ -22,6 +22,7 @@ import (
 	"strconv"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	vpa_types "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/utils/metrics"
 	"k8s.io/klog"
@@ -38,6 +39,9 @@ var (
 	cpuBuckets = prometheus.ExponentialBuckets(0.01, 2., 17)
 	// Buckets between 1MB and 65.5 GB
 	memoryBuckets = prometheus.ExponentialBuckets(1e6, 2., 17)
+	// Buckets for relative comparisons, from -100% to x100
+	relativeBuckets = []float64{-1., -.75, -.5, -.25, -.1, -.05, -0.025, -.01, -.005, -0.0025, -.001,
+		0., .001, .0025, .005, .01, .025, .05, .1, .25, .5, .75, 1., 2.5, 5., 10., 25., 50., 100.}
 )
 
 var (
@@ -46,8 +50,7 @@ var (
 			Namespace: metricsNamespace,
 			Name:      "usage_recommendation_relative_diffs",
 			Help:      "Diffs between recommendation and usage, normalized by recommendation value",
-			Buckets: []float64{-1., -.75, -.5, -.25, -.1, -.05, -0.025, -.01, -.005, -0.0025, -.001, 0.,
-				.001, .0025, .005, .01, .025, .05, .1, .25, .5, .75, 1., 2.5, 5., 10., 25., 50., 100.},
+			Buckets:   relativeBuckets,
 		}, []string{"update_mode", "resource", "is_oom"},
 	)
 	usageMissingRecommendationCounter = prometheus.NewCounterVec(
@@ -105,6 +108,14 @@ var (
 			Buckets:   memoryBuckets,
 		}, []string{"update_mode", "is_oom"},
 	)
+	relativeRecommendationChange = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: metricsNamespace,
+			Name:      "relative_recommendation_changes",
+			Help:      "Changes between consecutive recommendation values, normalized by old value",
+			Buckets:   relativeBuckets,
+		}, []string{"update_mode", "resource", "vpa_size_log2"},
+	)
 )
 
 // Register initializes all VPA quality metrics
@@ -117,6 +128,7 @@ func Register() {
 	prometheus.MustRegister(memoryRecommendationLowerOrEqualUsageDiff)
 	prometheus.MustRegister(cpuRecommendations)
 	prometheus.MustRegister(memoryRecommendations)
+	prometheus.MustRegister(relativeRecommendationChange)
 }
 
 // observeUsageRecommendationRelativeDiff records relative diff between usage and
@@ -182,6 +194,44 @@ func ObserveQualityMetrics(usage, recommendation float64, isOOM bool, resource c
 func ObserveQualityMetricsRecommendationMissing(usage float64, isOOM bool, resource corev1.ResourceName, updateMode *vpa_types.UpdateMode) {
 	observeMissingRecommendation(isOOM, resource, updateMode)
 	observeUsageRecommendationDiff(usage, 0, true, isOOM, resource, updateMode)
+}
+
+// ObserveRecommendationChange records relative_recommendation_changes metric.
+func ObserveRecommendationChange(previous, current corev1.ResourceList, updateMode *vpa_types.UpdateMode, vpaSize int) {
+	// This will happen if there is no previous recommendation, we don't want to emit anything then.
+	if previous == nil {
+		return
+	}
+	// This is not really expected thus a warning.
+	if current == nil {
+		klog.Warningf("Cannot compare with current recommendation being nil. VPA mode: %v, size: %v", updateMode, vpaSize)
+		return
+	}
+
+	for resource, amount := range current {
+		newValue := quantityAsFloat(resource, amount)
+		oldValue := quantityAsFloat(resource, previous[resource])
+
+		if oldValue > 0.0 {
+			diff := newValue/oldValue - 1.0 // -1.0 to report decreases as negative values and keep 0.0 neutral
+			relativeRecommendationChange.WithLabelValues(updateModeToString(updateMode), string(resource), string(vpaSize)).Observe(diff)
+		} else {
+			klog.Warningf("Cannot compare as old recommendation for %v is 0. VPA mode: %v, size: %v", resource, updateMode, vpaSize)
+		}
+	}
+}
+
+// quantityAsFloat converts resource.Quantity to a float64 value, in some scale (constant per resource but unspecified)
+func quantityAsFloat(resource corev1.ResourceName, quantity resource.Quantity) float64 {
+	switch resource {
+	case corev1.ResourceCPU:
+		return float64(quantity.MilliValue())
+	case corev1.ResourceMemory:
+		return float64(quantity.Value())
+	default:
+		klog.Warningf("Unknown resource: %v", resource)
+		return 0.0
+	}
 }
 
 func updateModeToString(updateMode *vpa_types.UpdateMode) string {
