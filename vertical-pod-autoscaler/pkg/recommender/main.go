ba6265f37c98404f4f4a22737b622c6a92e6793b
Jens Erat <jens.erat@daimler.com>
2020-06-15 11:06:34 +0200

    Allow out-of-cluster operation of VPA
    Because of how we operate our Kubernetes clusters, we need to
    run vertical pod autoscaler with an external kubeconfig. Also
    aligned and factored out redundant configuration code.
    
    Signed-off-by: Jens Erat <jens.erat@daimler.com>

--- vertical-pod-autoscaler/pkg/recommender/main.go
+++ vertical-pod-autoscaler/pkg/recommender/main.go
@@ -28,7 +28,6 @@ import (
 	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/utils/metrics"
 	metrics_quality "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/utils/metrics/quality"
 	metrics_recommender "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/utils/metrics/recommender"
-	"k8s.io/client-go/rest"
 	kube_flag "k8s.io/component-base/cli/flag"
 	"k8s.io/klog"
 )
@@ -39,6 +38,7 @@ var (
 	prometheusAddress      = flag.String("prometheus-address", "", `Where to reach for Prometheus metrics`)
 	prometheusJobName      = flag.String("prometheus-cadvisor-job-name", "kubernetes-cadvisor", `Name of the prometheus job name which scrapes the cAdvisor metrics`)
 	address                = flag.String("address", ":8942", "The address to expose Prometheus metrics.")
+	kubeconfig             = flag.String("kubeconfig", "", "Paths to a kubeconfig. Only required if out-of-cluster.")
 	kubeApiQps             = flag.Float64("kube-api-qps", 5.0, `QPS limit when making requests to Kubernetes apiserver`)
 	kubeApiBurst           = flag.Float64("kube-api-burst", 10.0, `QPS burst limit when making requests to Kubernetes apiserver`)
 
@@ -70,7 +70,7 @@ func main() {
 	kube_flag.InitFlags()
 	klog.V(1).Infof("Vertical Pod Autoscaler %s Recommender", common.VerticalPodAutoscalerVersion)
 
-	config := createKubeConfig(float32(*kubeApiQps), int(*kubeApiBurst))
+	config := common.CreateKubeConfigOrDie(*kubeconfig, float32(*kubeApiQps), int(*kubeApiBurst))
 
 	model.InitializeAggregationsConfig(model.NewAggregationsConfig(*memoryAggregationInterval, *memoryAggregationIntervalCount, *memoryHistogramDecayHalfLife, *cpuHistogramDecayHalfLife))
 
@@ -117,14 +117,4 @@ func main() {
 		recommender.RunOnce()
 		healthCheck.UpdateLastActivity()
 	}
-}
-
-func createKubeConfig(kubeApiQps float32, kubeApiBurst int) *rest.Config {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		klog.Fatalf("Failed to create config: %v", err)
-	}
-	config.QPS = kubeApiQps
-	config.Burst = kubeApiBurst
-	return config
 }
