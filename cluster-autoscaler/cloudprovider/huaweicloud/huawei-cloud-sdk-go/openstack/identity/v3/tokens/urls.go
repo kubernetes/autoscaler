f35dcfa581a45aa131a0ddd588d85fbe62eb5311
Maciek Pytel <maciekpytel@google.com>
2020-07-21 13:00:56 +0200

    Revert "Merge pull request #3099 from ysy2020/cluster-autoscaler-huaweicloud"
    This reverts commit 84cbb3bc7923d56c6cffe1b117cb89cb91820243, reversing
    changes made to 6d6903f2f9e680ec50d41bf15af2df1ca0160ca0.

--- cluster-autoscaler/cloudprovider/huaweicloud/huawei-cloud-sdk-go/openstack/identity/v3/tokens/urls.go
+++
@@ -1,7 +0,0 @@
-package tokens
-
-import "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/huaweicloud/huawei-cloud-sdk-go"
-
-func tokenURL(c *huawei_cloud_sdk_go.ServiceClient) string {
-	return c.ServiceURL("auth", "tokens")
-}
