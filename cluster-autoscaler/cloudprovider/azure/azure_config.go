1c9751b403f540a2fb328d069c677b97adc6bdd5
Marwan Ahmed <marwanad@microsoft.com>
2020-08-25 20:38:27 -0700

    gofmt and update header

--- cluster-autoscaler/cloudprovider/azure/azure_config.go
+++ cluster-autoscaler/cloudprovider/azure/azure_config.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package azure
 
 import (
@@ -22,7 +38,7 @@ const (
 	// The path of deployment parameters for standard vm.
 	deploymentParametersPath = "/var/lib/azure/azuredeploy.parameters.json"
 
-	metadataURL                    = "http://169.254.169.254/metadata/instance"
+	metadataURL = "http://169.254.169.254/metadata/instance"
 
 	// backoff
 	backoffRetriesDefault  = 6
@@ -100,7 +116,8 @@ type Config struct {
 	CloudProviderBackoffJitter   float64 `json:"cloudProviderBackoffJitter,omitempty" yaml:"cloudProviderBackoffJitter,omitempty"`
 }
 
-func BuildAzureConfig(configReader io.Reader)  (*Config, error) {
+// BuildAzureConfig returns a Config object for the Azure clients
+func BuildAzureConfig(configReader io.Reader) (*Config, error) {
 	var err error
 	cfg := &Config{}
 
@@ -438,6 +455,7 @@ func (cfg *Config) validate() error {
 
 	return nil
 }
+
 // getSubscriptionId reads the Subscription ID from the instance metadata.
 func getSubscriptionIdFromInstanceMetadata() (string, error) {
 	subscriptionID, present := os.LookupEnv("ARM_SUBSCRIPTION_ID")
@@ -456,4 +474,3 @@ func getSubscriptionIdFromInstanceMetadata() (string, error) {
 	}
 	return subscriptionID, nil
 }
-
