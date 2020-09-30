f04cd2ef63b1b8235f59191f00c251abf7db8e2e
Marwan Ahmed <marwanad@microsoft.com>
2020-09-29 21:31:07 -0700

    fix imports

--- cluster-autoscaler/cloudprovider/azure/azure_template.go
+++ cluster-autoscaler/cloudprovider/azure/azure_template.go
@@ -18,14 +18,14 @@ package azure
 
 import (
 	"fmt"
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-12-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
 	"k8s.io/autoscaler/cluster-autoscaler/utils/gpu"
 	cloudvolume "k8s.io/cloud-provider/volume"
-	"k8s.io/klog/v2"
+	"k8s.io/klog"
 	kubeletapis "k8s.io/kubernetes/pkg/kubelet/apis"
 	"math/rand"
 	"regexp"
