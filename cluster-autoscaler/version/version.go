e970923faf8b344f78b85d72b9bdcb016bbf36bd
Maciek Pytel <maciekpytel@google.com>
2020-11-03 15:01:15 +0100

    Cluster Autoscaler 1.16.7

--- cluster-autoscaler/version/version.go
+++ cluster-autoscaler/version/version.go
@@ -17,4 +17,4 @@ limitations under the License.
 package version
 
 // ClusterAutoscalerVersion contains version of CA.
-const ClusterAutoscalerVersion = "1.16.6"
+const ClusterAutoscalerVersion = "1.16.7"
