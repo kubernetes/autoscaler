087c668af7ffebb33ecfe451441a3107944d57d7
Yaroslava Serdiuk <yaroslava@google.com>
2020-11-25 16:48:24 +0000

    update generic labels for GCE

--- cluster-autoscaler/cloudprovider/gce/templates_test.go
+++ cluster-autoscaler/cloudprovider/gce/templates_test.go
@@ -166,15 +166,18 @@ func TestBuildGenericLabels(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			expectedLabels := map[string]string{
-				apiv1.LabelZoneRegion:        "us-central1",
-				apiv1.LabelZoneFailureDomain: "us-central1-b",
-				gceCSITopologyKeyZone:        "us-central1-b",
-				apiv1.LabelHostname:          "sillyname",
-				apiv1.LabelInstanceType:      "n1-standard-8",
-				kubeletapis.LabelArch:        cloudprovider.DefaultArch,
-				kubeletapis.LabelOS:          tc.expectedOsLabel,
-				apiv1.LabelArchStable:        cloudprovider.DefaultArch,
-				apiv1.LabelOSStable:          tc.expectedOsLabel,
+				apiv1.LabelZoneRegion:              "us-central1",
+				apiv1.LabelZoneRegionStable:        "us-central1",
+				apiv1.LabelZoneFailureDomain:       "us-central1-b",
+				apiv1.LabelZoneFailureDomainStable: "us-central1-b",
+				gceCSITopologyKeyZone:              "us-central1-b",
+				apiv1.LabelHostname:                "sillyname",
+				apiv1.LabelInstanceType:            "n1-standard-8",
+				apiv1.LabelInstanceTypeStable:      "n1-standard-8",
+				kubeletapis.LabelArch:              cloudprovider.DefaultArch,
+				kubeletapis.LabelOS:                tc.expectedOsLabel,
+				apiv1.LabelArchStable:              cloudprovider.DefaultArch,
+				apiv1.LabelOSStable:                tc.expectedOsLabel,
 			}
 			labels, err := BuildGenericLabels(GceRef{
 				Name:    "kubernetes-minion-group",
