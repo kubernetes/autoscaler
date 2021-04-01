ed269617b81fe8781eb74d4be6b19ece08071e29
David Morrison <david.morrison@airbnb.com>
2021-04-01 09:10:15 -0700

    cache ASG InstanceTypes for AWS

--- cluster-autoscaler/cloudprovider/aws/aws_manager.go
+++ cluster-autoscaler/cloudprovider/aws/aws_manager.go
@@ -56,11 +56,10 @@ const (
 
 // AwsManager is handles aws communication and data caching.
 type AwsManager struct {
-	autoScalingService autoScalingWrapper
-	ec2Service         ec2Wrapper
-	asgCache           *asgCache
-	lastRefresh        time.Time
-	instanceTypes      map[string]*InstanceType
+	awsService    awsWrapper
+	asgCache      *asgCache
+	lastRefresh   time.Time
+	instanceTypes map[string]*InstanceType
 }
 
 type asgTemplate struct {
@@ -161,7 +160,7 @@ func getRegion(cfg ...*aws.Config) string {
 	return region
 }
 
-// createAwsManagerInternal allows for a customer autoScalingWrapper to be passed in by tests
+// createAwsManagerInternal allows for a custom objects to be passed in by tests
 //
 // #1449 If running tests outside of AWS without AWS_REGION among environment
 // variables, avoid a 5+ second EC2 Metadata lookup timeout in getRegion by
@@ -172,8 +171,7 @@ func getRegion(cfg ...*aws.Config) string {
 func createAWSManagerInternal(
 	configReader io.Reader,
 	discoveryOpts cloudprovider.NodeGroupDiscoveryOptions,
-	autoScalingService *autoScalingWrapper,
-	ec2Service *ec2Wrapper,
+	awsService *awsWrapper,
 	instanceTypes map[string]*InstanceType,
 ) (*AwsManager, error) {
 
@@ -188,7 +186,7 @@ func createAWSManagerInternal(
 		return nil, err
 	}
 
-	if autoScalingService == nil || ec2Service == nil {
+	if awsService == nil {
 		awsSdkProvider := newAWSSDKProvider(cfg)
 		sess, err := session.NewSession(aws.NewConfig().WithRegion(getRegion()).
 			WithEndpointResolver(getResolver(awsSdkProvider.cfg)))
@@ -196,14 +194,7 @@ func createAWSManagerInternal(
 			return nil, err
 		}
 
-		if autoScalingService == nil {
-			c := newLaunchConfigurationInstanceTypeCache()
-			autoScalingService = &autoScalingWrapper{autoscaling.New(sess), c}
-		}
-
-		if ec2Service == nil {
-			ec2Service = &ec2Wrapper{ec2.New(sess)}
-		}
+		awsService = &awsWrapper{autoscaling.New(sess), ec2.New(sess)}
 	}
 
 	specs, err := parseASGAutoDiscoverySpecs(discoveryOpts)
@@ -211,16 +202,15 @@ func createAWSManagerInternal(
 		return nil, err
 	}
 
-	cache, err := newASGCache(*autoScalingService, discoveryOpts.NodeGroupSpecs, specs)
+	cache, err := newASGCache(awsService, discoveryOpts.NodeGroupSpecs, specs)
 	if err != nil {
 		return nil, err
 	}
 
 	manager := &AwsManager{
-		autoScalingService: *autoScalingService,
-		ec2Service:         *ec2Service,
-		asgCache:           cache,
-		instanceTypes:      instanceTypes,
+		awsService:    *awsService,
+		asgCache:      cache,
+		instanceTypes: instanceTypes,
 	}
 
 	if err := manager.forceRefresh(); err != nil {
@@ -247,7 +237,7 @@ func readAWSCloudConfig(config io.Reader) (*provider_aws.CloudConfig, error) {
 
 // CreateAwsManager constructs awsManager object.
 func CreateAwsManager(configReader io.Reader, discoveryOpts cloudprovider.NodeGroupDiscoveryOptions, instanceTypes map[string]*InstanceType) (*AwsManager, error) {
-	return createAWSManagerInternal(configReader, discoveryOpts, nil, nil, instanceTypes)
+	return createAWSManagerInternal(configReader, discoveryOpts, nil, instanceTypes)
 }
 
 // Refresh is called before every main loop and can be used to dynamically update cloud provider state.
@@ -315,7 +305,7 @@ func (m *AwsManager) getAsgTemplate(asg *asg) (*asgTemplate, error) {
 		klog.V(4).Infof("Found multiple availability zones for ASG %q; using %s for %s label\n", asg.Name, az, apiv1.LabelZoneFailureDomain)
 	}
 
-	instanceTypeName, err := m.buildInstanceType(asg)
+	instanceTypeName, err := getCachedInstanceTypeForAsg(m.asgCache, asg)
 	if err != nil {
 		return nil, err
 	}
@@ -329,23 +319,6 @@ func (m *AwsManager) getAsgTemplate(asg *asg) (*asgTemplate, error) {
 		}, nil
 	}
 	return nil, fmt.Errorf("ASG %q uses the unknown EC2 instance type %q", asg.Name, instanceTypeName)
-}
-
-func (m *AwsManager) buildInstanceType(asg *asg) (string, error) {
-	if asg.LaunchConfigurationName != "" {
-		return m.autoScalingService.getInstanceTypeByLCName(asg.LaunchConfigurationName)
-	} else if asg.LaunchTemplate != nil {
-		return m.ec2Service.getInstanceTypeByLT(asg.LaunchTemplate)
-	} else if asg.MixedInstancesPolicy != nil {
-		// always use first instance
-		if len(asg.MixedInstancesPolicy.instanceTypesOverrides) != 0 {
-			return asg.MixedInstancesPolicy.instanceTypesOverrides[0], nil
-		}
-
-		return m.ec2Service.getInstanceTypeByLT(asg.MixedInstancesPolicy.launchTemplate)
-	}
-
-	return "", errors.New("Unable to get instance type from launch config or launch template")
 }
 
 func (m *AwsManager) buildNodeFromTemplate(asg *asg, template *asgTemplate) (*apiv1.Node, error) {
