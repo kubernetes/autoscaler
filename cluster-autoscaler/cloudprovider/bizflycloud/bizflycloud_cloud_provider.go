04448bd547d538be6d77061dd4759078f36aaa86
Lê Minh Quân <quanlm@vccloud.vn>
2021-04-08 16:39:59 +0700

    Fix/Add Boilerplate header

--- cluster-autoscaler/cloudprovider/bizflycloud/bizflycloud_cloud_provider.go
+++ cluster-autoscaler/cloudprovider/bizflycloud/bizflycloud_cloud_provider.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package bizflycloud
 
 import (
@@ -167,7 +183,6 @@ func BuildBizflyCloud(
 
 	// the cloud provider automatically uses all node pools in Bizflycloud.
 	// This means we don't use the cloudprovider.NodeGroupDiscoveryOptions
-	// flags (which can be set via '--node-group-auto-discovery' or '-nodes')
 	provider, err := newBizflyCloudProvider(manager, rl)
 	if err != nil {
 		klog.Fatalf("Failed to create Blizflycloud provider: %v", err)
@@ -181,7 +196,7 @@ func toProviderID(nodeID string) string {
 	return fmt.Sprintf("%s%s", bizflyProviderIDPrefix, nodeID)
 }
 
-// toNodeID returns a node or droplet ID from the given provider ID.
+// toNodeID returns a node or physical ID from the given provider ID.
 func toNodeID(providerID string) string {
 	return strings.TrimPrefix(providerID, bizflyProviderIDPrefix)
 }
