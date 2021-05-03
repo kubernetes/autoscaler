99289fb9c547298c2b46cecc10b0a1dd831fa7b4
Alexander Block <ablock84@gmail.com>
2021-05-03 12:19:12 +0200

    Introduce UpdateDeprecatedTemplateLabels to set beta/deprecated labels
    And at the same time only set stable labels in all buildGenericLabels
    implementations.
    
    This fixes issues when a node group has 0 nodes yet and node labels are
    built using buildGenericLabels and the node-template labels.
    
    Issues include (anti-)affinity and nodeSelectors for the given labels,
    giving false-negative results for candidate nodes, which leads to ASGs
    never scaling up.

--- cluster-autoscaler/core/utils/utils.go
+++ cluster-autoscaler/core/utils/utils.go
@@ -18,6 +18,7 @@ package utils
 
 import (
 	"fmt"
+	kubeletapis "k8s.io/kubelet/pkg/apis"
 	"math/rand"
 	"reflect"
 	"time"
@@ -172,6 +173,8 @@ func GetNodeInfoFromTemplate(nodeGroup cloudprovider.NodeGroup, daemonsets []*ap
 		return nil, errors.ToAutoscalerError(errors.CloudProviderError, err)
 	}
 
+	updateDeprecatedTemplateLabels(baseNodeInfo)
+
 	pods, err := daemonset.GetDaemonSetPodsForNode(baseNodeInfo, daemonsets, predicateChecker)
 	if err != nil {
 		return nil, errors.ToAutoscalerError(errors.InternalError, err)
@@ -186,6 +189,26 @@ func GetNodeInfoFromTemplate(nodeGroup cloudprovider.NodeGroup, daemonsets []*ap
 		return nil, typedErr
 	}
 	return sanitizedNodeInfo, nil
+}
+
+// UpdateDeprecatedTemplateLabels updates beta and deprecated labels from stable labels
+func updateDeprecatedTemplateLabels(nodeInfo *schedulerframework.NodeInfo) {
+	node := nodeInfo.Node()
+	if v, ok := node.ObjectMeta.Labels[apiv1.LabelArchStable]; ok {
+		node.ObjectMeta.Labels[kubeletapis.LabelArch] = v
+	}
+	if v, ok := node.ObjectMeta.Labels[apiv1.LabelOSStable]; ok {
+		node.ObjectMeta.Labels[kubeletapis.LabelOS] = v
+	}
+	if v, ok := node.ObjectMeta.Labels[apiv1.LabelInstanceTypeStable]; ok {
+		node.ObjectMeta.Labels[apiv1.LabelInstanceType] = v
+	}
+	if v, ok := node.ObjectMeta.Labels[apiv1.LabelTopologyRegion]; ok {
+		node.ObjectMeta.Labels[apiv1.LabelZoneRegion] = v
+	}
+	if v, ok := node.ObjectMeta.Labels[apiv1.LabelTopologyZone]; ok {
+		node.ObjectMeta.Labels[apiv1.LabelZoneFailureDomain] = v
+	}
 }
 
 // isVirtualNode determines if the node is created by virtual kubelet
