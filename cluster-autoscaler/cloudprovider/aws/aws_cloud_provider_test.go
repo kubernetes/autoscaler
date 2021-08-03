ad0cc22c45987acf18733b12afeb476bfc2114a3
Benjamin Pineau <benjamin.pineau@datadoghq.com>
2021-08-03 20:14:42 +0200

    implement GetOptions for AWS
    Support per-ASG (scaledown) settings as permited by the
    cloudprovider's interface GetOptions() method.

--- cluster-autoscaler/cloudprovider/aws/aws_cloud_provider_test.go
+++ cluster-autoscaler/cloudprovider/aws/aws_cloud_provider_test.go
@@ -51,6 +51,7 @@ func newTestAwsManagerWithMockServices(mockAutoScaling autoScalingI, mockEC2 ec2
 			interrupt:             make(chan struct{}),
 			asgAutoDiscoverySpecs: autoDiscoverySpecs,
 			awsService:            &awsService,
+			autoscalingOptions:    make(map[AwsRef]map[string]string),
 		},
 	}
 }
