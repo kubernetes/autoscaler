57a06ee68f4bb7099f0d542a60a19781d360862e
Samarth Deyagond <deyagondsamarth@gmail.com>
2021-12-17 12:05:18 +0530

    implementation of generic scale from zero

--- cluster-autoscaler/cloudprovider/mcm/mcm_manager_test.go
+++ cluster-autoscaler/cloudprovider/mcm/mcm_manager_test.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
 	kubeletapis "k8s.io/kubernetes/pkg/kubelet/apis"
 )
@@ -36,8 +37,8 @@ func TestBuildGenericLabels(t *testing.T) {
 	labels := buildGenericLabels(&nodeTemplate{
 		InstanceType: &instanceType{
 			InstanceType: instanceTypeC4Large,
-			VCPU:         2,
-			MemoryMb:     3840,
+			VCPU:         resource.MustParse("2"),
+			Memory:       resource.MustParse("3840Mi"),
 		},
 		Region: regionUSEast1,
 		Zone:   zoneUSEast1a,
