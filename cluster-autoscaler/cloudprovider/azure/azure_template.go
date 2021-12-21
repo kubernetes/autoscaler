d8df72eca1355f012dbc7c147c05421c0e7bd9cd
Marwan Ahmed <marwanad@microsoft.com>
2021-12-21 20:35:24 +0200

    ignore azure csi topology label for similarity checks and populate it for scale from zero

--- cluster-autoscaler/cloudprovider/azure/azure_template.go
+++ cluster-autoscaler/cloudprovider/azure/azure_template.go
@@ -31,6 +31,8 @@ import (
 	"strings"
 )
 
+const azureDiskTopologyKey string = "topology.disk.csi.azure.com/zone"
+
 func buildInstanceOS(template compute.VirtualMachineScaleSet) string {
 	instanceOS := cloudprovider.DefaultOS
 	if template.VirtualMachineProfile != nil && template.VirtualMachineProfile.OsProfile != nil && template.VirtualMachineProfile.OsProfile.WindowsConfiguration != nil {
@@ -56,8 +58,10 @@ func buildGenericLabels(template compute.VirtualMachineScaleSet, nodeName string
 		}
 
 		result[apiv1.LabelTopologyZone] = strings.Join(failureDomains[:], cloudvolume.LabelMultiZoneDelimiter)
+		result[azureDiskTopologyKey] = strings.Join(failureDomains[:], cloudvolume.LabelMultiZoneDelimiter)
 	} else {
 		result[apiv1.LabelTopologyZone] = "0"
+		result[azureDiskTopologyKey] = ""
 	}
 
 	result[apiv1.LabelHostname] = nodeName
