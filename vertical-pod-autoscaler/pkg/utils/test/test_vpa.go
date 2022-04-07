3e8006aea89cf3d1f6cc8fbe63f288b4a96d8c20
Matthias Bertschy <matthias.bertschy@gmail.com>
2022-04-07 11:32:57 +0200

    move targetCPUPercentile into a flag

--- vertical-pod-autoscaler/pkg/utils/test/test_vpa.go
+++ vertical-pod-autoscaler/pkg/utils/test/test_vpa.go
@@ -40,6 +40,7 @@ type VerticalPodAutoscalerBuilder interface {
 	WithTargetRef(targetRef *autoscaling.CrossVersionObjectReference) VerticalPodAutoscalerBuilder
 	WithUpperBound(cpu, memory string) VerticalPodAutoscalerBuilder
 	WithAnnotations(map[string]string) VerticalPodAutoscalerBuilder
+	WithRecommender(string2 string) VerticalPodAutoscalerBuilder
 	AppendCondition(conditionType vpa_types.VerticalPodAutoscalerConditionType,
 		status core.ConditionStatus, reason, message string, lastTransitionTime time.Time) VerticalPodAutoscalerBuilder
 	AppendRecommendation(vpa_types.RecommendedContainerResources) VerticalPodAutoscalerBuilder
@@ -70,6 +71,7 @@ type verticalPodAutoscalerBuilder struct {
 	annotations             map[string]string
 	targetRef               *autoscaling.CrossVersionObjectReference
 	appendedRecommendations []vpa_types.RecommendedContainerResources
+	recommender             string
 }
 
 func (b *verticalPodAutoscalerBuilder) WithName(vpaName string) VerticalPodAutoscalerBuilder {
@@ -153,6 +155,12 @@ func (b *verticalPodAutoscalerBuilder) WithAnnotations(annotations map[string]st
 	return &c
 }
 
+func (b *verticalPodAutoscalerBuilder) WithRecommender(recommender string) VerticalPodAutoscalerBuilder {
+	c := *b
+	c.recommender = recommender
+	return &c
+}
+
 func (b *verticalPodAutoscalerBuilder) AppendCondition(conditionType vpa_types.VerticalPodAutoscalerConditionType,
 	status core.ConditionStatus, reason, message string, lastTransitionTime time.Time) VerticalPodAutoscalerBuilder {
 	c := *b
@@ -175,6 +183,10 @@ func (b *verticalPodAutoscalerBuilder) Get() *vpa_types.VerticalPodAutoscaler {
 	if b.containerName == "" {
 		panic("Must call WithContainer() before Get()")
 	}
+	var recommenders []*vpa_types.VerticalPodAutoscalerRecommenderSelector
+	if b.recommender != "" {
+		recommenders = []*vpa_types.VerticalPodAutoscalerRecommenderSelector{{Name: b.recommender}}
+	}
 	resourcePolicy := vpa_types.PodResourcePolicy{ContainerPolicies: []vpa_types.ContainerResourcePolicy{{
 		ContainerName:    b.containerName,
 		MinAllowed:       b.minAllowed,
@@ -196,6 +208,7 @@ func (b *verticalPodAutoscalerBuilder) Get() *vpa_types.VerticalPodAutoscaler {
 			UpdatePolicy:   b.updatePolicy,
 			ResourcePolicy: &resourcePolicy,
 			TargetRef:      b.targetRef,
+			Recommenders:   recommenders,
 		},
 		Status: vpa_types.VerticalPodAutoscalerStatus{
 			Recommendation: recommendation,
