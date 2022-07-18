e97d4e5f0c203edf1ffd03803c803add49a00169
Rishabh Patel <rishabh.patel@sap.com>
2022-07-18 13:08:33 +0530

    updated vendor of VPA

--- vertical-pod-autoscaler/vendor/k8s.io/component-base/version/base.go
+++ vertical-pod-autoscaler/vendor/k8s.io/component-base/version/base.go
@@ -55,8 +55,8 @@ var (
 	// NOTE: The $Format strings are replaced during 'git archive' thanks to the
 	// companion .gitattributes file containing 'export-subst' in this same
 	// directory.  See also https://git-scm.com/docs/gitattributes
-	gitVersion   = "v0.0.0-master+$Format:%h$"
-	gitCommit    = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
+	gitVersion   = "v0.0.0-master+271bafc399"
+	gitCommit    = "271bafc399d92f9a47855440c88049334d75255c" // sha1 from git, output of $(git rev-parse HEAD)
 	gitTreeState = ""            // state of git tree, either "clean" or "dirty"
 
 	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
