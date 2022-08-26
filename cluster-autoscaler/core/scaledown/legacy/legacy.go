a69c4206d3db5c722ba2919cf573b273ad8da602
Daniel Kłobuszewski <danielmk@google.com>
2022-08-26 10:03:12 +0200

    Allow simulator to persist changes in cluster snapshot

--- cluster-autoscaler/core/scaledown/legacy/legacy.go
+++ cluster-autoscaler/core/scaledown/legacy/legacy.go
@@ -65,7 +65,7 @@ type ScaleDown struct {
 // NewScaleDown builds new ScaleDown object.
 func NewScaleDown(context *context.AutoscalingContext, processors *processors.AutoscalingProcessors, clusterStateRegistry *clusterstate.ClusterStateRegistry, ndt *deletiontracker.NodeDeletionTracker) *ScaleDown {
 	usageTracker := simulator.NewUsageTracker()
-	removalSimulator := simulator.NewRemovalSimulator(context.ListerRegistry, context.ClusterSnapshot, context.PredicateChecker, usageTracker)
+	removalSimulator := simulator.NewRemovalSimulator(context.ListerRegistry, context.ClusterSnapshot, context.PredicateChecker, usageTracker, false)
 	unremovableNodes := unremovable.NewNodes()
 	return &ScaleDown{
 		context:              context,
