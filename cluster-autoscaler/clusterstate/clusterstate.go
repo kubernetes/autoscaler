7ec9486ce777dafcc1c439bc2289b8bd6ccc02a4
Clint Fooken <clfooken@ea.com>
2022-10-17 15:45:55 -0700

    Fixing errors due to merge on branches.

--- cluster-autoscaler/clusterstate/clusterstate.go
+++ cluster-autoscaler/clusterstate/clusterstate.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/autoscaler/cluster-autoscaler/clusterstate/utils"
 	"k8s.io/autoscaler/cluster-autoscaler/metrics"
 	"k8s.io/autoscaler/cluster-autoscaler/utils/backoff"
+	"k8s.io/autoscaler/cluster-autoscaler/utils/deletetaint"
 	kube_util "k8s.io/autoscaler/cluster-autoscaler/utils/kubernetes"
 
 	apiv1 "k8s.io/api/core/v1"
@@ -294,7 +295,6 @@ func (csr *ClusterStateRegistry) UpdateNodes(nodes []*apiv1.Node, nodeInfosForGr
 	}
 
 	cloudProviderNodeInstances, err := csr.getCloudProviderNodeInstances()
-	cloudProviderNodesRemoved := csr.getCloudProviderDeletedNodes(nodes, cloudProviderNodeInstances)
 	if err != nil {
 		return err
 	}
