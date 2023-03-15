b4196fab41256b5a9eaa2f7ce536a831c4ca5ffa
Daniel Gutowski <danielgutowski@google.com>
2023-03-15 02:48:32 -0700

    ScaleUp wrapper tests refactor
    * Add field names to struct literals
    * Slice iteration/population standardized
    * Add boilerplate and fix comment lint errors

--- cluster-autoscaler/core/scaleup/wrapper/skippedreasons.go
+++ cluster-autoscaler/core/scaleup/wrapper/skippedreasons.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package wrapper
 
 import (
@@ -17,11 +33,11 @@ func (sr *SkippedReasons) Reasons() []string {
 
 var (
 	// BackoffReason node group is in backoff.
-	BackoffReason         = &SkippedReasons{[]string{"in backoff after failed scale-up"}}
+	BackoffReason = &SkippedReasons{[]string{"in backoff after failed scale-up"}}
 	// MaxLimitReachedReason node group reached max size limit.
 	MaxLimitReachedReason = &SkippedReasons{[]string{"max node group size reached"}}
 	// NotReadyReason node group is not ready.
-	NotReadyReason        = &SkippedReasons{[]string{"not ready for scale-up"}}
+	NotReadyReason = &SkippedReasons{[]string{"not ready for scale-up"}}
 )
 
 // MaxResourceLimitReached returns a reason describing which cluster wide resource limits were reached.
