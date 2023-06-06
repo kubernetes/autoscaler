8a98124919b67a4324aa05685de83361b20c3719
shubham82 <shubham.kuchhal@india.nec.com>
2023-06-06 15:30:05 +0530

    Update VPA scripts to use v1.

--- vertical-pod-autoscaler/hack/vpa-process-yamls.sh
+++ vertical-pod-autoscaler/hack/vpa-process-yamls.sh
@@ -41,9 +41,6 @@ fi
 
 ACTION=$1
 COMPONENTS="vpa-v1-crd-gen vpa-rbac updater-deployment recommender-deployment admission-controller-deployment"
-case ${ACTION} in
-delete|diff|print) COMPONENTS+=" vpa-beta2-crd" ;;
-esac
 
 if [ $# -gt 1 ]; then
   COMPONENTS="$2-deployment"
