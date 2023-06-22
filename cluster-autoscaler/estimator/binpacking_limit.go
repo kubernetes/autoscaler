fac121266246952c9c3d450c4f65b8f10ea4a1c6
Yuriy Stryuchkov <43517125+ystryuchkov@users.noreply.github.com>
2023-06-22 17:46:48 +0200

    Add comments for exported types and functions

--- cluster-autoscaler/estimator/binpacking_limit.go
+++ cluster-autoscaler/estimator/binpacking_limit.go
@@ -1,5 +1,7 @@
 package estimator
 
+// BinpackingLimit returns nodes count limit, that is intended to be used as a cap for binpacking.
+// Return value of 0 means that no limit is set.
 type BinpackingLimit interface {
 	GetLimit() int
 }
--- cluster-autoscaler/estimator/threshold_binpacking_limit.go
+++ cluster-autoscaler/estimator/threshold_binpacking_limit.go
@@ -8,6 +8,8 @@ func (l *thresholdBinpackingLimit) GetLimit() int {
 	return l.maxNodes
 }
 
+// NewThresholdBinpackingLimit returns a BinpackingLimit that caps maximum node
+// count by the given static value
 func NewThresholdBinpackingLimit(maxNodes int) BinpackingLimit {
 	return &thresholdBinpackingLimit{
 		maxNodes: maxNodes,
