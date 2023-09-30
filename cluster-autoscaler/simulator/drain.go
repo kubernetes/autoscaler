d6ee96db194898004c4fb7ba56474aa76d7fb58d
Artem Minyaylov <artemvmin@google.com>
2023-09-30 07:20:22 +0000

    Convert scale-down pdb check to drainability rule

--- cluster-autoscaler/simulator/drain.go
+++ cluster-autoscaler/simulator/drain.go
@@ -17,7 +17,6 @@ limitations under the License.
 package simulator
 
 import (
-	"fmt"
 	"time"
 
 	apiv1 "k8s.io/api/core/v1"
@@ -31,14 +30,14 @@ import (
 	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework"
 )
 
-// GetPodsToMove returns a list of pods that should be moved elsewhere
-// and a list of DaemonSet pods that should be evicted if the node
-// is drained. Raises error if there is an unreplicated pod.
-// Based on kubectl drain code. If listers is nil it makes an assumption that RC, DS, Jobs and RS were deleted
-// along with their pods (no abandoned pods with dangling created-by annotation).
-// If listers is not nil it checks whether RC, DS, Jobs and RS that created these pods
-// still exist.
-// TODO(x13n): Rewrite GetPodsForDeletionOnNodeDrain into a set of DrainabilityRules.
+// GetPodsToMove returns a list of pods that should be moved elsewhere and a
+// list of DaemonSet pods that should be evicted if the node is drained.
+// Raises error if there is an unreplicated pod.
+// Based on kubectl drain code. If listers is nil it makes an assumption that
+// RC, DS, Jobs and RS were deleted along with their pods (no abandoned pods
+// with dangling created-by annotation).
+// If listers is not nil it checks whether RC, DS, Jobs and RS that created
+// these pods still exist.
 func GetPodsToMove(nodeInfo *schedulerframework.NodeInfo, deleteOptions options.NodeDeleteOptions, drainabilityRules rules.Rules, listers kube_util.ListerRegistry, remainingPdbTracker pdb.RemainingPdbTracker, timestamp time.Time) (pods []*apiv1.Pod, daemonSetPods []*apiv1.Pod, blockingPod *drain.BlockingPod, err error) {
 	var drainPods, drainDs []*apiv1.Pod
 	if drainabilityRules == nil {
@@ -82,13 +81,6 @@ func GetPodsToMove(nodeInfo *schedulerframework.NodeInfo, deleteOptions options.
 		deleteOptions.SkipNodesWithLocalStorage,
 		deleteOptions.SkipNodesWithCustomControllerPods,
 		timestamp)
-	pods = append(pods, drainPods...)
-	daemonSetPods = append(daemonSetPods, drainDs...)
 
-	if canRemove, _, blockingPodInfo := remainingPdbTracker.CanRemovePods(pods); !canRemove {
-		pod := blockingPodInfo.Pod
-		return []*apiv1.Pod{}, []*apiv1.Pod{}, blockingPodInfo, fmt.Errorf("not enough pod disruption budget to move %s/%s", pod.Namespace, pod.Name)
-	}
-
-	return pods, daemonSetPods, nil, nil
+	return append(pods, drainPods...), append(daemonSetPods, drainDs...), nil, nil
 }
