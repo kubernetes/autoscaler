85ee25cb4b5509cd6ca1725fc3eadc6e0b2f8138
Yuki Iwai <yuki.iwai.tz@gmail.com>
2023-11-24 14:04:24 +0900

    ClusterAutoscaler: Put APIs in a separate go module
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- cluster-autoscaler/processors/provreq/provisioning_request_injector_test.go
+++ cluster-autoscaler/processors/provreq/provisioning_request_injector_test.go
@@ -24,7 +24,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	apimeta "k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/autoscaler/cluster-autoscaler/provisioningrequest/apis/autoscaling.x-k8s.io/v1beta1"
+	"k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/autoscaling.x-k8s.io/v1beta1"
 	"k8s.io/autoscaler/cluster-autoscaler/provisioningrequest/provreqclient"
 	"k8s.io/autoscaler/cluster-autoscaler/provisioningrequest/provreqwrapper"
 	clock "k8s.io/utils/clock/testing"
--- cluster-autoscaler/provisioningrequest/provreqclient/testutils.go
+++ cluster-autoscaler/provisioningrequest/provreqclient/testutils.go
@@ -24,8 +24,8 @@ import (
 
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/autoscaler/cluster-autoscaler/provisioningrequest/apis/autoscaling.x-k8s.io/v1beta1"
-	"k8s.io/autoscaler/cluster-autoscaler/provisioningrequest/client/clientset/versioned/fake"
+	"k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/autoscaling.x-k8s.io/v1beta1"
+	"k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/client/clientset/versioned/fake"
 	"k8s.io/autoscaler/cluster-autoscaler/provisioningrequest/provreqwrapper"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
