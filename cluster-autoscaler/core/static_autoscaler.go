ae57ae82a3f943121e57b02c39c6d1a80ac13ffb
Guo Peng <370090914@qq.com>
2023-12-21 17:59:14 +0800

    feat:add node group health and back off metrics

--- cluster-autoscaler/core/static_autoscaler.go
+++ cluster-autoscaler/core/static_autoscaler.go
@@ -405,6 +405,7 @@ func (a *StaticAutoscaler) RunOnce(currentTime time.Time) caerrors.AutoscalerErr
 		if err != nil {
 			klog.Errorf("AutoscalingStatusProcessor error: %v.", err)
 		}
+		a.clusterStateRegistry.UpdateSafeScaleUpMetricsForNodeGroup(currentTime)
 	}()
 
 	// Check if there are any nodes that failed to register in Kubernetes
