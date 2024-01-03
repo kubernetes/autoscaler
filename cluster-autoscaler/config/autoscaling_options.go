d29ffd03b961b0cdd7b2f547035db9548c4ba022
Yaroslava Serdiuk <yaroslava@google.com>
2024-01-03 12:49:36 +0200

    Add ProvisioningRequestPodsFilter processor (#6386)
    * Introduce ProvisioningRequestPodsFilter processor
    
    * Review

--- cluster-autoscaler/config/autoscaling_options.go
+++ cluster-autoscaler/config/autoscaling_options.go
@@ -281,6 +281,8 @@ type AutoscalingOptions struct {
 	DynamicNodeDeleteDelayAfterTaintEnabled bool
 	// BypassedSchedulers are used to specify which schedulers to bypass their processing
 	BypassedSchedulers map[string]bool
+	// ProvisioningRequestEnabled tells if CA processes ProvisioningRequest.
+	ProvisioningRequestEnabled bool
 }
 
 // KubeClientOptions specify options for kube client
