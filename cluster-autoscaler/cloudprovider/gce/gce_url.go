a8ab563bd03c5a290fb53ecd0c14b2f38f9a424f
Edwinhr716 <edandres249@gmail.com>
2024-03-07 19:35:06 +0000

    separated url parser to its own function, created unit test for the function

--- cluster-autoscaler/cloudprovider/gce/gce_url.go
+++ cluster-autoscaler/cloudprovider/gce/gce_url.go
@@ -78,7 +78,6 @@ func GenerateMigUrl(domainUrl string, ref GceRef) string {
 	return fmt.Sprintf(migUrlTemplate, ref.Project, ref.Zone, ref.Name)
 }
 
-// IsInstanceTemplateRegional verifies if the instance template is regional or global
 func IsInstanceTemplateRegional(templateUrl string) (bool, error) {
 	return regexp.MatchString("(/projects/.*[A-Za-z0-9]+.*/regions/)", templateUrl)
 }
