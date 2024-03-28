5d0c9736522f080a669f53b14f9cb533a639865b
Daniel Gutowski <danielgutowski@google.com>
2024-03-28 08:45:30 -0700

    Make the Estimate func accept pods grouped.
    The grouping should be made by the schedulability equivalence
    meaning we can introduce optimizations to the binpacking.
    
    Introduce a benchmark that estimates capacity needed for 51k pods,
    which can be grouped to two equivalence groups 50k and 1k.

--- cluster-autoscaler/estimator/estimator.go
+++ cluster-autoscaler/estimator/estimator.go
@@ -35,11 +35,18 @@ const (
 // AvailableEstimators is a list of available estimators.
 var AvailableEstimators = []string{BinpackingEstimatorName}
 
+// PodEquivalenceGroup represents a group of pods, which have the same scheduling
+// requirements and are managed by the same controller.
+type PodEquivalenceGroup struct {
+	Pods []*apiv1.Pod
+}
+
 // Estimator calculates the number of nodes of given type needed to schedule pods.
 // It returns the number of new nodes needed as well as the list of pods it managed
 // to schedule on those nodes.
 type Estimator interface {
-	Estimate([]*apiv1.Pod, *schedulerframework.NodeInfo, cloudprovider.NodeGroup) (int, []*apiv1.Pod)
+	// Estimate estimates how many nodes are needed to provision pods coming from the given equivalence groups.
+	Estimate([]PodEquivalenceGroup, *schedulerframework.NodeInfo, cloudprovider.NodeGroup) (int, []*apiv1.Pod)
 }
 
 // EstimatorBuilder creates a new estimator object.
@@ -67,7 +74,7 @@ func NewEstimatorBuilder(name string, limiter EstimationLimiter, orderer Estimat
 // scale-up is limited by external factors.
 type EstimationLimiter interface {
 	// StartEstimation is called at the start of estimation.
-	StartEstimation([]*apiv1.Pod, cloudprovider.NodeGroup, EstimationContext)
+	StartEstimation([]PodEquivalenceGroup, cloudprovider.NodeGroup, EstimationContext)
 	// EndEstimation is called at the end of estimation.
 	EndEstimation()
 	// PermissionToAddNode is called by an estimator when it wants to add additional
@@ -81,5 +88,5 @@ type EstimationLimiter interface {
 // EstimationPodOrderer is an interface used to determine the order of the pods
 // used while binpacking during scale up estimation
 type EstimationPodOrderer interface {
-	Order(pods []*apiv1.Pod, nodeTemplate *framework.NodeInfo, nodeGroup cloudprovider.NodeGroup) []*apiv1.Pod
+	Order(podsEquivalentGroups []PodEquivalenceGroup, nodeTemplate *framework.NodeInfo, nodeGroup cloudprovider.NodeGroup) []PodEquivalenceGroup
 }
