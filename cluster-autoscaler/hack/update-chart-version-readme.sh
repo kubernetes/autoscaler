f341558f1d8fa6abbc8ad41e3965d9d2eeb60e60
mewa <marcin.k.chmiel@gmail.com>
2024-04-08 14:15:17 +0200

    Copyright boilerplate

--- cluster-autoscaler/hack/update-chart-version-readme.sh
+++ cluster-autoscaler/hack/update-chart-version-readme.sh
@@ -1,4 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Copyright 2024 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###
+# This script is to be used to update chart compatibility matrix in
+# README when there's a new version of the helm chart available
+###
+
 set -eo pipefail
 
 if [[ -n $(git status -s) ]]; then
