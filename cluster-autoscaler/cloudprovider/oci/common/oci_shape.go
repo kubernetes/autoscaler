5bd7dca455f2e381970c398e043fe37c3e157217
Robert Heinzmann <robert.heinzmann@goto.com>
2024-07-02 05:24:37 +0200

    fixing non flex shapes

--- cluster-autoscaler/cloudprovider/oci/common/oci_shape.go
+++ cluster-autoscaler/cloudprovider/oci/common/oci_shape.go
@@ -114,6 +114,7 @@ func (osf *shapeGetterImpl) GetNodePoolShape(np *oke.NodePool) (*Shape, error) {
 	// Update the cache based on latest results
 	for _, s := range resp.Items {
 		osf.cache[*s.Shape] = &Shape{
+			Name:          shapeName,
 			CPU:           getFloat32(s.Ocpus) * 2, // convert ocpu to vcpu
 			GPU:           getInt(s.Gpus),
 			MemoryInBytes: getFloat32(s.MemoryInGBs) * 1024 * 1024 * 1024,
