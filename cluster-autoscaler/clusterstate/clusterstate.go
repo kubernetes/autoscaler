2138a74d501d87b502452b4b11aaed357354c08f
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-08-17 14:22:32 +0100

    chore(issue-7071): added verbosity levels
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- cluster-autoscaler/clusterstate/clusterstate.go
+++ cluster-autoscaler/clusterstate/clusterstate.go
@@ -645,9 +645,7 @@ func (csr *ClusterStateRegistry) updateReadinessStats(currentTime time.Time) {
 			continue
 		}
 		if nodeGroup == nil || reflect.ValueOf(nodeGroup).IsNil() {
-			if klog.V(10).Enabled() {
-				klog.Warningf("Nodegroup is nil for %s", unregistered.Node.Name)
-			}
+			klog.V(4).Infof("Nodegroup is nil for %s", unregistered.Node.Name)
 			continue
 		}
 		perNgCopy := perNodeGroup[nodeGroup.Id()]
@@ -1054,7 +1052,7 @@ func (csr *ClusterStateRegistry) hasCloudProviderInstance(node *apiv1.Node) bool
 	if err == nil {
 		return exists
 	}
-	if !errors.Is(err, cloudprovider.ErrNotImplemented) && klog.V(10).Enabled() {
+	if !errors.Is(err, cloudprovider.ErrNotImplemented) && klog.V(4).Enabled() {
 		klog.Warningf("Failed to check cloud provider has instance for %s: %v", node.Name, err)
 	}
 	return !taints.HasToBeDeletedTaint(node)
