12a6d214e308bcd460727f90ccd29c1d6eea5e4a
Youn Jae Kim <younjaekim@microsoft.com>
2024-08-20 17:33:03 -0700

    lint

--- cluster-autoscaler/cloudprovider/azure/azure_template_test.go
+++ cluster-autoscaler/cloudprovider/azure/azure_template_test.go
@@ -20,9 +20,7 @@ import (
 	"fmt"
 	"testing"
 
-	//nolint SA1019 - deprecated package
-
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2022-03-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2022-03-01/compute" //nolint SA1019 - deprecated package
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/stretchr/testify/assert"
