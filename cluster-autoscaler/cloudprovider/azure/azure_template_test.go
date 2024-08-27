189c231568a5099a0bb16e18487293e78962712f
Charlie McBride <33269602+charliedmcb@users.noreply.github.com>
2024-08-27 20:54:50 +0000

    add existing tests

--- cluster-autoscaler/cloudprovider/azure/azure_template_test.go
+++ cluster-autoscaler/cloudprovider/azure/azure_template_test.go
@@ -18,6 +18,7 @@ package azure
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2022-08-01/compute"
@@ -104,6 +105,45 @@ func TestExtractTaintsFromScaleSet(t *testing.T) {
 	assert.Equal(t, makeTaintSet(expectedTaints), makeTaintSet(taints))
 }
 
+func TestExtractTaintsFromSpecString(t *testing.T) {
+	taintsString := []string{
+		"dedicated=foo:NoSchedule",
+		"group=bar:NoExecute",
+		"app=fizz:PreferNoSchedule",
+		"k8s.io/testing/underscore/to/slash=fizz:PreferNoSchedule",
+		"bar=baz",
+		"blank=",
+		"nosplit=some_value",
+	}
+
+	expectedTaints := []apiv1.Taint{
+		{
+			Key:    "dedicated",
+			Value:  "foo",
+			Effect: apiv1.TaintEffectNoSchedule,
+		},
+		{
+			Key:    "group",
+			Value:  "bar",
+			Effect: apiv1.TaintEffectNoExecute,
+		},
+		{
+			Key:    "app",
+			Value:  "fizz",
+			Effect: apiv1.TaintEffectPreferNoSchedule,
+		},
+		{
+			Key:    "k8s.io/testing/underscore/to/slash",
+			Value:  "fizz",
+			Effect: apiv1.TaintEffectPreferNoSchedule,
+		},
+	}
+
+	taints := extractTaintsFromSpecString(strings.Join(taintsString, ","))
+	assert.Len(t, taints, 4)
+	assert.Equal(t, makeTaintSet(expectedTaints), makeTaintSet(taints))
+}
+
 func TestExtractAllocatableResourcesFromScaleSet(t *testing.T) {
 	tags := map[string]*string{
 		fmt.Sprintf("%s%s", nodeResourcesTagName, "cpu"):                        to.StringPtr("100m"),
@@ -123,14 +163,6 @@ func TestExtractAllocatableResourcesFromScaleSet(t *testing.T) {
 	assert.Equal(t, (&exepectedCustomAllocatable).String(), labels["nvidia.com/Tesla-P100-PCIE"].String())
 }
 
-func makeTaintSet(taints []apiv1.Taint) map[apiv1.Taint]bool {
-	set := make(map[apiv1.Taint]bool)
-	for _, taint := range taints {
-		set[taint] = true
-	}
-	return set
-}
-
 func TestTopologyFromScaleSet(t *testing.T) {
 	testNodeName := "test-node"
 	testSkuName := "test-sku"
@@ -144,12 +176,16 @@ func TestTopologyFromScaleSet(t *testing.T) {
 		Location: to.StringPtr("westus"),
 	}
 	expectedZoneValues := []string{"westus-1", "westus-2", "westus-3"}
+
 	labels := buildGenericLabels(testVmss, testNodeName)
+	failureDomain, ok := labels[apiv1.LabelZoneFailureDomain]
+	assert.True(t, ok)
 	topologyZone, ok := labels[apiv1.LabelTopologyZone]
 	assert.True(t, ok)
 	azureDiskTopology, ok := labels[azureDiskTopologyKey]
 	assert.True(t, ok)
 
+	assert.Contains(t, expectedZoneValues, failureDomain)
 	assert.Contains(t, expectedZoneValues, topologyZone)
 	assert.Contains(t, expectedZoneValues, azureDiskTopology)
 }
@@ -165,9 +201,15 @@ func TestEmptyTopologyFromScaleSet(t *testing.T) {
 			VirtualMachineProfile: &compute.VirtualMachineScaleSetVMProfile{OsProfile: nil}},
 		Location: to.StringPtr("westus"),
 	}
+
+	expectedFailureDomain := "0"
 	expectedTopologyZone := "0"
 	expectedAzureDiskTopology := ""
 	labels := buildGenericLabels(testVmss, testNodeName)
+
+	failureDomain, ok := labels[apiv1.LabelZoneFailureDomain]
+	assert.True(t, ok)
+	assert.Equal(t, expectedFailureDomain, failureDomain)
 
 	topologyZone, ok := labels[apiv1.LabelTopologyZone]
 	assert.True(t, ok)
@@ -176,4 +218,12 @@ func TestEmptyTopologyFromScaleSet(t *testing.T) {
 	azureDiskTopology, ok := labels[azureDiskTopologyKey]
 	assert.True(t, ok)
 	assert.Equal(t, expectedAzureDiskTopology, azureDiskTopology)
+}
+
+func makeTaintSet(taints []apiv1.Taint) map[apiv1.Taint]bool {
+	set := make(map[apiv1.Taint]bool)
+	for _, taint := range taints {
+		set[taint] = true
+	}
+	return set
 }
