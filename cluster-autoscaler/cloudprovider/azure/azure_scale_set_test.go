836fedd85a04e241ddd1fe87e7fc75aa444f2c6c
Rachel Gregory <regex@berkeley.edu>
2024-09-05 21:18:51 +0000

    re-add enableDynamicInstanceList

--- cluster-autoscaler/cloudprovider/azure/azure_scale_set_test.go
+++ cluster-autoscaler/cloudprovider/azure/azure_scale_set_test.go
@@ -1115,6 +1115,8 @@ func TestTemplateNodeInfo(t *testing.T) {
 	asg.Name = "test-asg"
 
 	t.Run("Checking fallback to static because dynamic list is empty", func(t *testing.T) {
+		asg.enableDynamicInstanceList = true
+
 		nodeInfo, err := asg.TemplateNodeInfo()
 		assert.NoError(t, err)
 		assert.NotNil(t, nodeInfo)
@@ -1127,6 +1129,8 @@ func TestTemplateNodeInfo(t *testing.T) {
 	// override GetVMSSTypeDynamically and GetVMSSTypeStatically functions.
 
 	t.Run("Checking dynamic workflow", func(t *testing.T) {
+		asg.enableDynamicInstanceList = true
+
 		GetVMSSTypeDynamically = func(template compute.VirtualMachineScaleSet, azCache *azureCache) (InstanceType, error) {
 			vmssType := InstanceType{}
 			vmssType.VCPU = 1
@@ -1143,6 +1147,8 @@ func TestTemplateNodeInfo(t *testing.T) {
 	})
 
 	t.Run("Checking static workflow if dynamic fails", func(t *testing.T) {
+		asg.enableDynamicInstanceList = true
+
 		GetVMSSTypeDynamically = func(template compute.VirtualMachineScaleSet, azCache *azureCache) (InstanceType, error) {
 			return InstanceType{}, fmt.Errorf("dynamic error exists")
 		}
@@ -1162,6 +1168,8 @@ func TestTemplateNodeInfo(t *testing.T) {
 	})
 
 	t.Run("Fails to find vmss instance information using static and dynamic workflow, instance not supported", func(t *testing.T) {
+		asg.enableDynamicInstanceList = true
+
 		GetVMSSTypeDynamically = func(template compute.VirtualMachineScaleSet, azCache *azureCache) (InstanceType, error) {
 			return InstanceType{}, fmt.Errorf("dynamic error exists")
 		}
@@ -1176,6 +1184,8 @@ func TestTemplateNodeInfo(t *testing.T) {
 	// Note: static-only workflow tests can be removed once support for dynamic is always on
 
 	t.Run("Checking static-only workflow", func(t *testing.T) {
+		asg.enableDynamicInstanceList = false
+
 		GetVMSSTypeStatically = func(template compute.VirtualMachineScaleSet) (*InstanceType, error) {
 			vmssType := InstanceType{}
 			vmssType.VCPU = 1
@@ -1192,6 +1202,8 @@ func TestTemplateNodeInfo(t *testing.T) {
 	})
 
 	t.Run("Checking static-only workflow with built-in SKU list", func(t *testing.T) {
+		asg.enableDynamicInstanceList = false
+
 		nodeInfo, err := asg.TemplateNodeInfo()
 		assert.NoError(t, err)
 		assert.NotNil(t, nodeInfo)
