sarnaik007/cluster-autoscaler

cloudprovider-specific nodegroupset
Signed-off-by: Jack Francis <jackfrancis@gmail.com>

cluster-autoscaler/cloudprovider/aws/aws_nodegroups_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Kubernetes Authors.
+Copyright 2024 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,13 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package nodegroupset
+package aws
 
 import (
 	"testing"
 
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/processors/nodegroupset"
 	. "k8s.io/autoscaler/cluster-autoscaler/utils/test"
 )
 
@@ -169,14 +170,14 @@ func TestIsAwsNodeInfoSimilar(t *testing.T) {
 			if tc.removeOneLabel {
 				delete(node2.ObjectMeta.Labels, tc.label)
 			}
-			checkNodesSimilar(t, node1, node2, comparator, true)
+			nodegroupset.CheckNodesSimilar(t, node1, node2, comparator, true)
 		})
 	}
 }
 
 func TestFindSimilarNodeGroupsAwsBasic(t *testing.T) {
 	context := &context.AutoscalingContext{}
-	ni1, ni2, ni3 := buildBasicNodeGroups(context)
-	processor := &BalancingNodeGroupSetProcessor{Comparator: CreateAwsNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
-	basicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
+	ni1, ni2, ni3 := nodegroupset.BuildBasicNodeGroups(context)
+	processor := &nodegroupset.BalancingNodeGroupSetProcessor{Comparator: CreateAwsNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
+	nodegroupset.BasicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
 }

cluster-autoscaler/cloudprovider/azure/azure_nodegroups_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Kubernetes Authors.
+Copyright 2024 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package nodegroupset
+package azure
 
 import (
 	"testing"
@@ -23,6 +23,7 @@ import (
 	testprovider "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/test"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/processors/nodegroupset"
 	. "k8s.io/autoscaler/cluster-autoscaler/utils/test"
 	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework"
 
@@ -37,68 +38,68 @@ func TestIsAzureNodeInfoSimilar(t *testing.T) {
 	n2 := BuildTestNode("node2", 1000, 2000)
 	n2.ObjectMeta.Labels["test-label"] = "test-value"
 	// No node-pool labels.
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Empty agentpool labels
 	n1.ObjectMeta.Labels["agentpool"] = ""
 	n2.ObjectMeta.Labels["agentpool"] = ""
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// AKS agentpool labels
 	n1.ObjectMeta.Labels["kubernetes.azure.com/agentpool"] = "foo"
 	n2.ObjectMeta.Labels["kubernetes.azure.com/agentpool"] = "bar"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Only one non empty
 	n1.ObjectMeta.Labels["agentpool"] = ""
 	n2.ObjectMeta.Labels["agentpool"] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Only one present
 	delete(n1.ObjectMeta.Labels, "agentpool")
 	n2.ObjectMeta.Labels["agentpool"] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Different vales
 	n1.ObjectMeta.Labels["agentpool"] = "foo1"
 	n2.ObjectMeta.Labels["agentpool"] = "foo2"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Same values
 	n1.ObjectMeta.Labels["agentpool"] = "foo"
 	n2.ObjectMeta.Labels["agentpool"] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Same labels except for agentpool
 	delete(n1.ObjectMeta.Labels, "character")
 	n1.ObjectMeta.Labels["agentpool"] = "foo"
 	n2.ObjectMeta.Labels["agentpool"] = "bar"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Different creationSource
 	n1.ObjectMeta.Labels["creationSource"] = "aks-aks-nodepool2-vmss"
 	n2.ObjectMeta.Labels["creationSource"] = "aks-aks-nodepool3-vmss"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Different node image version
 	n1.ObjectMeta.Labels["kubernetes.azure.com/node-image-version"] = "AKSUbuntu-1804gen2-2021.01.28"
 	n2.ObjectMeta.Labels["kubernetes.azure.com/node-image-version"] = "AKSUbuntu-1804gen2-2022.01.30"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Custom label
 	n1.ObjectMeta.Labels["example.com/ready"] = "true"
 	n2.ObjectMeta.Labels["example.com/ready"] = "false"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// One node with aksConsolidatedAdditionalProperties label
 	n1.ObjectMeta.Labels[aksConsolidatedAdditionalProperties] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Same aksConsolidatedAdditionalProperties
 	n2.ObjectMeta.Labels[aksConsolidatedAdditionalProperties] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Different aksConsolidatedAdditionalProperties label
 	n2.ObjectMeta.Labels[aksConsolidatedAdditionalProperties] = "bar"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 }
 
 func TestFindSimilarNodeGroupsAzureBasic(t *testing.T) {
 	context := &context.AutoscalingContext{}
-	ni1, ni2, ni3 := buildBasicNodeGroups(context)
-	processor := &BalancingNodeGroupSetProcessor{Comparator: CreateAzureNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
-	basicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
+	ni1, ni2, ni3 := nodegroupset.BuildBasicNodeGroups(context)
+	processor := &nodegroupset.BalancingNodeGroupSetProcessor{Comparator: CreateAzureNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
+	nodegroupset.BasicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
 }
 
 func TestFindSimilarNodeGroupsAzureByLabel(t *testing.T) {
-	processor := &BalancingNodeGroupSetProcessor{Comparator: CreateAzureNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
+	processor := &nodegroupset.BalancingNodeGroupSetProcessor{Comparator: CreateAzureNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
 	context := &context.AutoscalingContext{}
 
 	n1 := BuildTestNode("n1", 1000, 1000)

cluster-autoscaler/main.go
@@ -46,7 +46,10 @@ import (
 	"k8s.io/apiserver/pkg/server/routes"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/aws"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/azure"
 	cloudBuilder "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/builder"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce/localssdsize"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/core"
@@ -547,12 +550,12 @@ func buildAutoscaler(debuggingSnapshotter debuggingsnapshot.DebuggingSnapshotter
 	} else {
 		nodeInfoComparatorBuilder := nodegroupset.CreateGenericNodeInfoComparator
 		if autoscalingOptions.CloudProviderName == cloudprovider.AzureProviderName {
-			nodeInfoComparatorBuilder = nodegroupset.CreateAzureNodeInfoComparator
+			nodeInfoComparatorBuilder = azure.CreateAzureNodeInfoComparator
 		} else if autoscalingOptions.CloudProviderName == cloudprovider.AwsProviderName {
-			nodeInfoComparatorBuilder = nodegroupset.CreateAwsNodeInfoComparator
+			nodeInfoComparatorBuilder = aws.CreateAwsNodeInfoComparator
 			opts.Processors.TemplateNodeInfoProvider = nodeinfosprovider.NewAsgTagResourceNodeInfoProvider(nodeInfoCacheExpireTime, *forceDaemonSets)
 		} else if autoscalingOptions.CloudProviderName == cloudprovider.GceProviderName {
-			nodeInfoComparatorBuilder = nodegroupset.CreateGceNodeInfoComparator
+			nodeInfoComparatorBuilder = gce.CreateGceNodeInfoComparator
 			opts.Processors.TemplateNodeInfoProvider = nodeinfosprovider.NewAnnotationNodeInfoProvider(nodeInfoCacheExpireTime, *forceDaemonSets)
 		}
 		nodeInfoComparator = nodeInfoComparatorBuilder(autoscalingOptions.BalancingExtraIgnoredLabels, autoscalingOptions.NodeGroupSetRatios)

kubernetes/autoscaler

cluster-autoscaler/cloudprovider/aws/aws_nodegroups_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Kubernetes Authors.
+Copyright 2024 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,18 +14,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package nodegroupset
+package aws
 
 import (
 	"testing"
 
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/processors/nodegroupset"
 	. "k8s.io/autoscaler/cluster-autoscaler/utils/test"
 )
 
 func TestIsAwsNodeInfoSimilar(t *testing.T) {
-	comparator := CreateAwsNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})
+	comparator := CreateNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})
 	node1 := BuildTestNode("node1", 1000, 2000)
 	node2 := BuildTestNode("node2", 1000, 2000)
 
@@ -169,14 +170,14 @@ func TestIsAwsNodeInfoSimilar(t *testing.T) {
 			if tc.removeOneLabel {
 				delete(node2.ObjectMeta.Labels, tc.label)
 			}
-			checkNodesSimilar(t, node1, node2, comparator, true)
+			nodegroupset.CheckNodesSimilar(t, node1, node2, comparator, true)
 		})
 	}
 }
 
 func TestFindSimilarNodeGroupsAwsBasic(t *testing.T) {
 	context := &context.AutoscalingContext{}
-	ni1, ni2, ni3 := buildBasicNodeGroups(context)
-	processor := &BalancingNodeGroupSetProcessor{Comparator: CreateAwsNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
-	basicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
+	ni1, ni2, ni3 := nodegroupset.BuildBasicNodeGroups(context)
+	processor := &nodegroupset.BalancingNodeGroupSetProcessor{Comparator: CreateNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
+	nodegroupset.BasicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
 }

cluster-autoscaler/cloudprovider/azure/azure_nodegroups_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Kubernetes Authors.
+Copyright 2024 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package nodegroupset
+package azure
 
 import (
 	"testing"
@@ -23,6 +23,7 @@ import (
 	testprovider "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/test"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/processors/nodegroupset"
 	. "k8s.io/autoscaler/cluster-autoscaler/utils/test"
 	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework"
 
@@ -30,75 +31,75 @@ import (
 )
 
 func TestIsAzureNodeInfoSimilar(t *testing.T) {
-	comparator := CreateAzureNodeInfoComparator([]string{"example.com/ready"}, config.NodeGroupDifferenceRatios{})
+	comparator := CreateNodeInfoComparator([]string{"example.com/ready"}, config.NodeGroupDifferenceRatios{})
 	n1 := BuildTestNode("node1", 1000, 2000)
 	n1.ObjectMeta.Labels["test-label"] = "test-value"
 	n1.ObjectMeta.Labels["character"] = "thing"
 	n2 := BuildTestNode("node2", 1000, 2000)
 	n2.ObjectMeta.Labels["test-label"] = "test-value"
 	// No node-pool labels.
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Empty agentpool labels
 	n1.ObjectMeta.Labels["agentpool"] = ""
 	n2.ObjectMeta.Labels["agentpool"] = ""
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// AKS agentpool labels
 	n1.ObjectMeta.Labels["kubernetes.azure.com/agentpool"] = "foo"
 	n2.ObjectMeta.Labels["kubernetes.azure.com/agentpool"] = "bar"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Only one non empty
 	n1.ObjectMeta.Labels["agentpool"] = ""
 	n2.ObjectMeta.Labels["agentpool"] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Only one present
 	delete(n1.ObjectMeta.Labels, "agentpool")
 	n2.ObjectMeta.Labels["agentpool"] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Different vales
 	n1.ObjectMeta.Labels["agentpool"] = "foo1"
 	n2.ObjectMeta.Labels["agentpool"] = "foo2"
-	checkNodesSimilar(t, n1, n2, comparator, false)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, false)
 	// Same values
 	n1.ObjectMeta.Labels["agentpool"] = "foo"
 	n2.ObjectMeta.Labels["agentpool"] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Same labels except for agentpool
 	delete(n1.ObjectMeta.Labels, "character")
 	n1.ObjectMeta.Labels["agentpool"] = "foo"
 	n2.ObjectMeta.Labels["agentpool"] = "bar"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Different creationSource
 	n1.ObjectMeta.Labels["creationSource"] = "aks-aks-nodepool2-vmss"
 	n2.ObjectMeta.Labels["creationSource"] = "aks-aks-nodepool3-vmss"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Different node image version
 	n1.ObjectMeta.Labels["kubernetes.azure.com/node-image-version"] = "AKSUbuntu-1804gen2-2021.01.28"
 	n2.ObjectMeta.Labels["kubernetes.azure.com/node-image-version"] = "AKSUbuntu-1804gen2-2022.01.30"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Custom label
 	n1.ObjectMeta.Labels["example.com/ready"] = "true"
 	n2.ObjectMeta.Labels["example.com/ready"] = "false"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// One node with aksConsolidatedAdditionalProperties label
 	n1.ObjectMeta.Labels[aksConsolidatedAdditionalProperties] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Same aksConsolidatedAdditionalProperties
 	n2.ObjectMeta.Labels[aksConsolidatedAdditionalProperties] = "foo"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 	// Different aksConsolidatedAdditionalProperties label
 	n2.ObjectMeta.Labels[aksConsolidatedAdditionalProperties] = "bar"
-	checkNodesSimilar(t, n1, n2, comparator, true)
+	nodegroupset.CheckNodesSimilar(t, n1, n2, comparator, true)
 }
 
 func TestFindSimilarNodeGroupsAzureBasic(t *testing.T) {
 	context := &context.AutoscalingContext{}
-	ni1, ni2, ni3 := buildBasicNodeGroups(context)
-	processor := &BalancingNodeGroupSetProcessor{Comparator: CreateAzureNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
-	basicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
+	ni1, ni2, ni3 := nodegroupset.BuildBasicNodeGroups(context)
+	processor := &nodegroupset.BalancingNodeGroupSetProcessor{Comparator: CreateNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
+	nodegroupset.BasicSimilarNodeGroupsTest(t, context, processor, ni1, ni2, ni3)
 }
 
 func TestFindSimilarNodeGroupsAzureByLabel(t *testing.T) {
-	processor := &BalancingNodeGroupSetProcessor{Comparator: CreateAzureNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
+	processor := &nodegroupset.BalancingNodeGroupSetProcessor{Comparator: CreateNodeInfoComparator([]string{}, config.NodeGroupDifferenceRatios{})}
 	context := &context.AutoscalingContext{}
 
 	n1 := BuildTestNode("n1", 1000, 1000)

cluster-autoscaler/main.go
@@ -47,7 +47,10 @@ import (
 	"k8s.io/apiserver/pkg/server/routes"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/aws"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/azure"
 	cloudBuilder "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/builder"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce/localssdsize"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/core"
@@ -568,12 +571,12 @@ func buildAutoscaler(debuggingSnapshotter debuggingsnapshot.DebuggingSnapshotter
 	} else {
 		nodeInfoComparatorBuilder := nodegroupset.CreateGenericNodeInfoComparator
 		if autoscalingOptions.CloudProviderName == cloudprovider.AzureProviderName {
-			nodeInfoComparatorBuilder = nodegroupset.CreateAzureNodeInfoComparator
+			nodeInfoComparatorBuilder = azure.CreateNodeInfoComparator
 		} else if autoscalingOptions.CloudProviderName == cloudprovider.AwsProviderName {
-			nodeInfoComparatorBuilder = nodegroupset.CreateAwsNodeInfoComparator
+			nodeInfoComparatorBuilder = aws.CreateNodeInfoComparator
 			opts.Processors.TemplateNodeInfoProvider = nodeinfosprovider.NewAsgTagResourceNodeInfoProvider(nodeInfoCacheExpireTime, *forceDaemonSets)
 		} else if autoscalingOptions.CloudProviderName == cloudprovider.GceProviderName {
-			nodeInfoComparatorBuilder = nodegroupset.CreateGceNodeInfoComparator
+			nodeInfoComparatorBuilder = gce.CreateGceNodeInfoComparator
 			opts.Processors.TemplateNodeInfoProvider = nodeinfosprovider.NewAnnotationNodeInfoProvider(nodeInfoCacheExpireTime, *forceDaemonSets)
 		}
 		nodeInfoComparator = nodeInfoComparatorBuilder(autoscalingOptions.BalancingExtraIgnoredLabels, autoscalingOptions.NodeGroupSetRatios)