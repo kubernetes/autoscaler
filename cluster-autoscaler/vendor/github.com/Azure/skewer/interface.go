48889cbe054cd3ab46f76d2d4b9f3d6b943ca8cc
Rachel Gregory <regex@berkeley.edu>
2024-09-16 16:09:19 +0000

    azure vendor updates

--- cluster-autoscaler/vendor/github.com/Azure/skewer/interface.go
+++ cluster-autoscaler/vendor/github.com/Azure/skewer/interface.go
@@ -3,22 +3,22 @@ package skewer
 import (
 	"context"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2022-08-01/compute" //nolint:staticcheck
 )
 
 // ResourceClient is the required Azure client interface used to populate skewer's data.
 type ResourceClient interface {
-	ListComplete(ctx context.Context, filter string) (compute.ResourceSkusResultIterator, error)
+	ListComplete(ctx context.Context, filter, includeExtendedLocations string) (compute.ResourceSkusResultIterator, error)
 }
 
 // ResourceProviderClient is a convenience interface for uses cases
 // specific to Azure resource providers.
 type ResourceProviderClient interface {
-	List(ctx context.Context, filter string) (compute.ResourceSkusResultPage, error)
+	List(ctx context.Context, filter, includeExtendedLocations string) (compute.ResourceSkusResultPage, error)
 }
 
 // client defines the internal interface required by the skewer Cache.
 // TODO(ace): implement a lazy iterator with caching (and a cursor?)
 type client interface {
-	List(ctx context.Context, filter string) ([]compute.ResourceSku, error)
+	List(ctx context.Context, filter, includeExtendedLocations string) ([]compute.ResourceSku, error)
 }
