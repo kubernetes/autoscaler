anatmiller/KubernetesScaling

CA: extract a Handle to scheduleframework.Framework out of PredicateChecker
This decouples PredicateChecker from the Framework initialization logic,
and allows creating multiple PredicateChecker instances while only
initializing the framework once.

This commit also fixes how CA integrates with Framework metrics. Instead
of Registering them they're only Initialized so that CA doesn't expose
scheduler metrics. And the initialization is moved from multiple
different places to the Handle constructor.

cluster-autoscaler/simulator/framework/handle.go
@@ -1,0 +1,70 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package framework
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/client-go/informers"
+	"k8s.io/kubernetes/pkg/scheduler/apis/config"
+	scheduler_config "k8s.io/kubernetes/pkg/scheduler/apis/config/latest"
+	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework"
+	scheduler_plugins "k8s.io/kubernetes/pkg/scheduler/framework/plugins"
+	schedulerframeworkruntime "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
+	schedulermetrics "k8s.io/kubernetes/pkg/scheduler/metrics"
+)
+
+// Handle is meant for interacting with the scheduler framework.
+type Handle struct {
+	Framework        schedulerframework.Framework
+	DelegatingLister *DelegatingSchedulerSharedLister
+}
+
+// NewHandle builds a framework Handle based on the provided informers and scheduler config.
+func NewHandle(informerFactory informers.SharedInformerFactory, schedConfig *config.KubeSchedulerConfiguration) (*Handle, error) {
+	if schedConfig == nil {
+		var err error
+		schedConfig, err = scheduler_config.Default()
+		if err != nil {
+			return nil, fmt.Errorf("couldn't create scheduler config: %v", err)
+		}
+	}
+
+	if len(schedConfig.Profiles) != 1 {
+		return nil, fmt.Errorf("unexpected scheduler config: expected one scheduler profile only (found %d profiles)", len(schedConfig.Profiles))
+	}
+	sharedLister := NewDelegatingSchedulerSharedLister()
+
+	schedulermetrics.InitMetrics()
+	framework, err := schedulerframeworkruntime.NewFramework(
+		context.TODO(),
+		scheduler_plugins.NewInTreeRegistry(),
+		&schedConfig.Profiles[0],
+		schedulerframeworkruntime.WithInformerFactory(informerFactory),
+		schedulerframeworkruntime.WithSnapshotSharedLister(sharedLister),
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create scheduler framework; %v", err)
+	}
+
+	return &Handle{
+		Framework:        framework,
+		DelegatingLister: sharedLister,
+	}, nil
+}

kubernetes/autoscaler

cluster-autoscaler/simulator/framework/handle.go
@@ -1,0 +1,70 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package framework
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/client-go/informers"
+	schedulerconfig "k8s.io/kubernetes/pkg/scheduler/apis/config"
+	schedulerconfiglatest "k8s.io/kubernetes/pkg/scheduler/apis/config/latest"
+	schedulerframework "k8s.io/kubernetes/pkg/scheduler/framework"
+	schedulerplugins "k8s.io/kubernetes/pkg/scheduler/framework/plugins"
+	schedulerframeworkruntime "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
+	schedulermetrics "k8s.io/kubernetes/pkg/scheduler/metrics"
+)
+
+// Handle is meant for interacting with the scheduler framework.
+type Handle struct {
+	Framework        schedulerframework.Framework
+	DelegatingLister *DelegatingSchedulerSharedLister
+}
+
+// NewHandle builds a framework Handle based on the provided informers and scheduler config.
+func NewHandle(informerFactory informers.SharedInformerFactory, schedConfig *schedulerconfig.KubeSchedulerConfiguration) (*Handle, error) {
+	if schedConfig == nil {
+		var err error
+		schedConfig, err = schedulerconfiglatest.Default()
+		if err != nil {
+			return nil, fmt.Errorf("couldn't create scheduler config: %v", err)
+		}
+	}
+
+	if len(schedConfig.Profiles) != 1 {
+		return nil, fmt.Errorf("unexpected scheduler config: expected one scheduler profile only (found %d profiles)", len(schedConfig.Profiles))
+	}
+	sharedLister := NewDelegatingSchedulerSharedLister()
+
+	schedulermetrics.InitMetrics()
+	framework, err := schedulerframeworkruntime.NewFramework(
+		context.TODO(),
+		schedulerplugins.NewInTreeRegistry(),
+		&schedConfig.Profiles[0],
+		schedulerframeworkruntime.WithInformerFactory(informerFactory),
+		schedulerframeworkruntime.WithSnapshotSharedLister(sharedLister),
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create scheduler framework; %v", err)
+	}
+
+	return &Handle{
+		Framework:        framework,
+		DelegatingLister: sharedLister,
+	}, nil
+}