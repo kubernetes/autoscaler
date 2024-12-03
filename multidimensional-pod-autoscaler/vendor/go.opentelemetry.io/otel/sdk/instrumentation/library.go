10a48ed17147a1c3bafb332ada7ba395925b532d
David van der Spek <david.vanderspek@flyrlabs.com>
2024-12-03 09:42:42 +0100

    cleanup: temporarily remove vendored files
    Signed-off-by: David van der Spek <david.vanderspek@flyrlabs.com>

--- multidimensional-pod-autoscaler/vendor/go.opentelemetry.io/otel/sdk/instrumentation/library.go
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-package instrumentation // import "go.opentelemetry.io/otel/sdk/instrumentation"
-
-// Library represents the instrumentation library.
-// Deprecated: please use Scope instead.
-type Library = Scope
