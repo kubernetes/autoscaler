anatmiller/KubernetesScaling

CA: plumb the DRA flag guard to PredicateSnapshot

cluster-autoscaler/core/autoscaler.go
@@ -124,7 +124,7 @@ func initializeDefaultOptions(opts *AutoscalerOptions, informerFactory informers
 		opts.FrameworkHandle = fwHandle
 	}
 	if opts.ClusterSnapshot == nil {
-		opts.ClusterSnapshot = predicate.NewPredicateSnapshot(base.NewBasicSnapshotBase(), opts.FrameworkHandle)
+		opts.ClusterSnapshot = predicate.NewPredicateSnapshot(base.NewBasicSnapshotBase(), opts.FrameworkHandle, opts.DynamicResourceAllocationEnabled)
 	}
 	if opts.RemainingPdbTracker == nil {
 		opts.RemainingPdbTracker = pdb.NewBasicRemainingPdbTracker()

cluster-autoscaler/core/scaledown/actuation/actuator.go
@@ -358,7 +358,7 @@ func (a *Actuator) taintNode(node *apiv1.Node) error {
 }
 
 func (a *Actuator) createSnapshot(nodes []*apiv1.Node) (clustersnapshot.ClusterSnapshot, error) {
-	snapshot := predicate.NewPredicateSnapshot(base.NewBasicSnapshotBase(), a.ctx.FrameworkHandle)
+	snapshot := predicate.NewPredicateSnapshot(base.NewBasicSnapshotBase(), a.ctx.FrameworkHandle, a.ctx.DynamicResourceAllocationEnabled)
 	pods, err := a.ctx.AllPodLister().List()
 	if err != nil {
 		return nil, err

cluster-autoscaler/main.go
@@ -506,7 +506,7 @@ func buildAutoscaler(context ctx.Context, debuggingSnapshotter debuggingsnapshot
 	opts := core.AutoscalerOptions{
 		AutoscalingOptions:   autoscalingOptions,
 		FrameworkHandle:      fwHandle,
-		ClusterSnapshot:      predicate.NewPredicateSnapshot(base.NewDeltaSnapshotBase(), fwHandle),
+		ClusterSnapshot:      predicate.NewPredicateSnapshot(base.NewDeltaSnapshotBase(), fwHandle, autoscalingOptions.DynamicResourceAllocationEnabled),
 		KubeClient:           kubeClient,
 		InformerFactory:      informerFactory,
 		DebuggingSnapshotter: debuggingSnapshotter,

cluster-autoscaler/simulator/clustersnapshot/predicate/predicate_snapshot.go
@@ -27,13 +27,15 @@ import (
 type PredicateSnapshot struct {
 	clustersnapshot.SnapshotBase
 	pluginRunner *SchedulerPluginRunner
+	draEnabled   bool
 }
 
 // NewPredicateSnapshot builds a PredicateSnapshot.
-func NewPredicateSnapshot(snapshotBase clustersnapshot.SnapshotBase, fwHandle *framework.Handle) *PredicateSnapshot {
+func NewPredicateSnapshot(snapshotBase clustersnapshot.SnapshotBase, fwHandle *framework.Handle, draEnabled bool) *PredicateSnapshot {
 	return &PredicateSnapshot{
 		SnapshotBase: snapshotBase,
 		pluginRunner: NewSchedulerPluginRunner(fwHandle, snapshotBase),
+		draEnabled:   draEnabled,
 	}
 }
 

cluster-autoscaler/simulator/clustersnapshot/testsnapshot/test_snapshot.go
@@ -34,7 +34,7 @@ func NewTestSnapshot() (clustersnapshot.ClusterSnapshot, error) {
 	if err != nil {
 		return nil, err
 	}
-	return predicate.NewPredicateSnapshot(base.NewBasicSnapshotBase(), testFwHandle), nil
+	return predicate.NewPredicateSnapshot(base.NewBasicSnapshotBase(), testFwHandle, true), nil
 }
 
 // NewTestSnapshotOrDie returns an instance of ClusterSnapshot that can be used in tests.
@@ -52,7 +52,7 @@ func NewCustomTestSnapshot(snapshot clustersnapshot.SnapshotBase) (clustersnapsh
 	if err != nil {
 		return nil, err
 	}
-	return predicate.NewPredicateSnapshot(snapshot, testFwHandle), nil
+	return predicate.NewPredicateSnapshot(snapshot, testFwHandle, true), nil
 }
 
 // NewCustomTestSnapshotOrDie returns an instance of ClusterSnapshot with a specific SnapshotBase that can be used in tests.

kubernetes/autoscaler

cluster-autoscaler/core/autoscaler.go
@@ -124,7 +124,7 @@ func initializeDefaultOptions(opts *AutoscalerOptions, informerFactory informers
 		opts.FrameworkHandle = fwHandle
 	}
 	if opts.ClusterSnapshot == nil {
-		opts.ClusterSnapshot = predicate.NewPredicateSnapshot(store.NewBasicSnapshotStore(), opts.FrameworkHandle)
+		opts.ClusterSnapshot = predicate.NewPredicateSnapshot(store.NewBasicSnapshotStore(), opts.FrameworkHandle, opts.DynamicResourceAllocationEnabled)
 	}
 	if opts.RemainingPdbTracker == nil {
 		opts.RemainingPdbTracker = pdb.NewBasicRemainingPdbTracker()

cluster-autoscaler/core/scaledown/actuation/actuator.go
@@ -358,7 +358,7 @@ func (a *Actuator) taintNode(node *apiv1.Node) error {
 }
 
 func (a *Actuator) createSnapshot(nodes []*apiv1.Node) (clustersnapshot.ClusterSnapshot, error) {
-	snapshot := predicate.NewPredicateSnapshot(store.NewBasicSnapshotStore(), a.ctx.FrameworkHandle)
+	snapshot := predicate.NewPredicateSnapshot(store.NewBasicSnapshotStore(), a.ctx.FrameworkHandle, a.ctx.DynamicResourceAllocationEnabled)
 	pods, err := a.ctx.AllPodLister().List()
 	if err != nil {
 		return nil, err

cluster-autoscaler/main.go
@@ -506,7 +506,7 @@ func buildAutoscaler(context ctx.Context, debuggingSnapshotter debuggingsnapshot
 	opts := core.AutoscalerOptions{
 		AutoscalingOptions:   autoscalingOptions,
 		FrameworkHandle:      fwHandle,
-		ClusterSnapshot:      predicate.NewPredicateSnapshot(store.NewDeltaSnapshotStore(), fwHandle),
+		ClusterSnapshot:      predicate.NewPredicateSnapshot(store.NewDeltaSnapshotStore(), fwHandle, autoscalingOptions.DynamicResourceAllocationEnabled),
 		KubeClient:           kubeClient,
 		InformerFactory:      informerFactory,
 		DebuggingSnapshotter: debuggingSnapshotter,

cluster-autoscaler/simulator/clustersnapshot/predicate/predicate_snapshot.go
@@ -27,13 +27,15 @@ import (
 type PredicateSnapshot struct {
 	clustersnapshot.ClusterSnapshotStore
 	pluginRunner *SchedulerPluginRunner
+	draEnabled   bool
 }
 
 // NewPredicateSnapshot builds a PredicateSnapshot.
-func NewPredicateSnapshot(snapshotStore clustersnapshot.ClusterSnapshotStore, fwHandle *framework.Handle) *PredicateSnapshot {
+func NewPredicateSnapshot(snapshotStore clustersnapshot.ClusterSnapshotStore, fwHandle *framework.Handle, draEnabled bool) *PredicateSnapshot {
 	return &PredicateSnapshot{
 		ClusterSnapshotStore: snapshotStore,
 		pluginRunner:         NewSchedulerPluginRunner(fwHandle, snapshotStore),
+		draEnabled:           draEnabled,
 	}
 }
 

cluster-autoscaler/simulator/clustersnapshot/testsnapshot/test_snapshot.go
@@ -34,7 +34,7 @@ func NewTestSnapshot() (clustersnapshot.ClusterSnapshot, error) {
 	if err != nil {
 		return nil, err
 	}
-	return predicate.NewPredicateSnapshot(store.NewBasicSnapshotStore(), testFwHandle), nil
+	return predicate.NewPredicateSnapshot(store.NewBasicSnapshotStore(), testFwHandle, true), nil
 }
 
 // NewTestSnapshotOrDie returns an instance of ClusterSnapshot that can be used in tests.
@@ -52,7 +52,7 @@ func NewCustomTestSnapshot(snapshotStore clustersnapshot.ClusterSnapshotStore) (
 	if err != nil {
 		return nil, err
 	}
-	return predicate.NewPredicateSnapshot(snapshotStore, testFwHandle), nil
+	return predicate.NewPredicateSnapshot(snapshotStore, testFwHandle, true), nil
 }
 
 // NewCustomTestSnapshotOrDie returns an instance of ClusterSnapshot with a specific ClusterSnapshotStore that can be used in tests.