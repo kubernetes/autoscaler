anatmiller/KubernetesScaling

CA: extend SchedulerPluginRunner with RunReserveOnNode
RunReserveOnNode runs the Reserve phase of schedulerframework,
which is necessary to obtain ResourceClaim allocations computed
by the DRA scheduler plugin.

RunReserveOnNode isn't used anywhere yet, so this should be a no-op.

cluster-autoscaler/simulator/clustersnapshot/predicate/plugin_runner_test.go
@@ -144,8 +144,10 @@ func TestRunFiltersOnNode(t *testing.T) {
 			err = snapshot.AddNodeInfo(framework.NewTestNodeInfo(tt.node, tt.scheduledPods...))
 			assert.NoError(t, err)
 
-			predicateError := pluginRunner.RunFiltersOnNode(tt.testPod, tt.node.Name)
+			node, state, predicateError := pluginRunner.RunFiltersOnNode(tt.testPod, tt.node.Name)
 			if tt.expectError {
+				assert.Nil(t, node)
+				assert.Nil(t, state)
 				assert.NotNil(t, predicateError)
 				assert.Equal(t, clustersnapshot.FailingPredicateError, predicateError.Type())
 				assert.Equal(t, "NodeResourcesFit", predicateError.FailingPredicateName())
@@ -154,6 +156,8 @@ func TestRunFiltersOnNode(t *testing.T) {
 				assert.Contains(t, predicateError.Error(), "Insufficient cpu")
 			} else {
 				assert.Nil(t, predicateError)
+				assert.NotNil(t, state)
+				assert.Equal(t, tt.node, node)
 			}
 		})
 	}
@@ -243,12 +247,15 @@ func TestRunFilterUntilPassingNode(t *testing.T) {
 			err = snapshot.AddNodeInfo(framework.NewTestNodeInfo(n2000))
 			assert.NoError(t, err)
 
-			nodeName, err := pluginRunner.RunFiltersUntilPassingNode(tc.pod, func(info *framework.NodeInfo) bool { return true })
+			node, state, err := pluginRunner.RunFiltersUntilPassingNode(tc.pod, func(info *framework.NodeInfo) bool { return true })
 			if tc.expectError {
+				assert.Nil(t, node)
+				assert.Nil(t, state)
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
-				assert.Contains(t, tc.expectedNodes, nodeName)
+				assert.NotNil(t, state)
+				assert.Contains(t, tc.expectedNodes, node.Name)
 			}
 		})
 	}
@@ -278,7 +285,7 @@ func TestDebugInfo(t *testing.T) {
 	err = clusterSnapshot.AddNodeInfo(framework.NewTestNodeInfo(node1))
 	assert.NoError(t, err)
 
-	predicateErr := defaultPluginnRunner.RunFiltersOnNode(p1, "n1")
+	_, _, predicateErr := defaultPluginnRunner.RunFiltersOnNode(p1, "n1")
 	assert.NotNil(t, predicateErr)
 	assert.Contains(t, predicateErr.FailingPredicateReasons(), "node(s) had untolerated taint {SomeTaint: WhyNot?}")
 	assert.Contains(t, predicateErr.Error(), "node(s) had untolerated taint {SomeTaint: WhyNot?}")
@@ -308,7 +315,7 @@ func TestDebugInfo(t *testing.T) {
 	err = clusterSnapshot.AddNodeInfo(framework.NewTestNodeInfo(node1))
 	assert.NoError(t, err)
 
-	predicateErr = customPluginnRunner.RunFiltersOnNode(p1, "n1")
+	_, _, predicateErr = customPluginnRunner.RunFiltersOnNode(p1, "n1")
 	assert.Nil(t, predicateErr)
 }
 

kubernetes/autoscaler

cluster-autoscaler/simulator/clustersnapshot/predicate/plugin_runner_test.go
@@ -144,8 +144,10 @@ func TestRunFiltersOnNode(t *testing.T) {
 			err = snapshot.AddNodeInfo(framework.NewTestNodeInfo(tt.node, tt.scheduledPods...))
 			assert.NoError(t, err)
 
-			predicateError := pluginRunner.RunFiltersOnNode(tt.testPod, tt.node.Name)
+			node, state, predicateError := pluginRunner.RunFiltersOnNode(tt.testPod, tt.node.Name)
 			if tt.expectError {
+				assert.Nil(t, node)
+				assert.Nil(t, state)
 				assert.NotNil(t, predicateError)
 				assert.Equal(t, clustersnapshot.FailingPredicateError, predicateError.Type())
 				assert.Equal(t, "NodeResourcesFit", predicateError.FailingPredicateName())
@@ -154,6 +156,8 @@ func TestRunFiltersOnNode(t *testing.T) {
 				assert.Contains(t, predicateError.Error(), "Insufficient cpu")
 			} else {
 				assert.Nil(t, predicateError)
+				assert.NotNil(t, state)
+				assert.Equal(t, tt.node, node)
 			}
 		})
 	}
@@ -243,12 +247,15 @@ func TestRunFilterUntilPassingNode(t *testing.T) {
 			err = snapshot.AddNodeInfo(framework.NewTestNodeInfo(n2000))
 			assert.NoError(t, err)
 
-			nodeName, err := pluginRunner.RunFiltersUntilPassingNode(tc.pod, func(info *framework.NodeInfo) bool { return true })
+			node, state, err := pluginRunner.RunFiltersUntilPassingNode(tc.pod, func(info *framework.NodeInfo) bool { return true })
 			if tc.expectError {
+				assert.Nil(t, node)
+				assert.Nil(t, state)
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
-				assert.Contains(t, tc.expectedNodes, nodeName)
+				assert.NotNil(t, state)
+				assert.Contains(t, tc.expectedNodes, node.Name)
 			}
 		})
 	}
@@ -278,7 +285,7 @@ func TestDebugInfo(t *testing.T) {
 	err = clusterSnapshot.AddNodeInfo(framework.NewTestNodeInfo(node1))
 	assert.NoError(t, err)
 
-	predicateErr := defaultPluginRunner.RunFiltersOnNode(p1, "n1")
+	_, _, predicateErr := defaultPluginRunner.RunFiltersOnNode(p1, "n1")
 	assert.NotNil(t, predicateErr)
 	assert.Contains(t, predicateErr.FailingPredicateReasons(), "node(s) had untolerated taint {SomeTaint: WhyNot?}")
 	assert.Contains(t, predicateErr.Error(), "node(s) had untolerated taint {SomeTaint: WhyNot?}")
@@ -308,7 +315,7 @@ func TestDebugInfo(t *testing.T) {
 	err = clusterSnapshot.AddNodeInfo(framework.NewTestNodeInfo(node1))
 	assert.NoError(t, err)
 
-	predicateErr = customPluginRunner.RunFiltersOnNode(p1, "n1")
+	_, _, predicateErr = customPluginRunner.RunFiltersOnNode(p1, "n1")
 	assert.Nil(t, predicateErr)
 }
 