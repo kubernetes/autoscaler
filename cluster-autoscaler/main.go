9946461d076bdf0d71d091c5b579b8dba1944ccf
idebeijer <71566757+idebeijer@users.noreply.github.com>
2025-01-23 21:47:41 +0100

    fix: move leader election flag binding above flag parsing which happens in kube_flag.InitFlags()

--- cluster-autoscaler/main.go
+++ cluster-autoscaler/main.go
@@ -698,6 +698,9 @@ func main() {
 		klog.Fatalf("Failed to add logging feature flags: %v", err)
 	}
 
+	leaderElection := leaderElectionConfiguration()
+	componentopts.BindLeaderElectionFlags(&leaderElection, pflag.CommandLine)
+
 	logsapi.AddFlags(loggingConfig, pflag.CommandLine)
 	featureGate.AddFlag(pflag.CommandLine)
 	kube_flag.InitFlags()
@@ -710,9 +713,6 @@ func main() {
 			klog.Fatalf("couldn't enable the DRA feature gate: %v", err)
 		}
 	}
-
-	leaderElection := leaderElectionConfiguration()
-	componentopts.BindLeaderElectionFlags(&leaderElection, pflag.CommandLine)
 
 	logs.InitLogs()
 	if err := logsapi.ValidateAndApply(loggingConfig, featureGate); err != nil {
