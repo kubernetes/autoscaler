11498429294c7799c81ab64a4e8e12b4c0a7abfd
Daniel Kłobuszewski <sephiroxien@gmail.com>
2025-01-31 17:22:07 +0100

    Drop unused gomock import

--- cluster-autoscaler/cloudprovider/azure/azure_scale_set_instance_cache_test.go
+++ cluster-autoscaler/cloudprovider/azure/azure_scale_set_instance_cache_test.go
@@ -21,7 +21,6 @@ import (
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2022-08-01/compute"
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
