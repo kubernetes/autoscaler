6a6df9ad86fe4399968a7d5e7ca2e895e8496f61
eric-higgins-ai <erichiggins@applied.co>
2025-03-05 20:34:24 -0800

    Address comment

--- cluster-autoscaler/cloudprovider/oci/nodepools/cache.go
+++ cluster-autoscaler/cloudprovider/oci/nodepools/cache.go
@@ -76,7 +76,9 @@ func (c *nodePoolCache) removeInstance(nodePoolID, instanceID string, nodeName s
 	if instanceID == "" {
 		klog.Errorf("Node %s doesn't have an instance id so it can't be deleted.", nodeName)
 		klog.Errorf("This could be due to a Compute Instance issue in OCI such as Out Of Host Capacity error. Check the instance status on OCI Console.")
-		return errors.Errorf("Node %s doesn't have an instance id so it can't be deleted.", nodeName)
+		// We silently fail here because returning an error will cause the cluster autoscaler to stop its reconciliation loop, preventing other
+		// node pools from autoscaling.
+		return nil
 	}
 
 	klog.Infof("Deleting instance %q from node pool %q", instanceID, nodePoolID)
