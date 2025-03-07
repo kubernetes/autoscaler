2a777c8e4a69cc0df55d8472f66409d4f02fcd73
Max Cao <macao@redhat.com>
2025-03-07 15:36:11 -0800

    VPA: Add features gates; add InPlaceVerticalScaling feature gate
    Signed-off-by: Max Cao <macao@redhat.com>

--- vertical-pod-autoscaler/pkg/updater/main.go
+++ vertical-pod-autoscaler/pkg/updater/main.go
@@ -38,6 +38,7 @@ import (
 	"k8s.io/autoscaler/vertical-pod-autoscaler/common"
 	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/admission-controller/resource/pod/patch"
 	vpa_clientset "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/client/clientset/versioned"
+	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/features"
 	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/target"
 	controllerfetcher "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/target/controller_fetcher"
 	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/updater/inplace"
@@ -89,6 +90,8 @@ func main() {
 
 	leaderElection := defaultLeaderElectionConfiguration()
 	componentbaseoptions.BindLeaderElectionFlags(&leaderElection, pflag.CommandLine)
+
+	features.MutableFeatureGate.AddFlag(pflag.CommandLine)
 
 	kube_flag.InitFlags()
 	klog.V(1).InfoS("Vertical Pod Autoscaler Updater", "version", common.VerticalPodAutoscalerVersion())
