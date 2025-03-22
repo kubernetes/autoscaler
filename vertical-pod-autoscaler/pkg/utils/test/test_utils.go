c3c8177e0095763b12b69358832dff0d680eecb2
Max Cao <macao@redhat.com>
2025-03-21 19:00:01 -0700

    VPA: Updater in-place updates unit tests
    Signed-off-by: Max Cao <macao@redhat.com>

--- vertical-pod-autoscaler/pkg/utils/test/test_utils.go
+++ vertical-pod-autoscaler/pkg/utils/test/test_utils.go
@@ -121,6 +121,24 @@ func (m *PodsEvictionRestrictionMock) CanEvict(pod *apiv1.Pod) bool {
 	return args.Bool(0)
 }
 
+// InPlaceUpdate is a mock implementation of PodsEvictionRestriction.InPlaceUpdate
+func (m *PodsEvictionRestrictionMock) InPlaceUpdate(pod *apiv1.Pod, vpa *vpa_types.VerticalPodAutoscaler, eventRecorder record.EventRecorder) error {
+	args := m.Called(pod, eventRecorder)
+	return args.Error(0)
+}
+
+// CanInPlaceUpdate is a mock implementation of PodsEvictionRestriction.CanInPlaceUpdate
+func (m *PodsEvictionRestrictionMock) CanInPlaceUpdate(pod *apiv1.Pod) bool {
+	args := m.Called(pod)
+	return args.Bool(0)
+}
+
+// IsInPlaceUpdating is a mock implementation of PodsEvictionRestriction.IsInPlaceUpdating
+func (m *PodsEvictionRestrictionMock) IsInPlaceUpdating(pod *apiv1.Pod) bool {
+	args := m.Called(pod)
+	return args.Bool(0)
+}
+
 // PodListerMock is a mock of PodLister
 type PodListerMock struct {
 	mock.Mock
