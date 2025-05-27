13f0b04129faf80aa3bd40dc33b3982246771781
MenD32 <amit.mendelevitch@gmail.com>
2025-05-27 21:31:47 +0300

    docs: added helpful comments
    Signed-off-by: MenD32 <amit.mendelevitch@gmail.com>

--- cluster-autoscaler/simulator/cluster.go
+++ cluster-autoscaler/simulator/cluster.go
@@ -227,6 +227,7 @@ func (r *RemovalSimulator) findPlaceFor(removedNode string, pods []*apiv1.Pod, n
 			klog.Errorf("Simulating removal of %s/%s return error; %v", pod.Namespace, pod.Name, err)
 		}
 	}
+	// Remove the node from the snapshot, so that it doesn't interfere with topology spread constraint scheduling.
 	r.clusterSnapshot.RemoveNodeInfo(removedNode)
 
 	newpods := make([]*apiv1.Pod, 0, len(pods))
