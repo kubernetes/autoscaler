beab2fcb98a6e645b41ab4dda10996294329a8be
Julian Lawrence <julianlawrence@gmail.com>
2025-09-05 12:36:49 -0700

    adding feature flag `NativeSidecar`

--- vertical-pod-autoscaler/pkg/features/versioned_features.go
+++ vertical-pod-autoscaler/pkg/features/versioned_features.go
@@ -34,4 +34,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	PerVPAConfig: {
 		{Version: version.MustParse("1.5"), Default: false, PreRelease: featuregate.Alpha},
 	},
+	NativeSidecar: {
+		{Version: version.MustParse("1.5"), Default: false, PreRelease: featuregate.Alpha},
+	},
 }
