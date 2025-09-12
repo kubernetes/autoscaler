b8a57cb9bc15d03ba9e60291c524fd259b6d5549
Jack Francis <jackfrancis@gmail.com>
2025-09-12 12:45:33 -0700

    Update deps for CA 1.33.1 release

--- cluster-autoscaler/version/version.go
+++ cluster-autoscaler/version/version.go
@@ -17,4 +17,4 @@ limitations under the License.
 package version
 
 // ClusterAutoscalerVersion contains version of CA.
-const ClusterAutoscalerVersion = "1.33.0"
+const ClusterAutoscalerVersion = "1.33.1"
