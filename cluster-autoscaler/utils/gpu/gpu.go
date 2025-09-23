0cb81e35ef6cd38f9450560955a3b58e2e2efb7b
Maksym Fuhol <mfuhol@google.com>
2025-09-23 09:40:12 +0000

    Add handling for DRA GPUs exposed in GetGpuInfoForMetrics

--- cluster-autoscaler/utils/gpu/gpu.go
+++ cluster-autoscaler/utils/gpu/gpu.go
@@ -17,6 +17,8 @@ limitations under the License.
 package gpu
 
 import (
+	"fmt"
+
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
 	podutils "k8s.io/autoscaler/cluster-autoscaler/utils/pod"
@@ -56,11 +58,18 @@ func GetGpuInfoForMetrics(gpuConfig *cloudprovider.GpuConfig, availableGPUTypes 
 	if gpuConfig == nil {
 		return "", MetricsNoGPU
 	}
+
 	resourceName := gpuConfig.ExtendedResourceName
 	capacity, capacityFound := node.Status.Capacity[resourceName]
 	// There is no label value, fallback to generic solution
 	if gpuConfig.Type == "" && capacityFound && !capacity.IsZero() {
 		return resourceName.String(), MetricsGenericGPU
+	}
+
+	// GPU is exposed using DRA, capacity won't be present
+	if gpuConfig.ExposedViaDra() {
+		draResourceName := fmt.Sprintf("dra_%s", gpuConfig.DraDriverName)
+		return draResourceName, validateGpuType(availableGPUTypes, gpuConfig.Type)
 	}
 
 	// GKE-specific label & capacity are present - consistent state
