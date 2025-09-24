9617e1cdd2047949bf7e3e430bce0cf8cb9c452a
Tetiana Yeremenko <tyeremenko@google.com>
2025-09-24 13:20:30 +0000

    fix linting issues

--- cluster-autoscaler/core/scaledown/latencytracker/node_latency_tracker_test.go
+++ cluster-autoscaler/core/scaledown/latencytracker/node_latency_tracker_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -7,8 +7,8 @@ You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an "AS IS" BASIS,
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
@@ -29,7 +29,7 @@ func TestNodeLatencyTracker(t *testing.T) {
 
 	tests := []struct {
 		name                  string
-		setupNodes            map[string]NodeInfo
+		setupNodes            map[string]nodeInfo
 		unneededList          []string
 		currentlyInDeletion   map[string]bool
 		updateThresholds      map[string]time.Duration
@@ -39,7 +39,7 @@ func TestNodeLatencyTracker(t *testing.T) {
 	}{
 		{
 			name:                 "add new unneeded nodes",
-			setupNodes:           map[string]NodeInfo{},
+			setupNodes:           map[string]nodeInfo{},
 			unneededList:         []string{"node1", "node2"},
 			currentlyInDeletion:  map[string]bool{},
 			updateThresholds:     map[string]time.Duration{},
@@ -48,7 +48,7 @@ func TestNodeLatencyTracker(t *testing.T) {
 		},
 		{
 			name: "observe deletion with threshold",
-			setupNodes: map[string]NodeInfo{
+			setupNodes: map[string]nodeInfo{
 				"node1": {UnneededSince: baseTime, Threshold: 2 * time.Second},
 			},
 			unneededList:         []string{},
@@ -62,7 +62,7 @@ func TestNodeLatencyTracker(t *testing.T) {
 		},
 		{
 			name: "remove unneeded node not in deletion",
-			setupNodes: map[string]NodeInfo{
+			setupNodes: map[string]nodeInfo{
 				"node1": {UnneededSince: baseTime, Threshold: 1 * time.Second},
 				"node2": {UnneededSince: baseTime, Threshold: 0},
 			},
@@ -77,7 +77,7 @@ func TestNodeLatencyTracker(t *testing.T) {
 		},
 		{
 			name: "update threshold",
-			setupNodes: map[string]NodeInfo{
+			setupNodes: map[string]nodeInfo{
 				"node1": {UnneededSince: baseTime, Threshold: 1 * time.Second},
 			},
 			unneededList:        []string{"node1"},
