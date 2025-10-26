124fd102eb16a0538201e1255c13fcb4551e3b5e
Adrian Moisey <adrian@changeover.za.net>
2025-10-26 17:17:39 +0200

    Remove unused const

--- vertical-pod-autoscaler/e2e/v1/e2e_test.go
+++ vertical-pod-autoscaler/e2e/v1/e2e_test.go
@@ -34,8 +34,6 @@ import (
 	"k8s.io/kubernetes/test/utils/image"
 )
 
-var viperConfig = flag.String("viper-config", "", "The name of a viper config file (https://github.com/spf13/viper#what-is-viper). All e2e command line parameters can also be configured in such a file. May contain a path and may or may not contain the file suffix. The default is to look for an optional file with `e2e` as base name. If a file is specified explicitly, it must be present.")
-
 // handleFlags sets up all flags and parses the command line.
 func handleFlags() {
 	config.CopyFlags(config.Flags, flag.CommandLine)
