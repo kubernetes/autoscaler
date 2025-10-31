c6d08e7cac967ad0ab781dc6b90bc6df6e95324a
Hemant Kumar <hekumar@redhat.com>
2025-10-31 10:41:14 -0400

    Fix to use CSIManager
    Add code to enable new scheduler interface for CSI
    Fix the pointer error
    
    Update the delegating scheduler

--- cluster-autoscaler/simulator/framework/test_utils.go
+++ cluster-autoscaler/simulator/framework/test_utils.go
@@ -44,7 +44,7 @@ func NewTestFrameworkHandle() (*Handle, error) {
 	if err != nil {
 		return nil, err
 	}
-	fwHandle, err := NewHandle(informers.NewSharedInformerFactory(clientsetfake.NewSimpleClientset(), 0), defaultConfig, true)
+	fwHandle, err := NewHandle(informers.NewSharedInformerFactory(clientsetfake.NewSimpleClientset(), 0), defaultConfig, true, true)
 	if err != nil {
 		return nil, err
 	}
