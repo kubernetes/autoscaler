d5897da08884b8f2d2c73601ad5b0a2917651090
Adrian Moisey <adrian@changeover.za.net>
2025-11-05 11:17:23 +0200

    Change junit output to $ARTIFACTS
    It seems that's what prow uses
    
    Asking prow to set WORKSPACE broke the e2e tests, so I assume that
    env-var is already used

--- vertical-pod-autoscaler/hack/run-e2e-locally.sh
+++ vertical-pod-autoscaler/hack/run-e2e-locally.sh
@@ -103,9 +103,9 @@ case ${SUITE} in
 
     echo " ** Running suite ${SUITE}"
     if [ ${SUITE} == recommender-externalmetrics ]; then
-       WORKSPACE=./workspace/_artifacts ${SCRIPT_ROOT}/hack/run-e2e-tests.sh recommender
+       ARTIFACTS=./workspace/_artifacts ${SCRIPT_ROOT}/hack/run-e2e-tests.sh recommender
     else
-      WORKSPACE=./workspace/_artifacts ${SCRIPT_ROOT}/hack/run-e2e-tests.sh ${SUITE}
+      ARTIFACTS=./workspace/_artifacts ${SCRIPT_ROOT}/hack/run-e2e-tests.sh ${SUITE}
     fi
     ;;
   *)
