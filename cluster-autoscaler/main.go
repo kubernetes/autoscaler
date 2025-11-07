f5e40d5464c73022758065d8e6bb9f2ea7ff0ec7
Jack Francis <jackfrancis@gmail.com>
2025-11-07 10:32:11 -0800

    remove comment

--- cluster-autoscaler/main.go
+++ cluster-autoscaler/main.go
@@ -107,7 +107,6 @@ func (flag *MultiStringFlag) Set(value string) error {
 	return nil
 }
 
-// multiStringFlag creates a new MultiStringFlag and registers it with the flag package.
 func multiStringFlag(name string, usage string) *MultiStringFlag {
 	value := new(MultiStringFlag)
 	flag.Var(value, name, usage)
