d30590cf538c7d39bf28e8c99be87531e3a2d868
Norbert Cyran <norbertcyran@google.com>
2025-12-02 17:48:05 +0100

    Add CapacityQuota API

--- cluster-autoscaler/apis/capacityquota/client/clientset/versioned/fake/clientset_generated.go
+++ cluster-autoscaler/apis/capacityquota/client/clientset/versioned/fake/clientset_generated.go
@@ -22,12 +22,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
-	applyconfiguration "k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/client/applyconfiguration"
-	clientset "k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/client/clientset/versioned"
-	autoscalingv1 "k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/client/clientset/versioned/typed/autoscaling.x-k8s.io/v1"
-	fakeautoscalingv1 "k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/client/clientset/versioned/typed/autoscaling.x-k8s.io/v1/fake"
-	autoscalingv1beta1 "k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/client/clientset/versioned/typed/autoscaling.x-k8s.io/v1beta1"
-	fakeautoscalingv1beta1 "k8s.io/autoscaler/cluster-autoscaler/apis/provisioningrequest/client/clientset/versioned/typed/autoscaling.x-k8s.io/v1beta1/fake"
+	applyconfiguration "k8s.io/autoscaler/cluster-autoscaler/apis/capacityquota/client/applyconfiguration"
+	clientset "k8s.io/autoscaler/cluster-autoscaler/apis/capacityquota/client/clientset/versioned"
+	autoscalingv1alpha1 "k8s.io/autoscaler/cluster-autoscaler/apis/capacityquota/client/clientset/versioned/typed/autoscaling.x-k8s.io/v1alpha1"
+	fakeautoscalingv1alpha1 "k8s.io/autoscaler/cluster-autoscaler/apis/capacityquota/client/clientset/versioned/typed/autoscaling.x-k8s.io/v1alpha1/fake"
 	"k8s.io/client-go/discovery"
 	fakediscovery "k8s.io/client-go/discovery/fake"
 	"k8s.io/client-go/testing"
@@ -127,12 +125,7 @@ var (
 	_ testing.FakeClient  = &Clientset{}
 )
 
-// AutoscalingV1 retrieves the AutoscalingV1Client
-func (c *Clientset) AutoscalingV1() autoscalingv1.AutoscalingV1Interface {
-	return &fakeautoscalingv1.FakeAutoscalingV1{Fake: &c.Fake}
-}
-
-// AutoscalingV1beta1 retrieves the AutoscalingV1beta1Client
-func (c *Clientset) AutoscalingV1beta1() autoscalingv1beta1.AutoscalingV1beta1Interface {
-	return &fakeautoscalingv1beta1.FakeAutoscalingV1beta1{Fake: &c.Fake}
+// AutoscalingV1alpha1 retrieves the AutoscalingV1alpha1Client
+func (c *Clientset) AutoscalingV1alpha1() autoscalingv1alpha1.AutoscalingV1alpha1Interface {
+	return &fakeautoscalingv1alpha1.FakeAutoscalingV1alpha1{Fake: &c.Fake}
 }
