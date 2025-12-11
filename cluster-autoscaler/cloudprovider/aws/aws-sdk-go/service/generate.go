5b135d6ea4c7918061830854897761385bcdb16b
dom.bozzuto <dom.bozzuto@datadoghq.com>
2025-12-11 11:39:51 -0500

    Remove aws v1 sdk

--- cluster-autoscaler/cloudprovider/aws/aws-sdk-go/service/generate.go
+++
@@ -1,5 +0,0 @@
-// Package service contains automatically generated AWS clients.
-package service
-
-//go:generate go run -tags codegen ../private/model/cli/gen-api/main.go -use-service-id -path=../service ../models/apis/*/*/api-2.json
-//go:generate gofmt -s -w ../service
--- cluster-autoscaler/cloudprovider/aws/aws-sdk-go/service/s3/platform_handlers.go
+++
@@ -1,9 +0,0 @@
-//go:build !go1.6
-// +build !go1.6
-
-package s3
-
-import "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/aws/aws-sdk-go/aws/request"
-
-func platformRequestHandlers(r *request.Request) {
-}
