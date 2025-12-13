16250232f6d2e94bf5e9fffaeffd40755792ca45
Martyna Grotek <martynagrotek@google.com>
2025-12-13 15:45:15 +0000

    Improve metrics testability

--- cluster-autoscaler/metrics/metrics_test.go
+++ cluster-autoscaler/metrics/metrics_test.go
@@ -21,24 +21,36 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
+	"k8s.io/component-base/metrics"
 )
 
+func newCaMetricsWithRegistry(registry metrics.KubeRegistry) *caMetrics {
+	reg := newCaMetrics()
+	reg.registry = registry
+	return reg
+}
+
 func TestDisabledPerNodeGroupMetrics(t *testing.T) {
-	t.Skip("Registering metrics multiple times causes panic. Skipping until the test is fixed to not impact other tests.")
-	RegisterAll(false)
-	assert.False(t, nodesGroupMinNodes.IsCreated())
-	assert.False(t, nodesGroupMaxNodes.IsCreated())
+	// Use a custom registry for isolation to avoid panics from re-registering metrics.
+	reg := metrics.NewKubeRegistry()
+	assert.NotNil(t, reg)
+	m := newCaMetricsWithRegistry(reg)
+	m.RegisterAll(false)
+	assert.False(t, m.nodesGroupMinNodes.IsCreated())
+	assert.False(t, m.nodesGroupMaxNodes.IsCreated())
 }
 
 func TestEnabledPerNodeGroupMetrics(t *testing.T) {
-	t.Skip("Registering metrics multiple times causes panic. Skipping until the test is fixed to not impact other tests.")
-	RegisterAll(true)
-	assert.True(t, nodesGroupMinNodes.IsCreated())
-	assert.True(t, nodesGroupMaxNodes.IsCreated())
+	// Use a custom registry for isolation
+	reg := metrics.NewKubeRegistry()
+	m := newCaMetricsWithRegistry(reg)
+	m.RegisterAll(true)
+	assert.True(t, m.nodesGroupMinNodes.IsCreated())
+	assert.True(t, m.nodesGroupMaxNodes.IsCreated())
 
-	UpdateNodeGroupMin("foo", 2)
-	UpdateNodeGroupMax("foo", 100)
+	m.UpdateNodeGroupMin("foo", 2)
+	m.UpdateNodeGroupMax("foo", 100)
 
-	assert.Equal(t, 2, int(testutil.ToFloat64(nodesGroupMinNodes.GaugeVec.WithLabelValues("foo"))))
-	assert.Equal(t, 100, int(testutil.ToFloat64(nodesGroupMaxNodes.GaugeVec.WithLabelValues("foo"))))
+	assert.Equal(t, 2, int(testutil.ToFloat64(m.nodesGroupMinNodes.GaugeVec.WithLabelValues("foo"))))
+	assert.Equal(t, 100, int(testutil.ToFloat64(m.nodesGroupMaxNodes.GaugeVec.WithLabelValues("foo"))))
 }
