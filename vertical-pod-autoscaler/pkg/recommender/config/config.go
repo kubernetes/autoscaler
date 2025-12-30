624b164dd2ff9ecf3aff4cdfd50055b86ec5504d
Adrian Moisey <adrian@changeover.za.net>
2025-12-30 17:40:02 +0200

    Refactor updater's flags
    Configure a struct that can be passed around, and populate that with
    CLI flags

---
+++ vertical-pod-autoscaler/pkg/recommender/config/config.go
@@ -0,0 +1,212 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"flag"
+	"os"
+	"strings"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/klog/v2"
+
+	"k8s.io/autoscaler/vertical-pod-autoscaler/common"
+	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/recommender/input"
+	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/recommender/model"
+	"k8s.io/autoscaler/vertical-pod-autoscaler/pkg/recommender/routines"
+)
+
+// RecommenderConfig holds all configuration for the recommender component
+type RecommenderConfig struct {
+	// Common flags
+	CommonFlags *common.CommonFlags
+
+	// Recommender-specific flags
+	RecommenderName        string
+	MetricsFetcherInterval time.Duration
+	CheckpointsGCInterval  time.Duration
+	Address                string
+	Storage                string
+	MemorySaver            bool
+	UpdateWorkerCount      int
+
+	// Prometheus history provider configuration
+	PrometheusAddress         string
+	PrometheusInsecure        bool
+	PrometheusJobName         string
+	HistoryLength             string
+	HistoryResolution         string
+	QueryTimeout              string
+	PodLabelPrefix            string
+	PodLabelsMetricName       string
+	PodNamespaceLabel         string
+	PodNameLabel              string
+	CtrNamespaceLabel         string
+	CtrPodNameLabel           string
+	CtrNameLabel              string
+	Username                  string
+	Password                  string
+	PrometheusBearerToken     string
+	PrometheusBearerTokenFile string
+
+	// External metrics provider configuration
+	UseExternalMetrics   bool
+	ExternalCpuMetric    string
+	ExternalMemoryMetric string
+
+	// Aggregation configuration
+	MemoryAggregationInterval      time.Duration
+	MemoryAggregationIntervalCount int64
+	MemoryHistogramDecayHalfLife   time.Duration
+	CpuHistogramDecayHalfLife      time.Duration
+	OOMBumpUpRatio                 float64
+	OOMMinBumpUp                   float64
+
+	// Post processors configuration
+	PostProcessorCPUasInteger bool
+	MaxAllowedCPU             resource.QuantityValue
+	MaxAllowedMemory          resource.QuantityValue
+}
+
+// DefaultRecommenderConfig returns a RecommenderConfig with default values
+func DefaultRecommenderConfig() *RecommenderConfig {
+	return &RecommenderConfig{
+		CommonFlags: common.DefaultCommonConfig(),
+
+		// Recommender-specific flags
+		RecommenderName:        input.DefaultRecommenderName,
+		MetricsFetcherInterval: 1 * time.Minute,
+		CheckpointsGCInterval:  10 * time.Minute,
+		Address:                ":8942",
+		Storage:                "",
+		MemorySaver:            false,
+		UpdateWorkerCount:      10,
+
+		// Prometheus history provider flags
+		PrometheusAddress:         "http://prometheus.monitoring.svc",
+		PrometheusInsecure:        false,
+		PrometheusJobName:         "kubernetes-cadvisor",
+		HistoryLength:             "8d",
+		HistoryResolution:         "1h",
+		QueryTimeout:              "5m",
+		PodLabelPrefix:            "pod_label_",
+		PodLabelsMetricName:       "up{job=\"kubernetes-pods\"}",
+		PodNamespaceLabel:         "kubernetes_namespace",
+		PodNameLabel:              "kubernetes_pod_name",
+		CtrNamespaceLabel:         "namespace",
+		CtrPodNameLabel:           "pod_name",
+		CtrNameLabel:              "name",
+		Username:                  "",
+		Password:                  "",
+		PrometheusBearerToken:     "",
+		PrometheusBearerTokenFile: "",
+
+		// External metrics provider flags
+		UseExternalMetrics:   false,
+		ExternalCpuMetric:    "",
+		ExternalMemoryMetric: "",
+
+		// Aggregation configuration flags
+		MemoryAggregationInterval:      model.DefaultMemoryAggregationInterval,
+		MemoryAggregationIntervalCount: model.DefaultMemoryAggregationIntervalCount,
+		MemoryHistogramDecayHalfLife:   model.DefaultMemoryHistogramDecayHalfLife,
+		CpuHistogramDecayHalfLife:      model.DefaultCPUHistogramDecayHalfLife,
+		OOMBumpUpRatio:                 model.DefaultOOMBumpUpRatio,
+		OOMMinBumpUp:                   model.DefaultOOMMinBumpUp,
+
+		// Post processors flags
+		PostProcessorCPUasInteger: false,
+		MaxAllowedCPU:             resource.QuantityValue{},
+		MaxAllowedMemory:          resource.QuantityValue{},
+	}
+}
+
+// InitRecommenderFlags initializes flags for the recommender component
+func InitRecommenderFlags() *RecommenderConfig {
+	config := DefaultRecommenderConfig()
+	config.CommonFlags = common.InitCommonFlags()
+
+	flag.StringVar(&config.RecommenderName, "recommender-name", config.RecommenderName, "Set the recommender name. Recommender will generate recommendations for VPAs that configure the same recommender name. If the recommender name is left as default it will also generate recommendations that don't explicitly specify recommender. You shouldn't run two recommenders with the same name in a cluster.")
+	flag.DurationVar(&config.MetricsFetcherInterval, "recommender-interval", config.MetricsFetcherInterval, `How often metrics should be fetched`)
+	flag.DurationVar(&config.CheckpointsGCInterval, "checkpoints-gc-interval", config.CheckpointsGCInterval, `How often orphaned checkpoints should be garbage collected`)
+	flag.StringVar(&config.Address, "address", ":8942", "The address to expose Prometheus metrics.")
+	flag.StringVar(&config.Storage, "storage", config.Storage, `Specifies storage mode. Supported values: prometheus, checkpoint (default)`)
+	flag.BoolVar(&config.MemorySaver, "memory-saver", false, `If true, only track pods which have an associated VPA`)
+	flag.IntVar(&config.UpdateWorkerCount, "update-worker-count", 10, "Number of concurrent workers to update VPA recommendations and checkpoints. When increasing this setting, make sure the client-side rate limits ('kube-api-qps' and 'kube-api-burst') are either increased or turned off as well. Determines the minimum number of VPA checkpoints written per recommender loop.")
+
+	// Prometheus history provider flags
+	flag.StringVar(&config.PrometheusAddress, "prometheus-address", config.PrometheusAddress, `Where to reach for Prometheus metrics`)
+	flag.BoolVar(&config.PrometheusInsecure, "prometheus-insecure", config.PrometheusInsecure, `Skip tls verify if https is used in the prometheus-address`)
+	flag.StringVar(&config.PrometheusJobName, "prometheus-cadvisor-job-name", config.PrometheusJobName, `Name of the prometheus job name which scrapes the cAdvisor metrics`)
+	flag.StringVar(&config.HistoryLength, "history-length", config.HistoryLength, `How much time back prometheus have to be queried to get historical metrics`)
+	flag.StringVar(&config.HistoryResolution, "history-resolution", config.HistoryResolution, `Resolution at which Prometheus is queried for historical metrics`)
+	flag.StringVar(&config.QueryTimeout, "prometheus-query-timeout", config.QueryTimeout, `How long to wait before killing long queries`)
+	flag.StringVar(&config.PodLabelPrefix, "pod-label-prefix", config.PodLabelPrefix, `Which prefix to look for pod labels in metrics`)
+	flag.StringVar(&config.PodLabelsMetricName, "metric-for-pod-labels", config.PodLabelsMetricName, `Which metric to look for pod labels in metrics`)
+	flag.StringVar(&config.PodNamespaceLabel, "pod-namespace-label", config.PodNamespaceLabel, `Label name to look for pod namespaces`)
+	flag.StringVar(&config.PodNameLabel, "pod-name-label", config.PodNameLabel, `Label name to look for pod names`)
+	flag.StringVar(&config.CtrNamespaceLabel, "container-namespace-label", config.CtrNamespaceLabel, `Label name to look for container namespaces`)
+	flag.StringVar(&config.CtrPodNameLabel, "container-pod-name-label", config.CtrPodNameLabel, `Label name to look for container pod names`)
+	flag.StringVar(&config.CtrNameLabel, "container-name-label", config.CtrNameLabel, `Label name to look for container names`)
+	flag.StringVar(&config.Username, "username", config.Username, "The username used in the prometheus server basic auth. Can also be set via the PROMETHEUS_USERNAME environment variable")
+	flag.StringVar(&config.Password, "password", config.Password, "The password used in the prometheus server basic auth. Can also be set via the PROMETHEUS_PASSWORD environment variable")
+	flag.StringVar(&config.PrometheusBearerToken, "prometheus-bearer-token", config.PrometheusBearerToken, "The bearer token used in the Prometheus server bearer token auth")
+	flag.StringVar(&config.PrometheusBearerTokenFile, "prometheus-bearer-token-file", config.PrometheusBearerTokenFile, "Path to the bearer token file used for authentication by the Prometheus server")
+
+	// External metrics provider flags
+	flag.BoolVar(&config.UseExternalMetrics, "use-external-metrics", config.UseExternalMetrics, "ALPHA.  Use an external metrics provider instead of metrics_server.")
+	flag.StringVar(&config.ExternalCpuMetric, "external-metrics-cpu-metric", config.ExternalCpuMetric, "ALPHA.  Metric to use with external metrics provider for CPU usage.")
+	flag.StringVar(&config.ExternalMemoryMetric, "external-metrics-memory-metric", config.ExternalMemoryMetric, "ALPHA.  Metric to use with external metrics provider for memory usage.")
+
+	// Aggregation configuration flags
+	flag.DurationVar(&config.MemoryAggregationInterval, "memory-aggregation-interval", config.MemoryAggregationInterval, `The length of a single interval, for which the peak memory usage is computed. Memory usage peaks are aggregated in multiples of this interval. In other words there is one memory usage sample per interval (the maximum usage over that interval)`)
+	flag.Int64Var(&config.MemoryAggregationIntervalCount, "memory-aggregation-interval-count", config.MemoryAggregationIntervalCount, `The number of consecutive memory-aggregation-intervals which make up the MemoryAggregationWindowLength which in turn is the period for memory usage aggregation by VPA. In other words, MemoryAggregationWindowLength = memory-aggregation-interval * memory-aggregation-interval-count.`)
+	flag.DurationVar(&config.MemoryHistogramDecayHalfLife, "memory-histogram-decay-half-life", config.MemoryHistogramDecayHalfLife, `The amount of time it takes a historical memory usage sample to lose half of its weight. In other words, a fresh usage sample is twice as 'important' as one with age equal to the half life period.`)
+	flag.DurationVar(&config.CpuHistogramDecayHalfLife, "cpu-histogram-decay-half-life", config.CpuHistogramDecayHalfLife, `The amount of time it takes a historical CPU usage sample to lose half of its weight.`)
+	flag.Float64Var(&config.OOMBumpUpRatio, "oom-bump-up-ratio", config.OOMBumpUpRatio, `Default memory bump up ratio when OOM occurs. This value applies to all VPAs unless overridden in the VPA spec. Default is 1.2.`)
+	flag.Float64Var(&config.OOMMinBumpUp, "oom-min-bump-up-bytes", config.OOMMinBumpUp, `Default minimal increase of memory (in bytes) when OOM occurs. This value applies to all VPAs unless overridden in the VPA spec. Default is 100 * 1024 * 1024 (100Mi).`)
+
+	// Post processors flags
+	// CPU as integer to benefit for CPU management Static Policy ( https://kubernetes.io/docs/tasks/administer-cluster/cpu-management-policies/#static-policy )
+	flag.BoolVar(&config.PostProcessorCPUasInteger, "cpu-integer-post-processor-enabled", config.PostProcessorCPUasInteger, "Enable the cpu-integer recommendation post processor. The post processor will round up CPU recommendations to a whole CPU for pods which were opted in by setting an appropriate label on VPA object (experimental)")
+	flag.Var(&config.MaxAllowedCPU, "container-recommendation-max-allowed-cpu", "Maximum amount of CPU that will be recommended for a container. VerticalPodAutoscaler-level maximum allowed takes precedence over the global maximum allowed.")
+	flag.Var(&config.MaxAllowedMemory, "container-recommendation-max-allowed-memory", "Maximum amount of memory that will be recommended for a container. VerticalPodAutoscaler-level maximum allowed takes precedence over the global maximum allowed.")
+
+	return config
+}
+
+// ValidateRecommenderConfig performs validation of the recommender flags
+func ValidateRecommenderConfig(config *RecommenderConfig) {
+	if *routines.MinCheckpointsPerRun != 10 { // Default value is 10
+		klog.InfoS("DEPRECATION WARNING: The 'min-checkpoints' flag is deprecated and has no effect. It will be removed in a future release.")
+	}
+
+	if config.PrometheusBearerToken != "" && config.PrometheusBearerTokenFile != "" && config.Username != "" {
+		klog.ErrorS(nil, "--bearer-token, --bearer-token-file and --username are mutually exclusive and can't be set together.")
+		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	if config.PrometheusBearerTokenFile != "" {
+		fileContent, err := os.ReadFile(config.PrometheusBearerTokenFile)
+		if err != nil {
+			klog.ErrorS(err, "Unable to read bearer token file", "filename", config.PrometheusBearerTokenFile)
+			klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+		}
+		config.PrometheusBearerTokenFile = strings.TrimSpace(string(fileContent))
+	}
+}
