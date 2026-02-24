c61f50bf49c11fe5c305c1d4285e83210cd87649
Norbert Cyran <norbertcyran@google.com>
2026-02-24 16:16:40 +0100

    Hide server dry run behind a CLI flag
    Server dry run requires a create permission for pods. If it's enabled by
     default, we will break capacity buffers for the users without that
     permission configured.

--- cluster-autoscaler/capacitybuffer/fakepods/resolver.go
+++ cluster-autoscaler/capacitybuffer/fakepods/resolver.go
@@ -20,42 +20,10 @@ import (
 	"context"
 
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/klog/v2"
-
-	podutils "k8s.io/autoscaler/cluster-autoscaler/utils/pod"
 )
 
-// Resolver creates a fully defaulted and validated pod using server-side dry-run.
+// Resolver resolves a pod based on the provided template.
 type Resolver interface {
-	// Resolve returns a fake pod based on the provided template.
-	// It performs a dry-run create request to the API server.
+	// Resolve resolves a pod based on the provided template.
 	Resolve(ctx context.Context, namespace string, template *corev1.PodTemplateSpec) (*corev1.Pod, error)
-}
-
-type fakePodResolver struct {
-	client kubernetes.Interface
-}
-
-// NewResolver returns a new instance of Resolver.
-func NewResolver(client kubernetes.Interface) Resolver {
-	return &fakePodResolver{
-		client: client,
-	}
-}
-
-func (r *fakePodResolver) Resolve(ctx context.Context, namespace string, template *corev1.PodTemplateSpec) (*corev1.Pod, error) {
-	pod := podutils.GetPodFromTemplate(template)
-	pod.GenerateName = "fake-pod-"
-
-	createdPod, err := r.client.CoreV1().Pods(namespace).Create(ctx, pod, metav1.CreateOptions{
-		DryRun: []string{metav1.DryRunAll},
-	})
-	if err != nil {
-		klog.Errorf("Failed to create dry-run pod for template %s/%s: %v", template.Namespace, template.Name, err)
-		return nil, err
-	}
-
-	return createdPod, nil
 }
