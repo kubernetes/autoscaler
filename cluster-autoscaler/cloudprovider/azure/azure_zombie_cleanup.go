5f13570e265049fddeaf9872cd2f8df60fdb660a
alimaazamat <alima.azamat2003@gmail.com>
2026-03-12 16:02:58 -0700

    preserve error chaining with %w in fmt.Errorf

--- cluster-autoscaler/cloudprovider/azure/azure_zombie_cleanup.go
+++ cluster-autoscaler/cloudprovider/azure/azure_zombie_cleanup.go
@@ -79,7 +79,7 @@ func (m *AzureManager) cleanupZombieNodesWithContext(nodes []*apiv1.Node) error 
 	scaleSets, err := m.azClient.virtualMachineScaleSetsClient.List(ctx, m.config.ResourceGroup)
 	if err != nil {
 		klog.Errorf("Failed to list VMSS: %v", err)
-		return fmt.Errorf("failed to list VMSS: %v", err)
+		return fmt.Errorf("failed to list VMSS: %w", err)
 	}
 
 	// Build K8s node lookup table
