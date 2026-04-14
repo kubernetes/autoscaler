dc8f15817a4bf3fcec3dad839ca6294778fc7371
Maksym Fuhol <mfuhol@google.com>
2026-04-14 13:01:57 +0000

    Adapt DRA snapshot claim tracker to new scheduler API
    Reference PR: https://github.com/kubernetes/kubernetes/pull/137897

---
+++ cluster-autoscaler/simulator/dynamicresources/snapshot/snapshot_pod_group_lister.go
@@ -0,0 +1,36 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package snapshot
+
+import (
+	"errors"
+
+	schedulingapi "k8s.io/api/scheduling/v1alpha2"
+)
+
+var (
+	errorGettingPodGroup = errors.New("PodGroups with DRA resource claims are not supported for autoscaling")
+)
+
+type snapshotPodGroupLister struct {
+	snapshot *Snapshot
+}
+
+// TODO(autoscaler/issues/9570): Actually handle PodGroups, design: KEP-5729
+func (sl snapshotPodGroupLister) Get(namespace, podGroupName string) (*schedulingapi.PodGroup, error) {
+	return nil, errorGettingPodGroup
+}
