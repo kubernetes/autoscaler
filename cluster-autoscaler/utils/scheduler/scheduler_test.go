f5706179f1fa4279d9c3fa5c1612153a5fa69d22
Daniel Kłobuszewski <danielmk@google.com>
2026-04-24 21:06:47 +0200

    CA: remove dead CreateNodeNameToInfoMap helper (#9528)
    * CA: remove dead CreateNodeNameToInfoMap helper
    
    The function has been unused since Feb 2020 (6 years), when the last
    usage in simulator/cluster.go was removed in PR #2890
    (commit b77579bd64163ba85f0f91e30eaf78e8e1dccfe5).
    
    * Remove unused import in scheduler.go
    
    Removed unused import for cluster-autoscaler simulator framework.
    
    * Remove unused test import in scheduler_test.go
    
    Removed import of test utilities from cluster-autoscaler.

--- cluster-autoscaler/utils/scheduler/scheduler_test.go
+++ cluster-autoscaler/utils/scheduler/scheduler_test.go
@@ -25,7 +25,6 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/resource"
 	testconfig "k8s.io/autoscaler/cluster-autoscaler/config/test"
-	. "k8s.io/autoscaler/cluster-autoscaler/utils/test"
 	"k8s.io/kubernetes/pkg/scheduler/apis/config"
 	schedulerimpl "k8s.io/kubernetes/pkg/scheduler/framework"
 
@@ -33,31 +32,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestCreateNodeNameToInfoMap(t *testing.T) {
-	p1 := BuildTestPod("p1", 1500, 200000)
-	p1.Spec.NodeName = "node1"
-	p2 := BuildTestPod("p2", 3000, 200000)
-	p2.Spec.NodeName = "node2"
-	p3 := BuildTestPod("p3", 3000, 200000)
-	p3.Spec.NodeName = "node3"
-
-	var priority int32 = 100
-	podWaitingForPreemption := BuildTestPod("w1", 1500, 200000)
-	podWaitingForPreemption.Spec.Priority = &priority
-	podWaitingForPreemption.Status.NominatedNodeName = "node1"
-
-	n1 := BuildTestNode("node1", 2000, 2000000)
-	n2 := BuildTestNode("node2", 2000, 2000000)
-
-	res := CreateNodeNameToInfoMap([]*apiv1.Pod{p1, p2, p3, podWaitingForPreemption}, []*apiv1.Node{n1, n2})
-	assert.Equal(t, 2, len(res))
-	assert.Equal(t, p1, res["node1"].Pods()[0].Pod)
-	assert.Equal(t, podWaitingForPreemption, res["node1"].Pods()[1].Pod)
-	assert.Equal(t, n1, res["node1"].Node())
-	assert.Equal(t, p2, res["node2"].Pods()[0].Pod)
-	assert.Equal(t, n2, res["node2"].Node())
-}
 
 func TestResourceList(t *testing.T) {
 	tests := []struct {
