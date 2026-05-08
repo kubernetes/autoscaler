f5a1cb1f14e6a5332130649645dab5c31f543a1d
Developer-1 <suyogkumawat1999@gmail.com>
2026-05-09 04:30:59 +0530

    Fix: Migrate VPA E2E testing deployment to Helm (#9328)

--- vertical-pod-autoscaler/hack/deploy-for-e2e-locally.sh
+++ vertical-pod-autoscaler/hack/deploy-for-e2e-locally.sh
@@ -93,7 +93,7 @@ done
 
 # Prepare Helm values
 HELM_CHART_PATH="${SCRIPT_ROOT}/charts/vertical-pod-autoscaler"
-VALUES_FILE="${SCRIPT_ROOT}/hack/e2e/values-e2e-local.yaml"
+VALUES_FILE="${SCRIPT_ROOT}/hack/e2e/values-e2e.yaml"
 HELM_RELEASE_NAME="vpa"
 HELM_NAMESPACE="kube-system"
 
