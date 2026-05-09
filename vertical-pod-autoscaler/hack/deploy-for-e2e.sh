b8b3ca3124509172b2661522ead0ca8461c19bac
Developer-1 <suyogkumawat1999@gmail.com>
2026-05-09 23:55:49 +0530

    Refactor CI script for Helm idempotency
    Signed-off-by: Developer-1 <suyogkumawat1999@gmail.com>

--- vertical-pod-autoscaler/hack/deploy-for-e2e.sh
+++ vertical-pod-autoscaler/hack/deploy-for-e2e.sh
@@ -59,7 +59,6 @@ case ${SUITE} in
     ;;
 esac
 
-# CI-specific registry logic
 export REGISTRY=gcr.io/`gcloud config get-value core/project`
 export TAG=latest
 
@@ -67,52 +66,57 @@ echo "Configuring registry authentication"
 mkdir -p "${HOME}/.docker"
 gcloud auth configure-docker -q
 
+# Standardize configuration across environments (Addresses Review #1)
+source "${SCRIPT_ROOT}/hack/e2e/helm-settings.sh"
+
 for i in ${COMPONENTS}; do
-  if [ $i == admission-controller ] ; then
-    (cd ${SCRIPT_ROOT}/pkg/${i} && bash ./gencerts.sh e2e || true)
-  fi
   ALL_ARCHITECTURES=amd64 make --directory ${SCRIPT_ROOT}/pkg/${i} release
 done
 
 echo "Deploying VPA components via Helm for E2E CI..."
 
-HELM_CHART_PATH="${SCRIPT_ROOT}/charts/vertical-pod-autoscaler"
-VALUES_FILE="${SCRIPT_ROOT}/hack/e2e/values-e2e.yaml"
-HELM_RELEASE_NAME="vpa"
-HELM_NAMESPACE="kube-system"
-
-# Build dynamic Helm set arguments based on components to isolate test suites
-HELM_SET_ARGS=()
-HELM_SET_ARGS+=("--set" "recommender.enabled=false")
-HELM_SET_ARGS+=("--set" "updater.enabled=false")
-HELM_SET_ARGS+=("--set" "admissionController.enabled=false")
-
+# Generate TLS certificates natively to prevent webhook conflicts in CI
 for COMPONENT in ${COMPONENTS}; do
-  case ${COMPONENT} in
-    recommender)
-      HELM_SET_ARGS+=("--set" "recommender.enabled=true")
-      HELM_SET_ARGS+=("--set" "recommender.image.repository=${REGISTRY}/vpa-recommender")
-      HELM_SET_ARGS+=("--set" "recommender.image.tag=${TAG}")
-      ;;
-    updater)
-      HELM_SET_ARGS+=("--set" "updater.enabled=true")
-      HELM_SET_ARGS+=("--set" "updater.image.repository=${REGISTRY}/vpa-updater")
-      HELM_SET_ARGS+=("--set" "updater.image.tag=${TAG}")
-      ;;
-    admission-controller)
-      HELM_SET_ARGS+=("--set" "admissionController.enabled=true")
-      HELM_SET_ARGS+=("--set" "admissionController.image.repository=${REGISTRY}/vpa-admission-controller")
-      HELM_SET_ARGS+=("--set" "admissionController.image.tag=${TAG}")
-      ;;
-  esac
-done
+  if [[ "${COMPONENT}" == "admission-controller" ]]; then
+    echo " ** Generating TLS certificates for admission-controller"
+    TMP_DIR=$(mktemp -d)
+    CN_BASE="vpa_webhook"
 
-# Helm does not delete or upgrade CRDs under crds/, so remove them here
-kubectl delete crd verticalpodautoscalers.autoscaling.k8s.io --ignore-not-found=true
-kubectl delete crd verticalpodautoscalercheckpoints.autoscaling.k8s.io --ignore-not-found=true
+    cat > "${TMP_DIR}/server.conf" << EOF
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = clientAuth, serverAuth
+subjectAltName = DNS:vpa-webhook.${HELM_NAMESPACE}.svc
+EOF
 
-helm upgrade --install ${HELM_RELEASE_NAME} "${HELM_CHART_PATH}" \
-  --namespace ${HELM_NAMESPACE} \
-  --values "${VALUES_FILE}" \
-  "${HELM_SET_ARGS[@]}" \
-  --wait
+    openssl genrsa -out "${TMP_DIR}/caKey.pem" 2048
+    openssl req -x509 -new -nodes -key "${TMP_DIR}/caKey.pem" -days 10000 \
+      -out "${TMP_DIR}/caCert.pem" -subj "/CN=${CN_BASE}_ca" \
+      -addext "subjectAltName = DNS:${CN_BASE}_ca"
+    openssl genrsa -out "${TMP_DIR}/serverKey.pem" 2048
+    openssl req -new -key "${TMP_DIR}/serverKey.pem" -out "${TMP_DIR}/server.csr" \
+      -subj "/CN=vpa-webhook.${HELM_NAMESPACE}.svc" -config "${TMP_DIR}/server.conf"
+    openssl x509 -req -in "${TMP_DIR}/server.csr" -CA "${TMP_DIR}/caCert.pem" \
+      -CAkey "${TMP_DIR}/caKey.pem" -CAcreateserial -out "${TMP_DIR}/serverCert.pem" \
+      -days 10000 -extensions SAN -extensions v3_req -extfile "${TMP_DIR}/server.conf"
+
+    kubectl delete secret -n ${HELM_NAMESPACE} vpa-tls-certs --ignore-not-found=true
+    kubectl create secret -n ${HELM_NAMESPACE} generic vpa-tls-certs \
+      --from-file="${TMP_DIR}/caKey.pem" \
+      --from-file="${TMP_DIR}/caCert.pem" \
+      --from-file="${TMP_DIR}/serverKey.pem" \
+      --from-file="${TMP_DIR}/serverCert.pem"
+
+    echo " ** Generating E2E rotation test certificates"
+    openssl genrsa -out "${TMP_DIR}/e2eCaKey.pem" 2048
+    openssl req -x509 -new -nodes -key "${TMP_DIR}/e2eCaKey.pem" -days 10000 \
+      -out "${TMP_DIR}/e2eCaCert.pem" -subj "/CN=${CN_BASE}_e2e_ca" \
+      -addext "subjectAltName = DNS:${CN_BASE}_e2e_ca"
+    openssl genrsa -out "${TMP_DIR}/e2eKey.pem" 2048
+    openssl req -new -key "${TMP_DIR}/e2eKey.pem" -out "${TMP_DIR}/e2e.csr" \
+      -subj "/CN=vpa-webhook.${HELM_NAMESPACE}.svc" -config "${TMP
