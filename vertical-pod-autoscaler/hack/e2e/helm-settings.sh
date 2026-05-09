19c6210c461e91f2a3bb487dfea14bce08678b9c
Developer-1 <suyogkumawat1999@gmail.com>
2026-05-10 00:00:11 +0530

    fix verify go error

--- vertical-pod-autoscaler/hack/e2e/helm-settings.sh
+++ vertical-pod-autoscaler/hack/e2e/helm-settings.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Shared Helm configuration for E2E deployments
 HELM_CHART_PATH="${SCRIPT_ROOT}/charts/vertical-pod-autoscaler"
 VALUES_FILE="${SCRIPT_ROOT}/hack/e2e/values-e2e.yaml"
