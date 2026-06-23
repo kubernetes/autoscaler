3557a2bbf79f95662b6d1590adfaac4a4c3d3a45
Lucas Andrade Flores <landrade@redhat.com>
2026-06-23 15:27:05 +0200

    feat(core): implement new metric

--- cluster-autoscaler/clusterstate/clusterstate.go
+++ cluster-autoscaler/clusterstate/clusterstate.go
@@ -435,6 +435,7 @@ func (csr *ClusterStateRegistry) UpdateNodes(nodes []*apiv1.Node, currentTime ti
 		currentTime,
 		targetSizes,
 	)
+
 	return nil
 }
 
@@ -1400,6 +1401,10 @@ func FakeNode(instance cloudprovider.Instance, reason string) *apiv1.Node {
 // GetScaleUpFailures returns the scale-up failures map.
 func (csr *ClusterStateRegistry) GetScaleUpFailures() map[string][]scaleupfailures.Record {
 	return csr.scaleUpFailures.Get()
+}
+
+func (csr *ClusterStateRegistry) GetPerNodeGroupReadiness() map[string]Readiness {
+	return csr.perNodeGroupReadiness
 }
 
 func truncateIfExceedMaxLength(s string, maxLength int) string {
