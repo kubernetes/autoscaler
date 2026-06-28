f907f1f3e33472623238892f548afc6e672c0bdb
Omer Aplatony <omerap12@gmail.com>
2026-06-28 14:13:23 +0300

    Update vertical-pod-autoscaler/pkg/updater/logic/updater.go
    Co-authored-by: Adrian Moisey <adrian@changeover.za.net>

--- vertical-pod-autoscaler/pkg/updater/logic/updater.go
+++ vertical-pod-autoscaler/pkg/updater/logic/updater.go
@@ -537,7 +537,7 @@ func filterNonInPlaceUpdatablePods(pods []*corev1.Pod, inplaceRestriction restri
 			// Cached infeasibility means we've already determined this pod cannot be
 			// updated in-place and cached the result to avoid redundant checks
 			eventRecorder.Event(pod, corev1.EventTypeNormal, "InPlaceResizeSkipped", "Previously recorded in-place update was infeasible, current recommendation is not lower than previous, skipping pod")
-			metrics_updater.RecordInPlaceInfeasibleCached(vpaSize, vpa.Name, vpa.Namespace)
+			metrics_updater.RecordInPlaceInfeasibleCached(vpaCount, vpa.Name, vpa.Namespace)
 			return false
 		default:
 			return false
