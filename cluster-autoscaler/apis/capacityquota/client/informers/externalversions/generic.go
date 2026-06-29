d9d97f69ea2f75495f817ac6d90139170a5929b8
Norbert Cyran <norbertcyran@google.com>
2026-06-29 14:57:30 +0200

    Add CapacityQuota v1beta1 version

--- cluster-autoscaler/apis/capacityquota/client/informers/externalversions/generic.go
+++ cluster-autoscaler/apis/capacityquota/client/informers/externalversions/generic.go
@@ -23,6 +23,7 @@ import (
 
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	v1alpha1 "k8s.io/autoscaler/cluster-autoscaler/apis/capacityquota/autoscaling.x-k8s.io/v1alpha1"
+	v1beta1 "k8s.io/autoscaler/cluster-autoscaler/apis/capacityquota/autoscaling.x-k8s.io/v1beta1"
 	cache "k8s.io/client-go/tools/cache"
 )
 
@@ -55,6 +56,10 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 	// Group=autoscaling.x-k8s.io, Version=v1alpha1
 	case v1alpha1.SchemeGroupVersion.WithResource("capacityquotas"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Autoscaling().V1alpha1().CapacityQuotas().Informer()}, nil
+
+		// Group=autoscaling.x-k8s.io, Version=v1beta1
+	case v1beta1.SchemeGroupVersion.WithResource("capacityquotas"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Autoscaling().V1beta1().CapacityQuotas().Informer()}, nil
 
 	}
 
