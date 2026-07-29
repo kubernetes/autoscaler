871a02f62b0f14bcedac390db028e160371bcc3f
Viktória Spišaková <vspisakova@google.com>
2026-07-29 09:50:44 +0000

    bench/efficiency: implement resourceUtilizationMetric
    Implements resourceUtilizationMetric alongside helper type resource to
    compute the ratio of allocatable vs. requested resource of generic
    ResourceName. Captures the initial utilization ratio, final state utilization ratio and derives the utilization improvement.

--- cluster-autoscaler/core/bench/scaledown/efficiency/metrics.go
+++ cluster-autoscaler/core/bench/scaledown/efficiency/metrics.go
@@ -17,11 +17,15 @@ limitations under the License.
 package efficiency
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/autoscaler/cluster-autoscaler/core/scaledown/status"
 	"k8s.io/autoscaler/cluster-autoscaler/simulator/framework"
+	"k8s.io/autoscaler/cluster-autoscaler/simulator/utilization"
+	"k8s.io/klog/v2"
 )
 
 type metricsDependencies struct {
@@ -75,4 +79,88 @@ func (mt *metricsTracker) ComputeMetrics(dependencies metricsDependencies, t tes
 		}
 		mt.metricsTimeline[m.Name()] = append(mt.metricsTimeline[m.Name()], val)
 	}
+}
+
+type resource struct {
+	allocatable float64
+	requested   float64
+}
+
+func resourceAllocatableRequested(resourceName apiv1.ResourceName, nodeInfo *framework.NodeInfo, nodeName string, currentTime time.Time) (resource, error) {
+	var r resource
+	util, err := utilization.CalculateUtilizationOfResource(nodeInfo, resourceName, true, true, currentTime)
+	if err != nil {
+		return r, err
+	}
+	r.requested = util
+	allocatable, found := nodeInfo.Node().Status.Allocatable[resourceName]
+	if !found {
+		return r, fmt.Errorf("failed to get allocatable %v from %s", resourceName, nodeName)
+	}
+	if resourceName == apiv1.ResourceCPU {
+		r.allocatable = float64(allocatable.MilliValue())
+	} else {
+		r.allocatable = float64(allocatable.Value())
+	}
+	return r, nil
+}
+
+func computeResourceUtilization(metricName string, resourceName apiv1.ResourceName, dependencies metricsDependencies) (float64, error) {
+	var totalRequested, totalAllocatable, ratio float64
+	for _, nodeInfo := range dependencies.nodeInfos {
+		if nodeInfo == nil || nodeInfo.Node() == nil {
+			continue
+		}
+		nodeName := nodeInfo.Node().Name
+		res, err := resourceAllocatableRequested(resourceName, nodeInfo, nodeName, dependencies.currentTime)
+		if err != nil {
+			klog.V(5).ErrorS(err, fmt.Sprintf("failed to calculate node resource %s", resourceName.String()))
+			return 0, err
+		}
+		nodeRequested := res.requested * res.allocatable
+		totalRequested += nodeRequested
+		totalAllocatable += res.allocatable
+	}
+
+	if totalAllocatable > 0 {
+		ratio = totalRequested / totalAllocatable
+	}
+
+	klog.V(5).Infof("Metric: %s, Allocated: %.2f, Allocatable: %.2f, Ratio: %.2f",
+		metricName, totalRequested, totalAllocatable, ratio)
+	return ratio, nil
+}
+
+// resourceUtilizationMetric computes utilization ratio for given resource of generic ResourceName.
+type resourceUtilizationMetric struct {
+	resourceName apiv1.ResourceName
+}
+
+// NewResourceUtilizationMetric creates a new instance of resourceUtilizationMetric.
+func NewResourceUtilizationMetric(rn apiv1.ResourceName) *resourceUtilizationMetric {
+	return &resourceUtilizationMetric{resourceName: rn}
+}
+
+func (m *resourceUtilizationMetric) Name() string {
+	return fmt.Sprintf("%s_utilization", m.resourceName.String())
+}
+
+func (m *resourceUtilizationMetric) Compute(dependencies metricsDependencies) (float64, error) {
+	ratio, err := computeResourceUtilization(m.Name(), m.resourceName, dependencies)
+	if err != nil {
+		return 0, err
+	}
+	return ratio, nil
+}
+
+func (m *resourceUtilizationMetric) Summarize(metricValues []float64) map[string]float64 {
+	first := metricValues[0]
+	last := metricValues[len(metricValues)-1]
+	diff := last - first
+	r := map[string]float64{
+		fmt.Sprintf("%s_%%_init", m.Name()):     first * 100,
+		fmt.Sprintf("%s_%%_final", m.Name()):    last * 100,
+		fmt.Sprintf("%s_%%_improved", m.Name()): diff * 100,
+	}
+	return r
 }
