7303ae367c636cf8eed1902d1c6b5aec3b8ed763
Hubert Grochowski <hgrochowski@google.com>
2026-08-03 09:40:46 +0000

    [DROP ME] Trigger ca benchmark
    This is to test is bench job works.

--- pkg/main.go
+++ pkg/main.go
@@ -199,6 +199,8 @@ func mustBuildAutoscaler(ctx context.Context, opts config.AutoscalingOptions, de
 	return autoscaler, trigger
 }
 
+// Comment to trigger a benchmark. 
+
 func main() {
 	klog.InitFlags(nil)
 
