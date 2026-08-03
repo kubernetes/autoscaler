1e04eecf968af861a9bc9d6cd99e4349047d4642
Viktória Spišaková <vspisakova@google.com>
2026-08-03 11:00:46 +0000

    bench/efficiency: introduce core metric interface and orchestration tracker
    Introduces the abstract scaleDownMetric interface which all metrics will implement and metricsTracker which orchestrates the computation, storing, and reporting of the configured scaleDownMetrics.

---
+++ pkg/core/bench/scaledown/efficiency/metrics.go
@@ -0,0 +1,82 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package efficiency
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/cluster-autoscaler/pkg/core/scaledown/status"
+	"sigs.k8s.io/cluster-autoscaler/pkg/simulator/framework"
+	"sigs.k8s.io/cluster-autoscaler/pkg/simulator/utilization"
+)
+
+type clusterState struct {
+	scaledDownNodes []*status.ScaleDownNode
+	nodeInfos       []*framework.NodeInfo
+	currentTime     time.Time
+}
+
+// scaleDownMetric is the unified interface all metrics implement.
+type scaleDownMetric interface {
+	// Name returns name of the scaleDownMetric.
+	Name() string
+	// Compute calculates single cluster-level metric value using clusterState.
+	Compute(clusterState clusterState) (float64, error)
+	// Summarize processes metric recorded timeline and derives final benchmarking values returned in a map.
+	Summarize(metricValues []float64) map[string]float64
+}
+
+// metricsTracker orchestrates the computation, storing, and reporting of multiple scaleDownMetric.
+type metricsTracker struct {
+	metrics         []scaleDownMetric
+	metricsTimeline map[string][]float64
+}
+
+// NewMetricsTracker creates a new instance of metricsTracker with given list of scaleDownMetrics.
+func NewMetricsTracker(metrics ...scaleDownMetric) *metricsTracker {
+	return &metricsTracker{
+		metrics:         metrics,
+		metricsTimeline: make(map[string][]float64),
+	}
+}
+
+// ReportToBenchmark registers final benchmark values to benchmark runner across a set of configured scaleDownMetrics.
+func (mt *metricsTracker) ReportToBenchmark(b *testing.B) {
+	for _, m := range mt.metrics {
+		metricValues := mt.metricsTimeline[m.Name()]
+		benchVals := m.Summarize(metricValues)
+		for name, val := range benchVals {
+			b.ReportMetric(val, name)
+		}
+	}
+}
+
+// ComputeMetrics computes metric value across a set of configured scaleDownMetrics.
+func (mt *metricsTracker) ComputeMetrics(clusterState clusterState, t testing.TB) {
+	for _, m := range mt.metrics {
+		val, err := m.Compute(clusterState)
+		if err != nil {
+			t.Errorf("failed to compute metric %s, err %v", m.Name(), err)
+			continue
+		}
+		mt.metricsTimeline[m.Name()] = append(mt.metricsTimeline[m.Name()], val)
+	}
+}
